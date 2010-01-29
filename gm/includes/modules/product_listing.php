343bd277972e134476d661102b9787cd73a1fabf
maker <makerwang@gmail.com>
2010-01-29 09:48:01 +0800

    $id$

--- gm/includes/modules/product_listing.php
+++ gm/includes/modules/product_listing.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: product_listing.php,v 1.1.1.1 2003/02/20 01:03:54 ptosh Exp $
+  $Id$
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
   Copyright (c) 2003 osCommerce
