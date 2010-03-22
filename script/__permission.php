9cabb5c23b066db547ed347c5505f34b0b4bdaca
maker <makerwang@gmail.com>
2010-03-22 16:09:53 +0800

    permissions

--- script/__permission.php
+++ script/__permission.php
@@ -1,12 +1,56 @@
 <?php
-admin/upload_images/
-admin/upload_images/0/products/
-admin/upload_images/0/categories/
-admin/upload_images/0/news/
-admin/upload_images/0/colors/
-admin/upload_images/0/present/
-admin/upload_images/0/banners/
-admin/upload_images/0/products/
-admin/upload_images/0/orders_status/
-admin/upload_images/0/tags/
-admin/upload_images/0/manufacturers/
+chmod('admin/upload_images/', 0777);
+
+chmod('admin/upload_images/0/products/', 0777);
+chmod('admin/upload_images/0/categories/', 0777);
+chmod('admin/upload_images/0/news/', 0777);
+chmod('admin/upload_images/0/colors/', 0777);
+chmod('admin/upload_images/0/present/', 0777);
+chmod('admin/upload_images/0/banners/', 0777);
+chmod('admin/upload_images/0/products/', 0777);
+chmod('admin/upload_images/0/orders_status/', 0777);
+chmod('admin/upload_images/0/tags/', 0777);
+chmod('admin/upload_images/0/manufacturers/', 0777);
+chmod('admin/upload_images/0/imagecache/', 0777);
+chmod('admin/upload_images/0/imagecache2/', 0777);
+chmod('admin/upload_images/0/imagecache3/', 0777);
+
+chmod('admin/upload_images/1/products/', 0777);
+chmod('admin/upload_images/1/categories/', 0777);
+chmod('admin/upload_images/1/news/', 0777);
+chmod('admin/upload_images/1/colors/', 0777);
+chmod('admin/upload_images/1/present/', 0777);
+chmod('admin/upload_images/1/banners/', 0777);
+chmod('admin/upload_images/1/products/', 0777);
+chmod('admin/upload_images/1/orders_status/', 0777);
+chmod('admin/upload_images/1/tags/', 0777);
+chmod('admin/upload_images/1/manufacturers/', 0777);
+chmod('admin/upload_images/1/imagecache/', 0777);
+chmod('admin/upload_images/1/imagecache2/', 0777);
+chmod('admin/upload_images/1/imagecache3/', 0777);
+chmod('admin/upload_images/2/products/', 0777);
+chmod('admin/upload_images/2/categories/', 0777);
+chmod('admin/upload_images/2/news/', 0777);
+chmod('admin/upload_images/2/colors/', 0777);
+chmod('admin/upload_images/2/present/', 0777);
+chmod('admin/upload_images/2/banners/', 0777);
+chmod('admin/upload_images/2/products/', 0777);
+chmod('admin/upload_images/2/orders_status/', 0777);
+chmod('admin/upload_images/2/tags/', 0777);
+chmod('admin/upload_images/2/manufacturers/', 0777);
+chmod('admin/upload_images/2/imagecache/', 0777);
+chmod('admin/upload_images/2/imagecache2/', 0777);
+chmod('admin/upload_images/2/imagecache3/', 0777);
+chmod('admin/upload_images/3/products/', 0777);
+chmod('admin/upload_images/3/categories/', 0777);
+chmod('admin/upload_images/3/news/', 0777);
+chmod('admin/upload_images/3/colors/', 0777);
+chmod('admin/upload_images/3/present/', 0777);
+chmod('admin/upload_images/3/banners/', 0777);
+chmod('admin/upload_images/3/products/', 0777);
+chmod('admin/upload_images/3/orders_status/', 0777);
+chmod('admin/upload_images/3/tags/', 0777);
+chmod('admin/upload_images/3/manufacturers/', 0777);
+chmod('admin/upload_images/3/imagecache/', 0777);
+chmod('admin/upload_images/3/imagecache2/', 0777);
+chmod('admin/upload_images/3/imagecache3/', 0777);
