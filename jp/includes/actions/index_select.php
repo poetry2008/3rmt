877e2e2a5f130fd9d1c7d64dc97b54f83b563f40
root <root@pgServer.(none)>
2010-05-13 13:58:50 +0800

    整理jp,gm,wm的index.php和actions

--- jp/includes/actions/index_select.php
+++
@@ -1,8 +0,0 @@
-	  <td valign="top" id="contents">
-      <?php include(DIR_WS_MODULES . 'select_categories.php');?>
-    </td>
-	  <!-- body_text_eof //--> 
-      <td width="<?php echo BOX_WIDTH; ?>" valign="top" class="right_colum_border">
-      <!-- right_navigation //--> 
-      <?php require(DIR_WS_INCLUDES . 'column_right.php'); ?> 
-      <!-- right_navigation_eof //--></td> 
