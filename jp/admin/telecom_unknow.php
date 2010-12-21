c737b06b85ac22ed9f9df5e01de0a31f06cab075
maker <makerwang@gmail.com>
2010-12-21 13:15:32 +0800

    全选报错

--- jp/admin/telecom_unknow.php
+++ jp/admin/telecom_unknow.php
@@ -38,16 +38,35 @@
   }
 // 全选
 function all_check(ele){
+  var error = false;
   if (ele.checked) {
+
+    $('.a_checkbox').each(function(){
+      if (this.value == 'false') {
+        error = error || true;
+      }
+    });
+    if (error) {
+      alert('引当が「済」になっているものは、非表示には出来ません');
+      ele.checked = false;
+      return false;
+    }
+
     $('.a_checkbox').attr('checked',true).parent().parent().find('td').css('background','#f08080');
   } else {
     $('.a_checkbox').each(function(){
       this.checked=false;
       check_one(this);
     });
+
   }
 }
 function check_one(ele){
+  if(ele.value=='false') {
+    alert('引当が「済」になっているものは、非表示には出来ません');
+    ele.checked = false;
+    return false;
+  }
   if(ele.checked){
     $('#tr_'+ele.value+' td').css('background','#f08080');
   } else {
@@ -182,7 +201,7 @@ function check_one(ele){
     while ($orders = tep_db_fetch_array($orders_query)) {
       echo '    <tr onmouseover="this.style.background=\'#FFCC99\'" onmouseout="this.style.background=\''.(tep_match_by_keywords($orders['telno'],TELNO_KEYWORDS)?'red':($orders['rel'] == 'no'?'#ccc':'#fff')).'\'" style="border-bottom:1px solid #000000;'.(tep_match_by_keywords($orders['telno'],TELNO_KEYWORDS)?'background:red':($orders['rel'] == 'no'?'background:#ccc':'background:#fff')).'" class="dataTableRow" id="tr_'.$orders['id'].'">' . "\n";
 ?>
-      <td align="left"   style="border-bottom:1px solid #000000;" class="dataTableContent"><?php if ($orders['type'] == 'success' && $orders['option']) { ?><input type="checkbox" name="ids[]" class="a_checkbox" onclick="check_one(this)" value="<?php echo $orders['id'];?>"><?php }?></td>
+      <td align="left"   style="border-bottom:1px solid #000000;" class="dataTableContent"><input type="checkbox" name="ids[]" class="a_checkbox" onclick="check_one(this)" value="<?php echo $orders['type'] == 'success' && $orders['option'] ? $orders['id'] : 'false';?>"></td>
       <td align="center" style="border-bottom:1px solid #000000;" class="dataTableContent"><?php echo tep_datetime_short($orders['date_added']); ?>&nbsp;</td>
       <td align="center" style="border-bottom:1px solid #000000;" class="dataTableContent" align="right"><?php echo $orders['rel'] == 'yes' ? '成功' : '失敗'; ?>&nbsp;</td>
       <td align="center" style="border-bottom:1px solid #000000;" class="dataTableContent" align="right"><?php echo $orders['type'] == 'success' && $orders['option'] ? '済' : '<font color="darkred">未</font>'; ?>&nbsp;</td>
