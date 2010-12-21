421d3da9cc618b6a3662417079a15f06e71b88ca
maker wang <jp_gamelife_jp@www.gamelife.jp>
2010-12-21 18:25:19 +0900

    for merge

--- jp/admin/includes/configure.php
+++ jp/admin/includes/configure.php
@@ -7,23 +7,23 @@
 
   Released under the GNU General Public License
 */
-$libpath = "/home/maker/project/3rmt/3rmtlib/";
+$libpath = "/home/.sites/22/site13/vhosts/jp/3rmtlib/";
 ini_set('include_path',ini_get('include_path').':'.$libpath);
 
 // Define the webserver and path parameters
-// * DIR_FS_* = Filesystem directories (local/physical)
+// * DIR_FS_* = Filesystem directories (local/physical) 
 // * DIR_WS_* = Webserver directories (virtual/URL)
-  define('HTTP_SERVER', 'http://3jp.maker.200.com/admin'); // eg, http://localhost or - https://localhost should not be empty for productive servers
-  define('HTTP_CATALOG_SERVER', 'http://3jp.maker.200.com/admin');
-  define('HTTPS_CATALOG_SERVER', 'https://3jp.maker.200.com/admin');
+  define('HTTP_SERVER', 'http://jp.gamelife.jp/admin'); // eg, http://localhost or - https://localhost should not be empty for productive servers
+  define('HTTP_CATALOG_SERVER', 'http://jp.gamelife.jp/admin');
+  define('HTTPS_CATALOG_SERVER', 'https://jp.gamelife.jp/admin');
   define('ENABLE_SSL_CATALOG', 'false'); // secure webserver for catalog module
-
-  define('DIR_FS_DOCUMENT_ROOT', '/home/maker/project/3rmt/jp/admin/'); // where the pages are located on the server
+  define('DIR_FS_DOCUMENT_ROOT', '/home/.sites/22/site13/vhosts/jp/admin/'); // where the pages are located on the server
   define('DIR_WS_ADMIN', '/'); // absolute path required
-  define('DIR_FS_ADMIN', '/home/maker/project/3rmt/jp/admin/'); // absolute pate required
-
+  define('DIR_FS_ADMIN', '/home/.sites/22/site13/vhosts/jp/admin/'); // absolute pate required
+  //define('DIR_WS_CATALOG', './'); // absolute path required
+  //define('DIR_FS_CATALOG', ''); // absolute path required
   define('DIR_WS_CATALOG', '/'); // absolute path required
-  define('DIR_FS_CATALOG', '/home/maker/project/3rmt/jp/admin/'); // absolute path required
+  define('DIR_FS_CATALOG', '/home/.sites/22/site13/vhosts/jp/admin/'); // absolute path required
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
 
@@ -47,11 +47,12 @@ ini_set('include_path',ini_get('include_path').':'.$libpath);
 
 // define our database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
-  define('DB_SERVER_USERNAME', 'root');
-  define('DB_SERVER_PASSWORD', '123456');
-  define('DB_DATABASE', 'maker_3rmt');
-  define('USE_PCONNECT', 'false'); // use persisstent connections?
-  define('STORE_SESSIONS', ''); // leave empty '' for default handler or set to 'mysql'
+  define('DB_SERVER_USERNAME', 'jp_gamelife_jp');
+  define('DB_SERVER_PASSWORD', 'kWSoiSiE');
+  define('DB_DATABASE',        'jp_gamelife_jp');
+  define('USE_PCONNECT',       'false'); // use persisstent connections?
+  define('STORE_SESSIONS',     ''); // leave empty '' for default handler or set to 'mysql'
+  define('SESSION_NAME_OSC','oscsid');
 
   //control sql_log
   define('SQL_LOG', false);
@@ -59,6 +60,15 @@ ini_set('include_path',ini_get('include_path').':'.$libpath);
   define('DIR_WS_IMAGE_DOCUMENTS', 'imageDocuments/');
   define('DIR_FS_CATALOG_IMAGE_DOCUMENTS', DIR_FS_CATALOG.'upload_images/0/imageDocuments/');
   //define('DIR_FS_CACHE', '/tep/jp');
+  $stop_site_url = array(
+      //"iimy.co.jp",
+      //"www.iimy.co.jp",
+      );
 
 // osticket start 
-  define('DIR_OST',DIR_FS_ADMIN.'../includes/ost');
+  define('DIR_OST','/home/.sites/22/site13/vhosts/jp/3rmtlib/includes/ost/');
+  define('SITE_TOPIC_1',3);
+  define('SITE_TOPIC_2',5);
+  define('SITE_TOPIC_3',4);
+    define('SITE_TOPIC_4',6);
+      define('SITE_TOPIC_5',7);
--- kt/includes/configure.php
+++ kt/includes/configure.php
@@ -6,8 +6,8 @@
 // Define the webserver and path parameters
 // * DIR_FS_* = Filesystem directories (local/physical)
 // * DIR_WS_* = Webserver directories (virtual/URL)
-  define('HTTP_SERVER', 'http://3km.kthiz.200.com'); // eg, http://localhost - should not be empty for productive servers
-  define('HTTPS_SERVER', 'https://cchm.vicp.net'); // eg, https://localhost - should not be empty for productive servers
+  define('HTTP_SERVER', 'http://kt.gamelife.jp'); // eg, http://localhost - should not be empty for productive servers
+  define('HTTPS_SERVER', 'https://kt.gamelife.jp'); // eg, https://localhost - should not be empty for productive servers
   define('ENABLE_SSL', false); // secure webserver for checkout procedure?
   define('DIR_WS_CATALOG', '/'); // absolute path required
   define('DIR_WS_IMAGES', 'images/');
@@ -21,18 +21,18 @@
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
 
   define('DIR_WS_DOWNLOAD_PUBLIC', DIR_WS_CATALOG . 'pub/');
-  define('DIR_FS_DOCUMENT_ROOT', '/home/kthiz/project/OSC_3RMT/km/');
-  define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT);
+  define('DIR_FS_DOCUMENT_ROOT', '/home/.sites/22/site13/vhosts/kt/');
+  define('DIR_FS_CATALOG', '/home/.sites/22/site13/vhosts/kt/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
   
-  define('DIR_FS_3RMTLIB', DIR_FS_DOCUMENT_ROOT.'../3rmtlib/');
+  define('DIR_FS_3RMTLIB', '/home/.sites/22/site13/vhosts/jp/3rmtlib/');
 
 // define our database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
-  define('DB_SERVER_USERNAME', 'root');
-  define('DB_SERVER_PASSWORD', '123456');
-  define('DB_DATABASE', 'maker_3rmt');
+  define('DB_SERVER_USERNAME', 'jp_gamelife_jp');
+  define('DB_SERVER_PASSWORD', 'kWSoiSiE');
+  define('DB_DATABASE', 'jp_gamelife_jp');
   define('USE_PCONNECT', 'false'); // use persistent connections?
   define('STORE_SESSIONS', ''); // leave empty '' for default handler or set to 'mysql'
 
@@ -43,9 +43,9 @@
   define('SITE_ID', '5'); 
   //control sql_log
   define('SQL_LOG', false);
+
   ini_set('include_path',ini_get('include_path').':'.DIR_FS_3RMTLIB);
 //osticket start
   //define('DIR_OST',DIR_FS_DOCUMENT_ROOT.DIR_WS_INCLUDES.'ost/');
   define('DIR_OST','includes/ost/');
-  define('SITE_TOPIC_ID',4); 
-
+  define('SITE_TOPIC_ID',7); 
