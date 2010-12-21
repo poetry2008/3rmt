e180440182e14be2a1c1993a43ae32ce3a8993ca
maker wang <jp_gamelife_jp@www.gamelife.jp>
2010-12-21 17:34:08 +0900

    delete script

--- script/unupgrade.sql
+++
@@ -1,5 +0,0 @@
-﻿delete from configuration where configuration_key = 'DEFAULT_PAGE_BOTTOM_CONTENTS';
-delete from configuration where configuration_key = 'ALL_GAME_RSS';
-delete from configuration where configuration_key = 'GAME_NEWS_MAX_DISPLAY';
-delete from configuration where configuration_key = 'CATEGORIES_GAME_NEWS_MAX_DISPLAY';
-delete from configuration where configuration_key = 'PRINT_EMAIL_ADDRESS';
