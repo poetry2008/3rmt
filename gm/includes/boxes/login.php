f6f4c0bf93fca75e3dc3727f73aac61de0ffcd4a
kthiz <kthiz2006@gmail.com>
2010-12-28 13:12:45 +0800

    rmt login edit

--- gm/includes/boxes/login.php
+++ gm/includes/boxes/login.php
@@ -5,37 +5,12 @@
 ?>
 <!-- login //-->
 <script type="text/javascript" src="js/logincheck.js"></script>
-<div class="login02">
-	<a href="<?php echo tep_href_link(FILENAME_LOGIN,'','SSL'); ?>"><img class="middle" src="images/banners/login02.gif" alt="ログイン"></a>
-</div>
 <?php
    if (!tep_session_is_registered('customer_id')) {
 ?>
-<?php echo tep_draw_form('login', tep_href_link(FILENAME_LOGIN, 'action=process', 'SSL'), 'post', 'onSubmit="return msg();"')."\n"; ?>
-<!--
-<div class="login_right01">
-<div class="box_des01">ログイン</div> 
-<table  border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td width="25" height="25">Email:</td>
-    <td><input type="text" name="email_address" style="width:80px; height:22px;"></td>
-  </tr>
-  <tr>
-    <td height="25">PW:</td>
-    <td><input type="password" name="password" style="width:80px; height:22px;"></td>
-  </tr>
-</table>
-<table  border="0" align="center" cellpadding="0" cellspacing="3">
-  <tr>
-    <td class="smallText"><a href="<?php echo tep_href_link(FILENAME_PASSWORD_FORGOTTEN,'','SSL');?>">PW忘れた?</a></td>
-    <td align="right" style="padding-right:5px; "><input name="image" type="image" alt="img" title="<?php echo HEADER_TITLE_LOGIN ; ?>" src="images/design/button/login.gif"></td>
-  </tr>
-</table>
-<div class="login_right_bottom01">
+<div class="login02">
+	<a href="<?php echo tep_href_link(FILENAME_LOGIN,'','SSL'); ?>"><img class="middle" src="images/banners/login02.gif" alt="ログイン"></a>
 </div>
-</div>
--->
-</form>
 <?php 
   } else {
 ?>
