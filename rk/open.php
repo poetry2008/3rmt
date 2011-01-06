76e83f914c77394a63f1e078a348da289724dda1
maker <makerwang@gmail.com>
2011-01-06 10:18:33 +0800

    替换网站名

--- rk/open.php
+++ rk/open.php
@@ -33,7 +33,7 @@ if($_POST):
     }
     //Ticket::create...checks for errors..
     if(($ticket=Ticket::create($_POST,$errors,SOURCE))){
-        $msg='お客様のご質問は RMTジャックポット へ送信されました。';
+        $msg='お客様のご質問は RMTカメズ へ送信されました。';
         if($thisclient && $thisclient->isValid()) //Logged in...simply view the newly created ticket.
             @header('Location: tickets.php?id='.$ticket->getExtId());
         //Thank the user and promise speedy resolution!
