6838d439221c181ac8314b85ce8710c9e5e0e442
maker <makerwang@gmail.com>
2011-01-07 13:42:29 +0800

    id link

--- id/info_shopping_cart.php
+++ id/info_shopping_cart.php
@@ -12,11 +12,13 @@
 <?php page_head();?>
 </head>
 <body>
+<div class="text_color">
 <p class="main"><b><?php echo HEADING_TITLE; ?></b><br><?php echo tep_draw_separator(); ?></p>
 <p class="main"><b><i><?php echo SUB_HEADING_TITLE_1; ?></i></b><br><?php echo SUB_HEADING_TEXT_1; ?></p>
 <p class="main"><b><i><?php echo SUB_HEADING_TITLE_2; ?></i></b><br><?php echo SUB_HEADING_TEXT_2; ?></p>
 <p class="main"><b><i><?php echo SUB_HEADING_TITLE_3; ?></i></b><br><?php echo SUB_HEADING_TEXT_3; ?></p>
 <p align="right" class="main"><a href="javascript:window.close();"><?php echo TEXT_CLOSE_WINDOW; ?></a></p>
+</div>
 </body>
 </html>
 <?php
