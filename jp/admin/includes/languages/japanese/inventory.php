ca8641293dcfd050fa1400be378c89fb05a8f13b
szn <sznforwork@gmail.com>
2011-01-13 11:35:06 +0800

    修改 库存管理

--- jp/admin/includes/languages/japanese/inventory.php
+++ jp/admin/includes/languages/japanese/inventory.php
@@ -1,8 +1,8 @@
 <?php
 define('INVENTORY_TITLE','在庫水準');
-define('TEXT_PRODUCT_NAME','カテゴリー / 商品');
-define('TEXT_IMAGINARY','架空   ');
-define('TEXT_PRODUCT_PRICE','単価');
-define('TEXT_PRODUCT_QUANTITY','数量');
-define('TEXT_MAX_INVENTORY','最大在庫水準');
-define('TEXT_MIN_INVENTORY','最小在庫水準');
+define('TEXT_PRODUCT_NAME','商品名（リンク有）');
+define('TEXT_IMAGINARY','架空数量');
+define('TEXT_PRODUCT_PRICE','価格');
+define('TEXT_PRODUCT_QUANTITY','現在の在庫');
+define('TEXT_MAX_INVENTORY','最大値');
+define('TEXT_MIN_INVENTORY','最小値');
