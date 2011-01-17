cf25ebc8f9864c651145f25b4659b413a687620d
szn <sznforwork@gmail.com>
2011-01-17 17:43:04 +0800

    change jp/admin/categories.php 样式

--- jp/admin/categories.php
+++ jp/admin/categories.php
@@ -2448,7 +2448,7 @@ $contents[] = array('text' => '<br>' . TEXT_PRODUCTS_AVERAGE_RATING . ' ' . numb
     }
 
     if ( (tep_not_null($heading)) && (tep_not_null($contents)) ) {
-      echo '            <td width="25%" valign="top">' . "\n";
+      echo '            <td style="padding-left:15px;" width="25%" valign="top">' . "\n";
 
       $box = new box;
       echo $box->infoBox($heading, $contents);
