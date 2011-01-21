d3f34d1a4d7601b977d9b92676c7ad419e5ad554
maker <makerwang@gmail.com>
2011-01-21 19:12:46 +0800

    q81

--- jp/admin/orders.php
+++ jp/admin/orders.php
@@ -563,7 +563,7 @@
   var base_url = '<?php echo tep_href_link(FILENAME_ORDERS, tep_get_all_get_params(array('questions_type')));?>';
   
   // 非完成状态的订单不显示最终确认
-  var show_q_8_1_able  = <?php echo tep_orders_finished($_GET['oID'])?'true':'false';?>;
+  var show_q_8_1_able  = <?php echo tep_orders_finished($_GET['oID']) && $order->tori['date'] !== '0000-00-00 00:00:00'?'true':'false';?>;
   
   var cfg_last_customer_action = '<?php echo LAST_CUSTOMER_ACTION;?>';
 
