1cc3f05916809e9099d0073c2a90635340d19a61
szn <sznforwork@gmail.com>
2011-01-27 14:06:44 +0800

    修改 项目 文件夹名称

---
+++ rg/checkout_success.php
@@ -0,0 +1,205 @@
+<?php
+/*
+  $Id$
+*/
+
+  require('includes/application_top.php');
+
+// if the customer is not logged on, redirect them to the shopping cart page
+  if (!tep_session_is_registered('customer_id')) {
+    tep_redirect(tep_href_link(FILENAME_SHOPPING_CART, '', 'SSL'));
+  }
+
+  if (isset($_GET['action']) && ($_GET['action'] == 'update')) {
+    $notify_string = 'action=notify&';
+    $notify = $_POST['notify'];
+    if (!is_array($notify)) $notify = array($notify);
+    for ($i=0, $n=sizeof($notify); $i<$n; $i++) {
+      $notify_string .= 'notify[]=' . $notify[$i] . '&';
+    }
+    if (strlen($notify_string) > 0) $notify_string = substr($notify_string, 0, -1);
+
+    tep_redirect(tep_href_link(FILENAME_DEFAULT, $notify_string));
+  }
+
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_CHECKOUT_SUCCESS);
+
+  $breadcrumb->add(NAVBAR_TITLE_1);
+  $breadcrumb->add(NAVBAR_TITLE_2);
+
+//ccdd
+  $global_query = tep_db_query("
+      SELECT global_product_notifications 
+      FROM " . TABLE_CUSTOMERS_INFO . " 
+      WHERE customers_info_id = '" . $customer_id . "'
+      ");
+  $global = tep_db_fetch_array($global_query);
+
+  if ($global['global_product_notifications'] != '1') {
+//ccdd
+    $orders_query = tep_db_query("
+        SELECT orders_id 
+        FROM " . TABLE_ORDERS . " 
+        WHERE customers_id = '" . $customer_id . "' 
+         AND site_id = '".SITE_ID."' 
+        ORDER BY date_purchased DESC 
+        LIMIT 1
+      ");
+    $orders = tep_db_fetch_array($orders_query);
+
+    $products_array = array();
+//ccdd
+    $products_query = tep_db_query("
+        SELECT products_id, products_name 
+        FROM " . TABLE_ORDERS_PRODUCTS . " 
+        WHERE orders_id = '" . $orders['orders_id'] . "' 
+        ORDER BY products_name
+      ");
+    while ($products = tep_db_fetch_array($products_query)) {
+      $products_array[] = array('id' => $products['products_id'],
+                                'text' => $products['products_name']);
+    }
+  }
+?>
+<?php page_head();?>
+</head>
+<body><div class="body_shadow" align="center"> 
+  <?php require(DIR_WS_INCLUDES . 'header.php'); ?> 
+  <!-- header_eof //--> 
+  <!-- body //--> 
+  <table width="900" border="0" cellpadding="0" cellspacing="0" class="side_border"> 
+    <tr> 
+      <td width="<?php echo BOX_WIDTH; ?>" align="right" valign="top" class="left_colum_border"> <!-- left_navigation //--> 
+        <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?> 
+        <!-- left_navigation_eof //--> </td> 
+      <!-- body_text //--> 
+      <td valign="top" id="contents"><?php echo tep_draw_form('order', tep_href_link(FILENAME_CHECKOUT_SUCCESS, 'action=update', 'SSL')); ?> 
+        <h1 class="pageHeading"><img align="top" alt="" src="images/menu_ico.gif"><span><?php echo HEADING_TITLE ; ?></span></h1> 
+        
+        <div class="comment"> 
+          <table border="0" width="100%" cellspacing="0" cellpadding="0">
+        
+      <?php
+      #convenience_store
+      if($_GET['SID'] != "" || $_POST['SID'] != ""){
+        
+        if($_GET['SID'] != ""){
+          $pr = '?sid=' . $_GET['SID'];
+        }
+        
+        if($_POST['SID'] != ""){
+          $pr = '?sid=' . $_POST['SID'];
+        }
+        
+        echo '<tr><td>';
+        //echo '<a href="convenience_store_chk.php' . $pr . '">' . 'コンビニ決済はこちらから！！</a>';
+        echo '</td></tr>';
+      }
+      ?>
+            <tr> 
+              <td><table border="0" width="100%" cellspacing="0" cellpadding="0"> 
+                  <tr> 
+                    <td width="20%"><table border="0" width="100%" cellspacing="0" cellpadding="0"> 
+                        <tr> 
+                          <td width="50%" align="right"><?php echo tep_draw_separator('pixel_silver.gif', '1', '5'); ?></td> 
+                          <td width="50%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
+                        </tr> 
+                      </table></td> 
+                    <td width="20%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
+                    <td width="20%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
+                    <td width="20%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
+                    <td width="20%"><table border="0" width="100%" cellspacing="0" cellpadding="0"> 
+                        <tr> 
+                          <td width="50%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
+                          <td width="50%"><?php echo tep_image(DIR_WS_IMAGES . 'checkout_bullet.gif'); ?></td> 
+                        </tr> 
+                      </table></td> 
+                  </tr> 
+                  <tr> 
+                    <td align="center" width="20%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_PRODUCTS; ?></td> 
+                    <td align="center" width="20%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_DELIVERY; ?></td> 
+                    <td align="center" width="20%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_PAYMENT; ?></td> 
+                    <td align="center" width="20%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_CONFIRMATION; ?></td> 
+                    <td align="center" width="20%" class="checkoutBarCurrent"><?php echo CHECKOUT_BAR_FINISHED; ?></td> 
+                  </tr> 
+                </table></td> 
+            </tr> 
+            <tr> 
+              <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td> 
+            </tr> 
+            <tr> 
+              <td align="right"><?php echo tep_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td> 
+            </tr> 
+            <tr> 
+              <td><table border="0" width="100%" cellspacing="4" cellpadding="2"> 
+                  <tr> 
+                    <td valign="top"><?php echo tep_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td> 
+                    <td valign="top" class="main"><?php echo tep_draw_separator('pixel_trans.gif', '1', '10'); ?> 
+                      <br>
+                      <?php echo TEXT_SUCCESS; ?><br>
+                      <br>
+                      <?php
+  if ($global['global_product_notifications'] != '1') {
+    echo TEXT_NOTIFY_PRODUCTS . '<br><p class="productsNotifications">';
+
+    $products_displayed = array();
+    for ($i=0, $n=sizeof($products_array); $i<$n; $i++) {
+      if (!in_array($products_array[$i]['id'], $products_displayed)) {
+        echo tep_draw_checkbox_field('notify[]', $products_array[$i]['id']) . ' ' . $products_array[$i]['text'] . '<br>';
+        $products_displayed[] = $products_array[$i]['id'];
+      }
+    }
+
+    echo '</p>';
+  } else {
+    echo TEXT_SEE_ORDERS . '<br><br>' . TEXT_CONTACT_STORE_OWNER;
+  }
+?> 
+                      <h2><?php echo TEXT_THANKS_FOR_SHOPPING; ?></h2></td> 
+                  </tr> 
+                </table></td> 
+            </tr> 
+            <tr> 
+              <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td> 
+            </tr> 
+            <tr> 
+              <td align="right" class="main"><?php echo tep_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td> 
+            </tr> 
+            <tr> 
+              <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td> 
+            </tr> 
+            <?php if (DOWNLOAD_ENABLED == 'true') include(DIR_WS_MODULES . 'downloads.php'); ?> 
+          </table> 
+          </form> 
+        </div>
+        <p class="pageBottom"></p>
+        </td> 
+      <!-- body_text_eof //--> 
+      <td valign="top" class="right_colum_border" width="<?php echo BOX_WIDTH; ?>"> <!-- right_navigation //--> 
+        <?php require(DIR_WS_INCLUDES . 'column_right.php'); ?> 
+        <!-- right_navigation_eof //--> </td> 
+  </table> 
+  <!-- body_eof //--> 
+  <!-- footer //--> 
+  <?php require(DIR_WS_INCLUDES . 'footer.php'); ?> 
+  <!-- footer_eof //--> 
+</div> 
+</body>
+</html>
+<?php 
+# For Guest - LogOff
+if($guestchk == '1') {
+  tep_session_unregister('customer_id');
+  tep_session_unregister('customer_default_address_id');
+  tep_session_unregister('customer_first_name');
+  tep_session_unregister('customer_last_name'); //Add Japanese osCommerce
+  tep_session_unregister('customer_country_id');
+  tep_session_unregister('customer_zone_id');
+  tep_session_unregister('comments');
+  tep_session_unregister('guestchk');
+
+  $cart->reset();  
+}
+
+require(DIR_WS_INCLUDES . 'application_bottom.php'); 
+?>
---
+++ rg/includes/boxes/reviews.php
@@ -0,0 +1,139 @@
+<?php
+/*
+  $Id$
+*/
+if (
+  basename($PHP_SELF) != FILENAME_CHECKOUT_PRODUCTS
+  && basename($PHP_SELF) != FILENAME_CHECKOUT_SHIPPING
+  && basename($PHP_SELF) != FILENAME_CHECKOUT_PAYMENT
+  && basename($PHP_SELF) != FILENAME_CHECKOUT_CONFIRMATION
+  && basename($PHP_SELF) != FILENAME_CHECKOUT_SUCCESS
+  && basename($PHP_SELF) != FILENAME_SHOPPING_CART
+  && basename($PHP_SELF) != FILENAME_LOGIN
+) {
+?>
+<!-- reviews //-->
+<?php
+  if(basename($PHP_SELF) == FILENAME_PRODUCT_INFO){
+    // ccdd
+    $reviews_query = tep_db_query("
+        select rd.reviews_text, 
+               r.reviews_rating, 
+               r.reviews_id, 
+               r.products_id, 
+               r.customers_name, 
+               r.date_added, 
+               r.last_modified, 
+               r.reviews_read 
+        from " .  TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd 
+        where r.reviews_id = rd.reviews_id 
+          and r.products_id = '" .  (int)$_GET['products_id'] . "' 
+          and r.reviews_status = '1' 
+          and  r.products_id not in".tep_not_in_disabled_products()." 
+          and r.site_id = ".SITE_ID
+        );
+    if(tep_db_num_rows($reviews_query)) {
+      echo  '<div class="pageHeading_long"><img align="top" src="images/menu_ico.gif" alt=""><span>'.$product_info['products_name'] .'のレビュー</span></div>'."\n" . '<div class="comment_long">'."\n" ;
+      while ($reviews = tep_db_fetch_array($reviews_query)) {
+        echo '<div class="reviews_area"><p class="main">
+<b>' . sprintf(TEXT_REVIEW_BY, tep_output_string_protected($reviews['customers_name'])) . '</b>&nbsp;&nbsp;' . tep_image(DIR_WS_IMAGES . 'stars_' . $reviews['reviews_rating'] . '.gif' , sprintf(BOX_REVIEWS_TEXT_OF_5_STARS, $reviews['reviews_rating'])) . '[' . sprintf(BOX_REVIEWS_TEXT_OF_5_STARS, $reviews['reviews_rating']) . ']
+<br>' . nl2br($reviews['reviews_text']) . "\n" . '</p></div>';
+//<div align="right"><i>' . sprintf(TEXT_REVIEW_DATE_ADDED, tep_date_long($reviews['date_added'])) . '</i></div></div>' . "\n";
+      }
+      //if(MAX_RANDOM_SELECT_REVIEWS > tep_db_num_rows($reviews_query)){
+      //  echo '<div align="right"><a href="'tep_href_link(FILENAME_PRODUCT_REVIEWS,'products_id='.(int)$_GET['products_id']).'">レビュー一覧へ</a></div>' ;
+      //}  
+      echo '</div>' . "\n";
+   } 
+} else {
+    if (isset($_GET['cPath']) && $cPath_array) {
+      $subcid = tep_get_categories_id_by_parent_id($cPath_array[count($cPath_array) - 1]);
+    }
+?>
+  <div class="reviews_box">
+  <div class="menu_top">
+  <a href="<?php echo tep_href_link(FILENAME_REVIEWS); ?>"><img src="images/menu_ico10.gif" alt="" align="top"><span>レビュー</span>
+  <?php //echo tep_image(DIR_WS_IMAGES.'design/box/reviews.gif',BOX_HEADING_REVIEWS,171,44); ?></a>
+  </div>
+    <div class="reviews_warp" align="center">
+    <?php
+  $random_select = "
+  select *
+  from (
+    select r.reviews_id, 
+           r.reviews_rating, 
+           p.products_id, 
+           p.products_image, 
+           pd.products_name,
+           r.site_id as rsid,
+           pd.site_id as psid
+    from " . TABLE_REVIEWS . " r, " .  TABLE_REVIEWS_DESCRIPTION . " rd, " . TABLE_PRODUCTS . " p, " .  TABLE_PRODUCTS_DESCRIPTION . " pd
+    ";
+    if (isset($subcid) && $subcid) {
+        $random_select .= (", " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c");
+    }
+    $random_select .= "
+    where p.products_status != '0' 
+      and p.products_id = r.products_id 
+      and r.reviews_id = rd.reviews_id 
+      and rd.languages_id = '" . $languages_id . "' 
+      and p.products_id = pd.products_id 
+      and pd.language_id = '" . $languages_id . "' 
+      and r.reviews_status = '1' 
+      and r.site_id = '".SITE_ID."'";
+  if (isset($subcid) && $subcid) {
+    $random_select .= "and p.products_id = p2c.products_id and p2c.categories_id in (".implode(',',$subcid).") ";
+  }
+  if (isset($_GET['products_id'])) {
+    $random_select .= " and p.products_id = '" . (int)$_GET['products_id'] . "'";
+  }
+  $random_select .= "
+    order by reviews_id, psid DESC
+  ) p
+  where psid = '0'
+     or psid = '".SITE_ID."'
+  group by reviews_id
+  ";
+  $random_select .= " order by reviews_id desc";
+  $random_product = tep_random_select($random_select);
+
+  $info_box_contents = array();
+
+  if ($random_product) {
+// display random review box
+    // ccdd
+    $review_query = tep_db_query("
+        select substring(reviews_text, 1, 60) as reviews_text 
+        from " . TABLE_REVIEWS_DESCRIPTION . " 
+        where reviews_id = '" . $random_product['reviews_id'] . "' 
+          and languages_id = '" . $languages_id . "'
+    ");
+    $review = tep_db_fetch_array($review_query);
+
+    $review = htmlspecialchars($review['reviews_text']);
+    $review = tep_break_string($review, 15, '-<br>');
+
+    echo '<p class="reviews_top"><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id']) . '" class="reviews_img">' . tep_image(DIR_WS_IMAGES . 'products/' . $random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br>'. tep_image(DIR_WS_IMAGES . 'stars_' . $random_product['reviews_rating'] . '.gif' , sprintf(BOX_REVIEWS_TEXT_OF_5_STARS, $random_product['reviews_rating']), 88, 16) . "\n".'</p>
+    <p class="reviews_bottom"><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id']) . '">' . $review . ' ...</a></p>'; 
+  } elseif (isset($_GET['products_id'])) {
+// display 'write a review' box
+    echo '<table border="0" cellspacing="2" cellpadding="2" width="100%">
+      <tr><td class="boxText">
+        <a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_WRITE, 'products_id=' . $_GET['products_id']) . '">' . tep_image(DIR_WS_IMAGES . 'box_write_review.gif', IMAGE_BUTTON_WRITE_REVIEW) . '</a>
+      </td><td class="boxText">
+        <a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_WRITE, 'products_id=' . $_GET['products_id']) . '">' . BOX_REVIEWS_WRITE_REVIEW .'</a>
+      </td></tr>
+    </table>' . "\n";
+  } else {
+// display 'no reviews' box
+    echo BOX_REVIEWS_NO_REVIEWS;
+  }
+?>
+</div>
+    </div>
+<!-- reviews_eof //-->
+<?php
+  }
+}
+?>
+
