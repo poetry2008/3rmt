24dbddb44459b59bdb847ee7f72f95981a8c6db1
maker <makerwang@gmail.com>
2011-02-08 17:15:24 +0800

    1713

--- jp/advanced_search.php
+++ jp/advanced_search.php
@@ -146,7 +146,8 @@ function popupWindow(url) {
             </tr> 
             <tr> 
               <td><table border="0" width="100%" cellspacing="0" cellpadding="2"> 
-                  <tr> 
+                  <tr>
+                  	<td class="smallText"><?php echo TEXT_SEARCH_HELP_LINK ?></td>
                     <td class="smallText" align="right"><?php echo tep_image_submit('button_search.gif', IMAGE_BUTTON_SEARCH); ?></td> 
                   </tr> 
                 </table></td> 
