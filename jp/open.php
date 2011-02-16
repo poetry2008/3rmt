31c7f9d9943c64bc19c4f9392d8b39b7979536ad
szn <sznforwork@gmail.com>
2011-02-16 17:35:58 +0800

    change ost by szn

--- jp/open.php
+++ jp/open.php
@@ -26,20 +26,20 @@ if($_POST):
     //$_POST['deptId']=$_POST['emailId']=0; //Just Making sure we don't accept crap...only topicId is expected.
     $_POST['emailId']=0; //Just Making sure we don't accept crap...only topicId is expected.
     if(!$thisuser && $cfg->enableCaptcha()){
-       if(!(isset($thisclient)&&$thisclient->isValid())){ 
         if(!$_POST['captcha'])
             $errors['captcha']='認証コードを入力してください';
         elseif(strcmp($_SESSION['captcha'],md5(strtolower($_POST['captcha']))))
             $errors['captcha']='認証コードを入力しなおしてください!';
-       }
     }
     //Ticket::create...checks for errors..
     if(($ticket=Ticket::create($_POST,$errors,SOURCE))){
               $msg='お客様のご質問は RMTジャックポット へ送信されました。';
       //       $msg = 'お客様のご質問は RMTワールドマネー へ送信されました。 ';
+      /*
         if($thisclient && $thisclient->isValid()) //Logged in...simply view the newly created ticket.
             @header('Location: tickets.php?id='.$ticket->getExtId());
         //Thank the user and promise speedy resolution!
+      */
         $inc='thankyou.inc.php';
     }else{
         $errors['err']=$errors['err']?$errors['err']:'必要な情報を正しくご入力ください';
