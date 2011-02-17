89aa312929cfc6d0de7c9895110f125f3133bf3d
maker <makerwang@gmail.com>
2011-02-17 19:06:11 +0800

    orders

--- jp/admin/orders.php
+++ jp/admin/orders.php
@@ -217,7 +217,7 @@
         ),$comments
         );
         
-        if (tep_is_oroshi($check_status['customers_id'])) {
+        if (!tep_is_oroshi($check_status['customers_id'])) {
           tep_mail($check_status['customers_name'], $check_status['customers_email_address'], $title, $comments, get_configuration_by_site_id('STORE_OWNER', $site_id), get_configuration_by_site_id('STORE_OWNER_EMAIL_ADDRESS', $site_id), $site_id);
         } 
         tep_mail(get_configuration_by_site_id('STORE_OWNER', $site_id), get_configuration_by_site_id('SENTMAIL_ADDRESS', $site_id), '送信済：'.$title, $comments, $check_status['customers_name'], $check_status['customers_email_address'], $site_id);
