86790cbf0759a6a5c4cc361925f06aa89b21828f
maker <makerwang@gmail.com>
2011-02-25 18:28:39 +0800

    geshu

--- jp/admin/edit_new_orders.php
+++ jp/admin/edit_new_orders.php
@@ -806,6 +806,41 @@ if ($order->info['payment_method'] === 'クレジットカード決済') {
       
       // 2.2.1 Update inventory Quantity
       $p = tep_db_fetch_array(tep_db_query("select * from products where products_id='".$add_product_products_id."'"));
+      if ((int)$add_product_quantity > $p['products_real_quantity']) {
+        // 买取商品大于实数
+        tep_db_perform('products',array(
+          'products_quantity' => $p['products_quantity'] - (int)$add_product_quantity,
+          'products_real_quantity' => 0,
+          'products_virtual_quantity' => $p['products_virtual_quantity'] - ((int)$add_product_quantity - $p['products_real_quantity'])
+        ),
+        'update',
+        "products_id = '" . $add_product_products_id . "'");
+        // 同步架空
+        tep_db_perform(
+          'set_menu_list',
+          array(
+            'kakuukosuu' => $p['products_virtual_quantity'] - ((int)$add_product_quantity - $p['products_real_quantity'])
+          ),
+          'update',
+          "products_id = '" . $add_product_products_id . "'"
+        );
+      } else {
+        tep_db_perform('products',array(
+          'products_quantity' => $p['products_quantity'] - (int)$add_product_quantity,
+          'products_real_quantity' => $p['products_virtual_quantity'] - (int)$add_product_quantity
+        ),
+        'update',
+        "products_id = '" . $add_product_products_id . "'");
+      }
+      // 增加销售量
+      tep_db_query("update " . TABLE_PRODUCTS . " set products_ordered = products_ordered + " . (int)$add_product_quantity . " where products_id = '" . $add_product_products_id . "'");
+      // 处理负数问题
+      tep_db_query("update " . TABLE_PRODUCTS . " set products_quantity = 0 where products_quantity < 0 and products_id = '" . $add_product_products_id . "'");
+      tep_db_query("update " . TABLE_PRODUCTS . " set products_real_quantity = 0 where products_real_quantity < 0 and products_id = '" . $add_product_products_id . "'");
+      tep_db_query("update " . TABLE_PRODUCTS . " set products_virtual_quantity = 0 where products_virtual_quantity < 0 and products_id = '" . $add_product_products_id . "'");
+      
+      /*
+      $p = tep_db_fetch_array(tep_db_query("select * from products where products_id='".$add_product_products_id."'"));
       $p_quantity  = $p['products_quantity'];
       $pr_quantity = $p['products_real_quantity'];
       if ($pr_quantity - (int)$add_product_quantity < 0) {
@@ -816,7 +851,7 @@ if ($order->info['payment_method'] === 'クレジットカード決済') {
       }
       tep_db_query("update " . TABLE_PRODUCTS . " set products_quantity = '".$p_quantity."', products_real_quantity = '".$pr_quantity."', products_ordered = products_ordered + " . (int)$add_product_quantity . " where products_id = '" . $add_product_products_id . "'");
       tep_db_query("update " . TABLE_PRODUCTS . " set products_quantity = 0 where products_quantity < 0 and products_id = '" . $add_product_products_id . "'");
-
+      */
       if (IsSet($add_product_options)) {
         foreach($add_product_options as $option_id => $option_value_id) {
           $Query = "insert into " . TABLE_ORDERS_PRODUCTS_ATTRIBUTES . " set
