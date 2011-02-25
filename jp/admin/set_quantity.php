208e4cd0cf21e7050b8dc5cd1837c0f98062cb07
maker <makerwang@gmail.com>
2011-02-25 16:24:02 +0800

    geshu

--- jp/admin/set_quantity.php
+++ jp/admin/set_quantity.php
@@ -9,4 +9,4 @@ if ($pID && trim($_GET['quantity']) !== '') {
 }
 
 $product = tep_db_fetch_array(tep_db_query("select * from products where products_id='".$pID."'"));
-echo $product['products_quantity'];
+echo $product['products_real_quantity'];
