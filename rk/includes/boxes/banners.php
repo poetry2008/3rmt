588816e97c61067b52fb10f737613375f2f6513a
maker <makerwang@gmail.com>
2011-03-11 15:27:36 +0800

    klk

--- rk/includes/boxes/banners.php
+++ rk/includes/boxes/banners.php
@@ -5,7 +5,7 @@
 ?>
 <!--left_banner -->
 <?php
-  if ($banner = tep_banner_exists('dynamic', 'left1')) { echo '<div class="link_banner">'.tep_display_banner('static', $banner, 170, 328).'</div>'."\n"; } 
+  if ($banner = tep_banner_exists('dynamic', 'left1')) { echo '<div class="link_banner">'.tep_display_banner('static', $banner).'</div>'."\n"; } 
   //if ($banner = tep_banner_exists('dynamic', 'left2')) { echo '<div class="link_banner">'.tep_display_banner('static', $banner, 170, 79).'</div>'."\n"; }
   //if ($banner = tep_banner_exists('dynamic', 'left3')) { echo '<div class="link_banner">'.tep_display_banner('static', $banner, 170, 79).'</div>'."\n"; }
   if ($banner = tep_banner_exists('dynamic', 'left4')) { echo '<div class="link_banner">'.tep_display_banner('static', $banner).'</div>'."\n"; }
