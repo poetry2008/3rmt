1fa8633527024184b5b7328a1450d443ae955362
szn <sznforwork@gmail.com>
2011-03-14 18:17:10 +0800

    change to 手続失敗

--- jp/includes/languages/japanese/checkout_unsuccess.php
+++ jp/includes/languages/japanese/checkout_unsuccess.php
@@ -11,6 +11,6 @@
 */
 
 define('NAVBAR_TITLE_1', 'レジ');
-define('NAVBAR_TITLE_2', '手続完了');
+define('NAVBAR_TITLE_2', '<font color="red">手続失敗</font>');
 define('TEXT_PAY_UNSUCCESS','申し訳ございませんが、支払方法選択画面に戻り<br />再度注文をしてください。');
 ?>
