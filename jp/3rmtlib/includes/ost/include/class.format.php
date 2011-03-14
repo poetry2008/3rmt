539cd4f507e6c8479d9a4ceb749a49f2a56a0fd8
gc <gc@example.com>
2011-03-14 17:16:47 +0800

    git commit by szn change gc

--- jp/3rmtlib/includes/ost/include/class.format.php
+++ jp/3rmtlib/includes/ost/include/class.format.php
@@ -1,3 +1,4 @@
+
 <?php
 /*********************************************************************
     class.format.php
@@ -79,14 +80,18 @@ class Format {
     //Format text for display..
     function display($text) {
         global $cfg;
+        
 
         $text=Format::htmlchars($text); //take care of html special chars
         if($cfg && $cfg->clickableURLS() && $text)
-            $text=Format::clickableurls($text);
-
-        //Wrap long words...
+         //Wrap long words...
         $text =preg_replace_callback('/\w{75,}/',create_function('$matches','return wordwrap($matches[0],70,"\n",true);'),$text);
-
+           $text=Format::clickableurls($text);
+         $str1='㈱';
+$num=stripos($text,$str1);
+if($num) {
+  $text=substr_replace($text,"&#12849;",$num,3);
+ }
         return nl2br($text);
     }
 
