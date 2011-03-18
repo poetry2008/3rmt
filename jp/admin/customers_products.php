c7f4250f17a38850911fc93b94064bac66ad06ac
maker <makerwang@gmail.com>
2011-03-18 18:04:22 +0800

    式样书

--- jp/admin/customers_products.php
+++ jp/admin/customers_products.php
@@ -51,10 +51,10 @@
     <td>
       <table border="0" width="195" class="print_innput">
       	<tr><td height="10"></td></tr>
-        <tr><td height="23" valign="top" align="right"><input name="textfield" type="text" id="textfield" value="2009年7月7日星期二" style="height:18px; width:150px; font-size:12px; margin-right:5px;"></td></tr>
+        <tr><td height="23" valign="top" align="right"><input name="textfield" type="text" id="textfield" value="2009年7月7日星期二" style="height:18px; width:150px; text-align:right; font-size:12px; margin-right:5px;"></td></tr>
         <tr><td width="31"></td></tr>
-        <tr><td align="right" height="44" valign="bottom"><font size="2"><textarea type="text" rows="6" value="カ)アールエムティエイチアイ" ></textarea></font></td></tr>
-        <tr><td align="right" height="19" valign="bottom"><font size="1"><a href="#"><input name="textfield" type="text" id="textfield" value="takahasi.tetuya@live.jp" style=" height:18px; width:190px; font-size:12px; margin-right:5px;"></a></font></td></tr>
+        <tr><td align="right" height="44" valign="bottom"><font size="2"><textarea type="text" rows="3" value="カ)アールエムティエイチアイ" ></textarea></font></td></tr>
+        <tr><td align="right" height="19" valign="bottom"><font size="1"><a href="#"><input name="textfield" type="text" id="textfield" value="takahasi.tetuya@live.jp" style=" height:18px; width:190px; text-align:right; font-size:12px; margin-right:5px;"></a></font></td></tr>
         <tr><td width="19"></td></tr>
         <tr><td align="right" colspan="4" height="163">
           <table width="30" style="border:#666666 1px solid;">
