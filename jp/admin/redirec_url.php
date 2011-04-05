76b6f2b78ec7223389989d0b1ebfbac30ab84ad7
szn <sznforwork@gmail.com>
2011-04-05 18:14:56 +0800

    change redirec url

--- jp/admin/redirec_url.php
+++ jp/admin/redirec_url.php
@@ -1,7 +1,7 @@
 <?php
   require('includes/application_top.php');
   if(isset($_GET['url'])&&$_GET['url']){
-    echo "<a target='_blank' href='".urldecode($_GET['url'])."'>";
+    echo "<a href='".urldecode($_GET['url'])."'>";
     echo urldecode($_GET['url']);
     echo "</a>";
   }
