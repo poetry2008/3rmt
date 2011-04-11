dbee4fdcfebe6f4cc1bac3f9d5e0f3f00e2b2f26
szn <sznforwork@gmail.com>
2011-04-11 10:16:55 +0800

    change ff link

--- ff/link/APP/Model/Submit.php
+++ ff/link/APP/Model/Submit.php
@@ -6,7 +6,7 @@ class Model_Submit extends FLEA_Db_TableDataGateway
     parent::FLEA_Db_TableDataGateway();
     $this->disableLinks();
   }
-  var $tableName = 'rk_submit';
+  var $tableName = 'ff_submit';
   var $primaryKey = 'id';
 
     /**
