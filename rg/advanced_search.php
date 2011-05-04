75476ae1912be0e30bd4e25515c1f44f3ae4d7c5
szn <sznforwork@gmail.com>
2011-05-04 15:01:15 +0800

    klk

--- rg/advanced_search.php
+++ rg/advanced_search.php
@@ -258,7 +258,6 @@ function popupWindow(url) {
   <?php require(DIR_WS_INCLUDES . 'footer.php'); ?> 
   <!-- footer_eof //--> 
 </div> 
-</div> 
 </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
--- rg/logoff.php
+++ rg/logoff.php
@@ -63,7 +63,6 @@
   <!-- footer //--> 
   <?php require(DIR_WS_INCLUDES . 'footer.php'); ?> 
   <!-- footer_eof //--> 
-</div> 
 </div>
 </body>
 </html>
