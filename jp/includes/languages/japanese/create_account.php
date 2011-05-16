d4e9d8cf8b9b63244ea6c6eb55fd4b25a381a8d9
szn <sznforwork@gmail.com>
2011-05-16 15:46:16 +0800

    change languages for jp

--- jp/includes/languages/japanese/create_account.php
+++ jp/includes/languages/japanese/create_account.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE', '会員の作成');
+define('NAVBAR_TITLE', '会員登録');
 define('HEADING_TITLE', 'お客様情報');
 define('TEXT_ORIGIN_LOGIN', '<font color="#FF0000"><small><b>ご注意:</b></font></small> 以前に会員を作成されている方は、<a href="%s"><u>ログインページ</u></a>からログインしてください。');
 ?>
