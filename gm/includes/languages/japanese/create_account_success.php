639ad71ae74c14d82610c08cf252e22ab2ceb673
szn <sznforwork@gmail.com>
2011-05-17 14:57:09 +0800

    change language by tcz

--- gm/includes/languages/japanese/create_account_success.php
+++ gm/includes/languages/japanese/create_account_success.php
@@ -10,8 +10,8 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE_1', 'アカウントの作成');
+define('NAVBAR_TITLE_1', '会員登録');
 define('NAVBAR_TITLE_2', '完了');
-define('HEADING_TITLE', 'あなたのアカウントが作成されました!');
+define('HEADING_TITLE', 'あなたの会員が作成されました!');
 define('TEXT_ACCOUNT_CREATED', '<b>『次へ進む』をクリックしてください。</b><br><br>ありがとうございます。これでお客様のアカウント作成は完了です！<br>これから' . STORE_NAME . 'でのオンラインショッピングを、思う存分お楽しみください。もし' . STORE_NAME . '内の操作他でご不明な点がございましたら、お問い合わせページよりお気軽にお問い合わせください。<br><br>また、ご登録いただきましたメールアドレスに、登録ご確認メールをお送りいたしました。もし10分以内に届かないようでしたら、ご連絡をお願いいたします。');
 ?>
