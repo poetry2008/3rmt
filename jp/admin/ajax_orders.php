521a4d1a92af10880a665a6dd233bb2019123275
kthiz <kthiz2006@gmail.com>
2011-05-18 11:26:56 +0800

    price edit

--- jp/admin/ajax_orders.php
+++ jp/admin/ajax_orders.php
@@ -126,9 +126,9 @@ if ($_POST['orders_id'] && $_POST['orders_comment']) {
     </td>
     <td style="border-bottom:1px solid #000000;background-color: darkred;" class="dataTableContent" align="right" onClick="chg_td_color(<?php echo $orders['orders_id']; ?>)">
       <?php if (!$ocertify->npermission && (time() - strtotime($orders['date_purchased']) > 86400*7)) {?>
-      <font color="#999"><?php echo strip_tags(tep_get_ot_total_by_orders_id($orders['orders_id']));?></font>
+      <font color="#999"><?php echo strip_tags(tep_get_ot_total_by_orders_id($orders['orders_id'], true));?></font>
       <?php } else { ?>
-      <?php echo strip_tags(tep_get_ot_total_by_orders_id($orders['orders_id']));?>
+      <?php echo strip_tags(tep_get_ot_total_by_orders_id($orders['orders_id'], true));?>
       <?php }?>
     </td>
     <td style="border-bottom:1px solid #000000;background-color: darkred;" class="dataTableContent" align="right" onClick="chg_td_color(<?php echo $orders['orders_id']; ?>)"><?php echo $next_mark; ?><font color="<?php echo !$ocertify->npermission && (time() - strtotime($orders['date_purchased']) > 86400*7)?'#999':$today_color; ?>"><?php echo tep_datetime_short($orders['torihiki_date']); ?></font></td>
