3334e56a5740d4918cd4db8fcefaf5f28f4ca79a
szn <sznforwork@gmail.com>
2011-05-18 17:12:00 +0800

    change for languages

--- rr/includes/languages/japanese/create_account.php
+++ rr/includes/languages/japanese/create_account.php
@@ -12,5 +12,5 @@
 
 define('NAVBAR_TITLE', '会員登録');
 define('HEADING_TITLE', 'お客様情報');
-define('TEXT_ORIGIN_LOGIN', '<font color="#FF0000" size="1" ><b>ご注意:</b></font> 以前に会員を作成されている方は、<a href="%s"><font color="#cccccc"> <u>ログインページ</u></font></a>からログインしてください。');
+define('TEXT_ORIGIN_LOGIN', '<font color="#FF0000" size="1" ><b>ご注意:</b></font> 以前に会員登録されている方は、<a href="%s"><font color="#cccccc"> <u>ログインページ</u></font></a>からログインしてください。');
 ?>
