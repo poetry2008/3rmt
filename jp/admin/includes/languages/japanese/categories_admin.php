6e56b2e0a9559b75856d431be96b8e70ddf84906
kthiz <kthiz2006@gmail.com>
2011-05-19 14:30:05 +0800

    admin qiehuan yuyan edit

---
+++ jp/admin/includes/languages/japanese/categories_admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $Id$
+*/
+ 
+define('CATEGORY_ADMIN_TITLE', '商品卸価格管理');
+define('CATEGORY_ADMIN_CATREE_TITLE', 'ジャンプ▼');
+define('CATEGORY_ADMIN_SINGLE_PRICE', '単価ルール:');
+define('TABLE_HEADING_CATEGORIES_ZHUWEN_NUM', '注文数');
+define('TABLE_HEADING_CATEGORIES_JIAKONG', '架空在庫');
+define('TABLE_HEADING_CATEGORIES_SHIZAIKU', '実在庫');
+define('TABLE_HEADING_CATEGORIES_YEZHE', '業者');
+define('CATEGORY_ADMIN_BEI_TEXT', '倍');
+define('CATEGORY_ADMIN_SETTING_TONGYE', '同業者未設定');
+define('TABLE_HEADING_CATEGORIES_NOW_PRICE', '現在単価');
+define('TABLE_HEADING_CATEGORIES_SETTING_PRICE', '単価設定');
+define('TABLE_HEADING_CATEGORIES_CHAE', '増減');
+define('PIC_MAE_ALT_TEXT', '日前に注文有');
+define('CATEGORY_ADMIN_BUTTON_CAL_SETTING', '計算設定');
+define('CATEGORY_ADMIN_BUTTON_LOGIN', '担当者登録');
+define('CATEGORY_ADMIN_BUTTON_XIEYE_PRICE', '卸業者単価設定');
+define('CATEGORY_ADMIN_BUTTON_ALL_UPDATE', '一括更新');
+define('CATEGORY_ADMIN_COUNTNUM_TEXT', 'カテゴリー:');
+define('CATEGORY_ADMIN_PRODUCT_NUM', '商品数:');
+define('CATEGORY_ADMIN_LAST_MODIFIED_TEXT', '更新日の日付');
+define('CATEGORY_ADMIN_AUTHOR_TEXT', '担当者:');
+define('CATEGORY_ADMIN_COMMENT_TEXT', 'コメント:');
