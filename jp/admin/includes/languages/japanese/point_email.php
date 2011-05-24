78c39b00a7c8f2ea9f7fdc62d36de4dec82f0b16
szn <sznforwork@gmail.com>
2011-05-24 14:17:29 +0800

    point mail language

---
+++ jp/admin/includes/languages/japanese/point_email.php
@@ -0,0 +1,13 @@
+<?php
+define('TEXT_POINT_EMAIL_GLOBAL_TEXT',
+    '${NAME}:ユーザー名<br>${POINT}:ポイント<br>${POINT_DATE}:期限<br>${SITE_NAME}:サイト名');
+define('TABLE_HEADING_MAIL_DATE','期限');
+define('TEXT_INFO_POINT_MAIL_DATE','期限');
+define('TABLE_HEADING_DESCRIPTION','メール内容');
+define('TEXT_INFO_POINT_DESCRIPTION','メール内容');
+define('TEXT_INFO_HEADING_DELETE','テンプレート削除');
+define('TEXT_INFO_DELETE','削除しますか？');
+define('HEADING_TITLE','ポイントお知らせメール');
+define('TEXT_INFO_HEADING_EDIT_POINT_MAIL ','テンプレート編集');
+define('TEXT_INFO_HEADING_NEW','テンプレート新規作成');
+define('TEXT_INFO_HEADING_EDIT_POINT_MAIL','メールのテンプレート');
