b26550e43923a71c5d5d8fb029e2317f023e39dc
szn <sznforwork@gmail.com>
2011-05-25 13:54:44 +0800

    test send mail

--- jp/admin/send_mail.php
+++ jp/admin/send_mail.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php 
 set_time_limit(0);
-//define('ROOT_DIR','/home/.sites/22/site13/vhosts/jp/admin');
-define('ROOT_DIR','/home/szn/project/3rmt/jp/admin');
+define('ROOT_DIR','/home/.sites/22/site13/vhosts/jp/admin');
+//define('ROOT_DIR','/home/szn/project/3rmt/jp/admin');
 require(ROOT_DIR.'/includes/configure.php');
 
 
