55f4d1c7e12bac4c6f752a72e71e300b910d9bfd
kthiz <kthiz2006@gmail.com>
2011-05-30 09:57:51 +0800

    rg zhuce liucheng edit

--- rg/includes/languages/japanese/guest_info.php
+++ rg/includes/languages/japanese/guest_info.php
@@ -9,7 +9,7 @@
 
   Released under the GNU General Public License
 */
-define('NAVBAR_TITLE', 'ゲストは登録を成功しました');
-define('HEADING_TITLE', 'ゲストは登録を成功しました');
-define('GUEST_SUCCESS_INFO_COMMENT', '72時間以内にメールにあるURLをクリックして下さい');
+define('NAVBAR_TITLE', 'メール認証手続き');
+define('HEADING_TITLE', 'メール送信完了');
+define('GUEST_SUCCESS_INFO_COMMENT', STORE_NAME.'をご利用いただくには、メール認証を行う必要があります。<br>会員登録するお客様は、メール内に記載してありますURLをクリックしていただけますと、会員登録完了となります <br>会員登録をしないお客様は、メール内にあるURLをクリックしていただいてから引き続き '.STORE_NAME.'をご利用くださいませ。 <br>正常に受信できる場合：「送信」後5分以内でRMT学園からの確認メールが届きます。 <br>受信できない場合：「送信」後5分以上経過しても受信できない場合は、スパムフィルター等で受け取りを拒否されている可能性が高いです。 <br>メール受信の設定に関しては、 「フリーメールでメールが受け取れない方へ」 をご参考ください。');
 ?>
