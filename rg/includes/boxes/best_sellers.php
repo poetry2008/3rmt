dca6f640f4fee18d633afe31f8e9837a06767e1c
kthiz <kthiz2006@gmail.com>
2011-06-02 16:51:15 +0800

    rg best sellers edit

--- rg/includes/boxes/best_sellers.php
+++ rg/includes/boxes/best_sellers.php
@@ -7,8 +7,6 @@
     // ccdd
     // del distinct 10.8.2
     $best_sellers_query = tep_db_query("
-      select *
-      from (
         select p.products_id,
                         p.products_image,
                         p.products_ordered,
@@ -17,30 +15,21 @@
                         pd.products_description,
                         pd.site_id
         from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION .  " pd, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c, " .  TABLE_CATEGORIES . " c 
-        where (pd.site_id = '0'
-          or pd.site_id = '".SITE_ID."' )
+        where pd.site_id = '".SITE_ID."'
           and p.products_ordered > 0 
           and p.products_id = pd.products_id 
           and pd.language_id = '" . $languages_id . "' 
           and p.products_id = p2c.products_id 
           and p2c.categories_id = c.categories_id 
           and '" .  $current_category_id . "' in (c.categories_id, c.parent_id) 
-        order by pd.site_id DESC
-        limit 100
-        ) p
-      where site_id = '0'
-         or site_id = '".SITE_ID."' 
-      group by products_id
-      having p.products_status != '0' and p.products_status != '3' 
-      order by products_ordered desc, products_name 
+          and pd.products_status != '0' and pd.products_status != '3' 
+          order by p.products_ordered desc, pd.products_name 
       limit " . MAX_DISPLAY_BESTSELLERS);
     
   } else {
     // ccdd
     // del distinct 10.8.2
     $best_sellers_query = tep_db_query("
-      select *
-      from (
         select p.products_id,
                         p.products_image,
                         p.products_ordered,
@@ -49,20 +38,13 @@
                         pd.products_description,
                         pd.site_id
         from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION .  " pd 
-        where (pd.site_id = '0'
-          or pd.site_id = '".SITE_ID."' )
+        where pd.site_id = '".SITE_ID."'
           and p.products_ordered > 0 
           and p.products_id = pd.products_id 
           and pd.language_id = '" .  $languages_id . "' 
           and p.products_id not in".tep_not_in_disabled_products()." 
-        order by pd.site_id DESC
-        limit 100
-      ) p
-      where site_id = '0'
-         or site_id = '".SITE_ID."' 
-      group by products_id
-      having p.products_status != '0' and p.products_status != '3' 
-      order by products_ordered desc, products_name 
+          and pd.products_status != '0' and pd.products_status != '3' 
+        order by p.products_ordered desc, pd.products_name 
       limit " . MAX_DISPLAY_BESTSELLERS
         );
   }
