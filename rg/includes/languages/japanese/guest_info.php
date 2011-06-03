613a6a1c1032db619ebf4459b5e9ecc6f3d29371
kthiz <kthiz2006@gmail.com>
2011-06-03 11:22:23 +0800

    rg zhuce xiugai

--- rg/includes/languages/japanese/guest_info.php
+++ rg/includes/languages/japanese/guest_info.php
@@ -12,6 +12,6 @@
 define('NAVBAR_TITLE', 'メール認証手続き');
 define('HEADING_TITLE', 'メール送信完了');
 define('HEADING_TITLE_FINISH', 'メール認証完了');
-define('GUEST_SUCCESS_INFO_COMMENT', '<font color="#ff0000">*メールが届かない場合は上記送信フォームより再度メールを送信してください</font><br><br>'.STORE_NAME.'をご利用いただくには、メール認証を行う必要があります。<br>メール内に記載してあるURLをクリックしメール認証を完了させてから'.STORE_NAME.'を利用することが出来ます。<br><br><br><b>正常に受信できる場合：</b>「送信」後5分以内で'.STORE_NAME.'からの確認メールが届きます。<br><b>受信できない場合：</b>「送信」後5分以上経過しても受信できない場合は、スパムフィルター等で受け取りを拒否されている可能性が高いです。<br><br><br>メール受信の設定に関しては、 「<a href="'.tep_href_link('email_trouble.php').'"><font color="#656565" size="2">フリーメールでメールが受け取れない方へ</font></a>」 をご参考ください。');
+define('GUEST_SUCCESS_INFO_COMMENT', '<font color="#ff0000">*メールが届かない場合は上記送信フォームより再度メールを送信してください</font><br><br>'.STORE_NAME.'をご利用いただくには、メール認証を行う必要があります。<br>メール内に記載してあるURLをクリックしメール認証を完了させてから'.STORE_NAME.'を利用することが出来ます。<br><br><br><b>正常に受信できる場合：</b>「送信」後5分以内で'.STORE_NAME.'からの確認メールが届きます。<br><b>受信できない場合：</b>「送信」後5分以上経過しても受信できない場合は、スパムフィルター等で受け取りを拒否されている可能性が高いです。<br><br><br>メール受信の設定に関しては、 「<a href="'.tep_href_link('email_trouble.php').'">フリーメールでメールが受け取れない方へ</a>」 をご参考ください。');
 define('CHECK_FINISH_TEXT', 'メール認証が完了いたしました。<br>「次へ進む」をクリックしてショッピングを続けてください。');
 ?>
