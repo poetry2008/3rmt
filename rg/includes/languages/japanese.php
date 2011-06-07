1a0af30752deb3140a46e1d3b6f7d8750bcf8967
kthiz <kthiz2006@gmail.com>
2011-06-07 15:10:36 +0800

    rg zhuce edit

--- rg/includes/languages/japanese.php
+++ rg/includes/languages/japanese.php
@@ -435,7 +435,7 @@ define('TEXT_TAGS', 'タグ一覧');
 define('COLUMNLEFT_SEARCH_DEFAULT_VALUE', '入力して下さい');
 define('COLUMNRIGHT_BEST_GOODS_TITLE', 'お勧めゲーム');
 define('ENTRY_GUEST_EMAIL_NOT_ACTIVE', '&nbsp;<small><font color="#FF0000">認証手続きはまだ完了していません。</font></small>');
-define('CHECK_EMAIL_EXISTS_ERROR', 'メールアドレスはすでに存在しています!');
+define('CHECK_EMAIL_EXISTS_ERROR', 'メールアドレスが無効です。お問い合わせください');
 define('SENDMAIL_SUCCESS_COMMENT_TEXT', '正常に受信できる場合：「送信」後5分以内でRMT学園からの確認メールが届きます。<br>受信できない場合：「送信」後5分以上経過しても受信できない場合は、スパムフィルター等で受け取りを拒否されている可能性が高いです。');
 define('ALREADY_SEND_MAIL_TEXT', 'メールアドレスが無効です。お問い合わせください');
 ?>
