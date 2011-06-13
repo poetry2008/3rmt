dcc83887cade8953469882aa10aaf5923561541a
szn <sznforwork@gmail.com>
2011-06-13 14:36:49 +0800

    change for nishimola

--- rr/includes/configure.php
+++ rr/includes/configure.php
@@ -7,7 +7,7 @@
 // * DIR_FS_* = Filesystem directories (local/physical)
 // * DIR_WS_* = Webserver directories (virtual/URL)
 //  define('HTTP_SERVER', 'http://3xy.szn.200.com'); // eg, http://localhost - should not be empty for productive servers
-  define('HTTP_SERVER', 'http://3rr.kthiz.200.com'); // eg, http://localhost - should not be empty for productive servers
+  define('HTTP_SERVER', 'http://3rr.szn.200.com'); // eg, http://localhost - should not be empty for productive servers
   define('HTTPS_SERVER', 'https://cchm.vicp.net'); // eg, https://localhost - should not be empty for productive servers
   define('URL_ROMAJI_ENABLED', true);
   define('ENABLE_SSL', false); // secure webserver for checkout procedure?
@@ -23,12 +23,12 @@
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
 
   define('DIR_WS_DOWNLOAD_PUBLIC', DIR_WS_CATALOG . 'pub/');
-  define('DIR_FS_DOCUMENT_ROOT', '/home/kthiz/project/OSC_3RMT/rr/');
+  define('DIR_FS_DOCUMENT_ROOT', '/home/szn/project/3rmt/rr/');
   define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT);
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
   
-  define('DIR_FS_3RMTLIB', '/home/kthiz/project/OSC_3RMT/jp/3rmtlib/');
+  define('DIR_FS_3RMTLIB', '/home/szn/project/3rmt/jp/3rmtlib/');
 
 // define our database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
@@ -42,7 +42,7 @@
   define('REWRITE_CATEGORIES', 'rmt/');//Add Ultimate_SEO_URLS
   define('REWRITE_MANUFACTURERES', 'game/');//Add Ultimate_SEO_URLS
 
-  define('SITE_ID', '8'); 
+  define('SITE_ID', '7'); 
   //control sql_log
   define('SQL_LOG', false);
   ini_set('include_path',ini_get('include_path').':'.DIR_FS_3RMTLIB);
