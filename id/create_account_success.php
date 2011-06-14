6939037a1fcda3b76419e36df8a8effa014d41bd
kthiz <kthiz2006@gmail.com>
2011-06-14 12:21:16 +0800

    id,rk,ff zhuce edit

--- id/create_account_success.php
+++ id/create_account_success.php
@@ -14,7 +14,7 @@
     $origin_href = tep_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], array(tep_session_name())), $navigation->snapshot['mode']);
     $navigation->clear_snapshot();
   } else {
-    $origin_href = HTTP_SERVER.'?'.tep_session_name().'='.tep_session_id();
+    $origin_href = tep_href_link(FILENAME_DEFAULT);
   }
 ?>
 <?php page_head();?>
