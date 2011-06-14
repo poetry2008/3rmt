2bb06d2b60209c26f5fa69a65ba9df89f8d22f9b
kthiz <kthiz2006@gmail.com>
2011-06-14 09:21:23 +0800

    id,rk,ff zhuce edit

--- id/ac_mail_finish.php
+++ id/ac_mail_finish.php
@@ -106,7 +106,7 @@
               <td><br> 
                 <table border="0" width="100%" cellspacing="0" cellpadding="0"> 
                   <tr> 
-                    <td class="main"><?php echo '<a href="' .  tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . tep_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td> 
+                    <td class="main" align="right"><?php echo '<a href="' .  tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td> 
                     <td align="right" class="main">
                     </td> 
                   </tr> 
