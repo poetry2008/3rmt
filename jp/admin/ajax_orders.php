b5ac6a6b3c54742c8ad25c06aef2b442496af57e
szn <sznforwork@gmail.com>
2011-06-20 16:30:05 +0800

    change for faq romaji

--- jp/admin/ajax_orders.php
+++ jp/admin/ajax_orders.php
@@ -422,4 +422,13 @@ if ($_POST['orders_id'] && $_POST['orders_comment']) {
   }else{
     echo 'false';
   }
+}else if(isset($_GET['action'])&&$_GET['action'] == 'check_romaji'){
+  $romaji = $_POST['romaji'];
+  if(preg_match('/[^\x{4e00}-\x{9fa5}\x{3130}-\x{318F}\x{0800}-\x{4e00}a-zA-Z0-9-]/u',$romaji)){
+  $new_romaji =
+    preg_replace('/[^\x{4e00}-\x{9fa5}\x{3130}-\x{318F}\x{0800}-\x{4e00}a-zA-Z0-9-]/u','-',$romaji);
+    echo $new_romaji;
+  }else{
+    echo '';
+  }
 }
