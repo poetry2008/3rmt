15e973df78f42441f3b44e7973d837c8d58e0200
kthiz <kthiz2006@gmail.com>
2011-06-22 19:07:35 +0800

    ff hanshu edit

--- ff/shopping_cart.php
+++ ff/shopping_cart.php
@@ -326,7 +326,7 @@ B:ポイントの加算（<?php echo STORE_NAME;?>会員でなければ表示さ
                   </tr>
                 </table>
 <?php 
-    $cat_cid_arr = tep_ff_get_categories_id_by_parent_id(FF_CID); 
+    $cat_cid_arr = tep_other_get_categories_id_by_parent_id(FF_CID); 
     if (empty($cat_cid_arr)) {
       $cat_cid_arr = array(FF_CID); 
     } else {
