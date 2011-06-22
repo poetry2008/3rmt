5fabde8a4797e7ba357f55897d203aee6a0e4955
szn <sznforwork@gmail.com>
2011-06-22 11:41:25 +0800

    change for zlq

--- id/product_notifications.php
+++ id/product_notifications.php
@@ -200,7 +200,7 @@
           </tr>
           </table>
           </form>
-          </td>
+        <!-- </td>
       </tr>
     </table>
     </div>
@@ -209,12 +209,12 @@
       <td valign="top" class="right_colum_border" width="<?php echo BOX_WIDTH; ?>"> <!-- right_navigation //--> 
         <?php require(DIR_WS_INCLUDES . 'column_right.php'); ?> 
         <!-- right_navigation_eof //--> </td> 
-  </table> 
+  </table>
   <!-- body_eof //--> 
   <!-- footer //--> 
   <?php require(DIR_WS_INCLUDES . 'footer.php'); ?> 
   <!-- footer_eof //--> 
-</div> 
+</div>  -->
 </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
