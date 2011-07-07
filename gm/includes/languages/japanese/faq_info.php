032ce9e1b26b5075a1f6ab667e3f7ab92bf073a0
szn <sznforwork@gmail.com>
2011-07-07 11:02:57 +0800

    change for gm faq

--- gm/includes/languages/japanese/faq_info.php
+++ gm/includes/languages/japanese/faq_info.php
@@ -3,3 +3,4 @@ define('TEXT_FAQ','よくあるご質問');
 define('TEXT_BACK','戻る');
 define('TEXT_QUESTION_TITLE','よくある質問');
 define('TEXT_FAQ_TITLE_END','お客様から寄せられる、よくある質問とその回答');
+define('TEXT_CATEGORY_TITLE','について質問一覧');
