5900bcb3a79f317646065e8d2c63a32bc9c83a55
kthiz <kthiz2006@gmail.com>
2011-07-25 16:27:16 +0800

    oainit edit

--- jp/admin/oainit.php
+++ jp/admin/oainit.php
@@ -225,7 +225,13 @@ function method_2($order,$form_id,$group_id,$item_id){
 }
 //q_15_2 checkbox 支払：
 function method_3($order,$form_id,$group_id,$item_id){
-  $value = oa_date($order['q_15_2']);
+  $order_status_query = tep_db_query("select * from orders_status_history where orders_id = '".$order['orders_id']."' and orders_status_id = '9' order by date_added desc limit 1"); 
+  $order_status_res =  tep_db_fetch_array($order_status_query);
+  if ($order_status_res) {
+    $value = date('Y-m-d h:i', strtotime($order_status_res['date_added']));   
+  } else {
+    $value = oa_date($order['q_15_2']);
+  } 
   oavalue($value,$form_id,$group_id,$item_id,$order['orders_id']);
 }
 ////q_16_2 checkbox 支払完了メール送信：  
@@ -235,7 +241,13 @@ function method_4($order,$form_id,$group_id,$item_id){
 }
 //q_5_2  date 発送  
 function method_5($order,$form_id,$group_id,$item_id){
-  $value = oa_date($order['q_5_2']);
+  $order_status_query = tep_db_query("select * from orders_status_history where orders_id = '".$order['orders_id']."' and orders_status_id = '9' order by date_added desc limit 1"); 
+  $order_status_res =  tep_db_fetch_array($order_status_query);
+  if ($order_status_res) {
+    $value = date('Y-m-d h:i', strtotime($order_status_res['date_added']));   
+  } else {
+    $value = oa_date($order['q_5_2']);
+  } 
   oavalue($value,$form_id,$group_id,$item_id,$order['orders_id']);
 }
 //q_7_2  checkbox 発送完了メール送信      如果是 1 则_0 如果是null 或 0 不删 空值  
@@ -282,7 +294,13 @@ function method_8($order,$form_id,$group_id,$item_id){
 }
 //q_13_2 date 受领注意
 function method_9($order,$form_id,$group_id,$item_id){
-  $value = oa_date($order['q_13_2']);
+  $order_status_query = tep_db_query("select * from orders_status_history where orders_id = '".$order['orders_id']."' and orders_status_id = '9' order by date_added desc limit 1"); 
+  $order_status_res =  tep_db_fetch_array($order_status_query);
+  if ($order_status_res) {
+    $value = date('Y-m-d h:i', strtotime($order_status_res['date_added']));   
+  } else {
+    $value = oa_date($order['q_13_2']);
+  } 
   oavalue($value,$form_id,$group_id,$item_id,$order['orders_id']);
 }
 //q_14_1 checkbox 受領メール送信
@@ -303,7 +321,13 @@ function method_12($order,$form_id,$group_id,$item_id){
 }
 //q_9_2  date 決算確認：
 function method_13($order,$form_id,$group_id,$item_id){
-  $value = oa_date($order['q_9_2']);
+  $order_status_query = tep_db_query("select * from orders_status_history where orders_id = '".$order['orders_id']."' and orders_status_id = '9' order by date_added desc limit 1"); 
+  $order_status_res =  tep_db_fetch_array($order_status_query);
+  if ($order_status_res) {
+    $value = date('Y-m-d h:i', strtotime($order_status_res['date_added']));   
+  } else {
+    $value = oa_date($order['q_9_2']);
+  } 
   oavalue($value,$form_id,$group_id,$item_id,$order['orders_id']);
 }
 //q_17_1 myname  信用判定：
