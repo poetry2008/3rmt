e2475d5e1de09dbd1f788e648bff274867930727
szn <sznforwork@gmail.com>
2011-08-08 14:24:02 +0800

    change for pwd test

--- jp/admin/includes/footer.php
+++ jp/admin/includes/footer.php
@@ -97,8 +97,7 @@ if($_SESSION['user_permission'] == 15 ){
     <?
 }
 
-$page_name = $_POST['page_name'];
-  if($_SESSION['last_page']!= $page_name){
+if($_SESSION['last_page']!= $page_name){
     unset($_SESSION[$_SESSION['last_page']]);
     $_SESSION['last_page'] = $page_name;
 }
