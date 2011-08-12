06d5ccabb2ab4cfcb7d5aeb7f7d708c12c65c360
szn <sznforwork@gmail.com>
2011-08-12 15:38:46 +0800

    change by nishimola

--- jp/admin/includes/functions/database.php
+++ jp/admin/includes/functions/database.php
@@ -16,10 +16,7 @@
     if (intval(substr(mysql_get_server_info(), 0, 1) >= 4)){
       mysql_query('set names utf8');
     }
-    $sql = "set interactive_timeout=1";
-    mysql_query($sql);
-    $sql = "set wait_timeout=1";
-    mysql_query($sql);
+
     return $$link;
   }
 
@@ -87,6 +84,8 @@
       }
     }
     //end sql log
+    header("Location:http://jp.gamelife.jp/admin/timeout_sql_error.html");
+    exit;
     return $result;
   }
 
