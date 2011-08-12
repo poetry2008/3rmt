7bc5ce1998e95fa609336d7a5d2fe3b3194922ed
szn <sznforwork@gmail.com>
2011-08-12 16:20:28 +0800

    change name to php

---
+++ jp/admin/timeout_sql_error.php
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD>
+<!-- locale-sensitive -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>エラー</TITLE>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF" >
+エラー
+</BODY>
+</HTML>
