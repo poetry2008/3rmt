3ff42acf21c8b9f147cd0221d7060a497da1bd0b
szn <sznforwork@gmail.com>
2011-08-16 12:17:43 +0800

    handle_payment_time.php

--- jp/admin/handle_payment_time.php
+++ jp/admin/handle_payment_time.php
@@ -3,12 +3,12 @@ require('includes/application_top.php');
 
 $order_id = $_POST['oID'];
 $status_id = $_POST['stid'];
+tep_insert_pwd_log($_POST['once_pwd'],$ocertify->auth_user);
 $orders_status_query = tep_db_query("select * from ".TABLE_ORDERS_STATUS_HISTORY." where orders_status_history_id = '".$status_id."'");
 $orders_status_res = tep_db_fetch_array($orders_status_query);
 if ($orders_status_res) {
   if ($orders_status_res['orders_status_id'] == '9') {
     $del_confirm_payemnt_time_query = tep_db_query("UPDATE `".TABLE_ORDERS."` set `confirm_payment_time` = '0000-00-00 00:00:00' where orders_id = '".$order_id."'");
-    tep_insert_pwd_log($_POST['once_pwd'],$ocertify->auth_user);
   }
   
   $other_status_query = tep_db_query("select * from ".TABLE_ORDERS_STATUS_HISTORY." where orders_status_history_id != '".$status_id."' and orders_id = '".$order_id."' order by date_added desc limit 1");
