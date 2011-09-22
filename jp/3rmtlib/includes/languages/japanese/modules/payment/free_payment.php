c0309e75fee14443c997737eab41e91730bacee2
bobhero <bobhero.chen@gmail.com>
2011-09-22 16:42:11 +0800

    payment add

---
+++ jp/3rmtlib/includes/languages/japanese/modules/payment/free_payment.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+*/
+
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_TITLE', '支払いなし');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_EXPLAIN', '');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_DESCRIPTION', '総額が0円なので支払いは発生しません。');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_EMAIL_FOOTER', '');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_PROCESS', '支払いなし決済手数料が別途かかります。');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_FEE', '支払いなし決済手数料:');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_MAILFOOTER', '');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_OVERFLOW_ERROR', 'お買い上げ金額がコンビニ決済の制限を超えたためお取り扱いできません。');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_ERROR_MESSAGE', '支払いなし決済の処理中にエラーが発生しました. 入力内容を訂正しもう一度試してください。　');
+  define('MODULE_PAYMENT_FREE_PAYMENT_TEXT_SHOW', '振込先口座情報に誤りがないかご確認ください。');
