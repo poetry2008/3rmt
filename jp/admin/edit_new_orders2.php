109e63315270ca2d287020d3b84ed3479772cb01
szn <sznforwork@gmail.com>
2011-10-05 20:45:39 +0800

    change for edit new orders

--- jp/admin/edit_new_orders2.php
+++ jp/admin/edit_new_orders2.php
@@ -450,10 +450,8 @@
       // orders 
       //      var_dump($_SESSION['create_order2']['orders']);
       $new_orders2_id = '';
-      if(!tep_get_orders_status_id($_SESSION['create_order2']['orders']['orders_id'])){
         $_SESSION['create_order2']['orders']['orders_id']= date("Ymd") . '-' . date("His") . tep_get_order_end_num();
         $new_orders2_id = $_SESSION['create_order2']['orders']['orders_id'];
-      }
       tep_db_perform(TABLE_ORDERS, $_SESSION['create_order2']['orders']);
       last_customer_action();
       orders_updated($_SESSION['create_order2']['orders']['orders_id']);
