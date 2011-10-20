35fc69c6d1536306447ca7b9ad5b0457a29a28ad
szn <sznforwork@gmail.com>
2011-10-20 16:33:28 +0800

    change for asset date by nishimola

--- jp/admin/includes/functions/general.php
+++ jp/admin/includes/functions/general.php
@@ -6596,7 +6596,8 @@ function tep_get_relate_date($pid,$site_id=0,$start='',$end='')
 {
   $sql = "select max(o.torihiki_date) as max_date from ".TABLE_ORDERS." o ,
     ".TABLE_ORDERS_PRODUCTS." op ,".TABLE_ORDERS_STATUS." os 
-      where  o.orders_id = op.orders_id and op.products_id ='".$pid."' 
+      where  o.orders_id = op.orders_id and p.products_id = '".$pid."'
+      and p.relate_products_id= op.products_id 
       and o.orders_status = os.orders_status_id and os.calc_price = '1' ";
   if($site_id!=0){
     $sql .= " and o.site_id='".$site_id."' ";
