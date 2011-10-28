03e60ea2b0714e8f59941d00cc6b8b21241927b0
szn <sznforwork@gmail.com>
2011-10-28 15:05:04 +0800

    change for test preorder fround *pre.php

---
+++ ff/ffpre.php
@@ -0,0 +1,40 @@
+<?php
+/*
+  $Id$
+*/
+
+  require('includes/application_top.php');
+?>
+<?php page_head();?>
+</head>
+<body>
+<div align="center">
+  <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+  <table width="900" border="0" cellpadding="0" cellspacing="0" class="side_border">
+    <tr>
+      <td valign="top" class="left_colum_border">
+        <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+      </td>
+      <td valign="top" id="contents">
+        <h1 class="pageHeading"></h1>
+                <div class="comment">
+        <table border="0" width="100%" cellspacing="0" cellpadding="0" class="product_info_box">
+          <tr>
+            <td>
+            </td>
+          </tr>
+        </table>
+                </div>
+                <p class="pageBottom"></p>
+      </td>
+      <td valign="top" class="right_colum_border" width="<?php echo BOX_WIDTH; ?>">
+        <?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+      </td>
+    </tr>
+  </table>
+  <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+</div>
+</div>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
