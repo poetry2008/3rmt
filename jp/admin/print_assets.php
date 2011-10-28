afea36fcc6fee5e278553b7c09e3fa9de32d62d0
szn <sznforwork@gmail.com>
2011-10-28 09:55:56 +0800

    change for asset

--- jp/admin/print_assets.php
+++ jp/admin/print_assets.php
@@ -96,9 +96,9 @@ charset=<?php echo CHARSET; ?>">
 <div class="breadcreumb_asset">
 <?php 
   echo TEXT_TOP;
-  echo '&nbsp;&gt;&nbsp;';
-  echo tep_output_generated_category_path($_GET['pid'], 'product');
-  echo '&nbsp;&gt;&nbsp;';
+  echo '&gt;&gt;';
+  echo tep_output_generated_category_path($_GET['pid'], 'asset');
+  echo '&gt;&gt;';
   echo  tep_get_products_name($_GET['pid'],0,$_GET['site_id']);
 ?>
 </div>
