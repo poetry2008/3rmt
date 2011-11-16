14157a4bf5b68dfc8b5da7a6a13f8dcc9b7cb1c4
szn <sznforwork@gmail.com>
2011-11-16 10:49:06 +0800

    change for language

--- jp/3rmtlib/includes/languages/japanese/modules/payment/buying.php
+++ jp/3rmtlib/includes/languages/japanese/modules/payment/buying.php
@@ -16,12 +16,10 @@
 //振込先口座情報に誤りがないかご確認ください。');
   define('MODULE_PAYMENT_BUYING_TEXT_CONFIRMATION',"");
 
-  /*
-  define('CONFIRMATION_BUYING_TEXT_TITLE', '銀行振込(買い取り)');
-  define('CONFIRMATION_BUYING_TEXT_FEE', '銀行振込(買い取り)決済手数料:');
-  define('TEXT_HANDLE_FEE_CONFIRMATION', '手数料:');
-  define('TEXT_POINT_NOW_TWO','<b>買取はポイントがつきません</b>&nbsp;&nbsp;今回の獲得予定ポイント:');
-  */
+define('CONFIRMATION_BUYING_TEXT_TITLE', '銀行振込(買い取り)');
+define('CONFIRMATION_BUYING_TEXT_FEE', '銀行振込(買い取り)決済手数料:');
+define('TEXT_HANDLE_FEE_CONFIRMATION', '手数料:');
+define('TEXT_POINT_NOW_TWO','<b>買取はポイントがつきません</b>&nbsp;&nbsp;今回の獲得予定ポイント:');
 
 
 define('TEXT_BANK_ERROR_NAME', '【金融機関名】が入力されていません');
@@ -31,4 +29,13 @@ define('TEXT_BANK_ERROR_KOUZA_NUM', '【口座番号】が入力されていま�
 define('TEXT_BANK_ERROR_KOUZA_NUM2', '【口座番号】は半角で入力してください。');
 define('TEXT_BANK_ERROR_KOUZA_NAME', '【口座名義】が入力されていません');
 
+define('TABLE_HEADING_BANK', '振込先口座情報');
+define('TEXT_BANK_NAME', '金融機関名:');
+define('TEXT_BANK_SHITEN', '支店名:');
+define('TEXT_BANK_KAMOKU', '口座種別:');
+define('TEXT_BANK_KOUZA_NUM', '口座番号:');
+define('TEXT_BANK_KOUZA_NAME', '口座名義:');
+
+define('TEXT_BANK_SELECT_KAMOKU_F', '普通');
+define('TEXT_BANK_SELECT_KAMOKU_T', '当座');
 ?>
