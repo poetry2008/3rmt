512ab669b2e8489c867204577c6f4edb08aa9471
kthiz <kthiz2006@gmail.com>
2011-11-21 10:41:24 +0800

    jp account and yuyue page edit

--- jp/member_auth.php
+++ jp/member_auth.php
@@ -108,11 +108,10 @@
                 	<td colspan="3" class="mail_img"><img src="images/design/mail_top.gif" align=""></td>
                 </tr>
                 <tr>
-                  <td colspan="3">
-                  <div style="background-color: #FDEBAC; color: #000000; height: 25px; line-height: 25px; margin:5px 0; padding-left: 10px;">
+                  <td colspan="3" style="background-color: #FDEBAC; color: #000000; height: 25px; line-height: 25px; padding-left: 10px;">
                   <font size="2"><?php
                     echo sprintf(NOTICE_SEND_TO_EMAIL_TEXT, (isset($_POST['cemail'])?$_POST['cemail']:$cus_email)); 
-                  ?></font></div>
+                  ?></font>
                   </td>
                 </tr>
                 <tr>
@@ -121,17 +120,14 @@
                   </td>
                 </tr>
                 <tr>
-                  <td class="active_email_name" colspan="3">
-                  <div style="width:520px;">
-                  <div style="float:left; line-height: 20px; padding-right: 5px;">
+                  <td class="active_email_name">
                   <b><?php echo INPUT_SEND_MAIL;?>:</b> 
-                  </div>
-                  <div class="active_email"  style="float:left; padding-right:25px;">
+                  </td>
+                  <td class="active_email">
                   <?php echo tep_draw_input_field('cemail', (isset($_POST['cemail'])?$_POST['cemail']:$cus_email));?> 
-                  </div>
-                  <div style="float:left; line-height:20px;">
+                  </td>
+                  <td>
                   <?php echo tep_image_submit('button_send_mail.gif', SENDMAIL_BUTTON);?>
-                  </div></div>
                   </td>
                 </tr>
                 <tr>
