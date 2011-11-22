1aa878a3c142c778c84274a33120182b3e4a53ad
kthiz <kthiz2006@gmail.com>
2011-11-22 17:56:14 +0800

    gm account and yuyue page edit

--- gm/non-member_auth.php
+++ gm/non-member_auth.php
@@ -117,35 +117,49 @@
         ?>
         <tr>
           <td>
-          <table>
+          <table class="box_des">
             <tr>
-                <td style="color: rgb(255, 0, 0);" colspan="3">
-                <font size="3"><b><?php echo EMAIL_RED_TEXT_INFO;?></b></font>
-                </td>
-              </tr>
-              <tr>
-                <td style="font-size:11px;" colspan="3">
-                <?php echo EMAIL_READ_INFO_TEXT;?>
-                <br>
-                </td>
-              </tr>
+              <td colspan="3" class="mail_img"><img src="images/design/mail_top.gif" alt=""></td> 
+            </tr>
             <tr>
-              <td class="active_email_name">
+              <td colspan="3" class="information_color">
+              <?php
+                echo sprintf(NOTICE_SEND_TO_EMAIL_TEXT, (isset($_POST['cemail'])?$_POST['cemail']:$cus_email)); 
+              ?>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3">
+              <br><?php echo ACTIVE_INFO_FRONT_COMMENT;?><br><br> 
+              </td>
+            </tr>
+            <tr>
+              <td width="100">
               <b><?php echo INPUT_SEND_MAIL;?>:</b> 
               </td>
-              <td class="active_email">
-              <?php echo tep_draw_input_field('cemail', (isset($_POST['cemail'])?$_POST['cemail']:$cus_email));?> 
+              <td width="285">
+              <?php echo tep_draw_input_field('cemail', (isset($_POST['cemail'])?$_POST['cemail']:$cus_email),'size="37"');?> 
               </td>
               <td>
               <?php echo tep_image_submit('button_send_mail.gif', SENDMAIL_BUTTON);?> 
+              </td>
+            </tr>
+            <tr>
+              <td colspan="3">
+              <br><?php echo ACTIVE_INFO_END_COMMENT;?> 
               </td>
             </tr>
           </table>
           </td>
         </tr>
         <tr>
+          <td class="mail_img_next">
+          <img src="images/design/mail_bottom.gif" alt=""> 
+          </td>
+        </tr>
+        <tr>
           <td>
-          <?php echo GUEST_SUCCESS_INFO_COMMENT;?> 
+          <?php echo ACTIVE_INFO_EMAIL_READ;?> 
           </td>
         </tr>
         <?php }?> 
@@ -157,8 +171,6 @@
           <?php 
           if ($_GET['cu'] == 1) {
             echo '<a href="'.tep_href_link(FILENAME_SHOPPING_CART, '', 'SSL').'">' .  tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; 
-          } else {
-            echo '<a href="' .  tep_href_link(FILENAME_DEFAULT) . '">' .  tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>';
           }
           ?>
                     </td> 
--- gm/non-preorder_auth.php
+++ gm/non-preorder_auth.php
@@ -130,40 +130,51 @@
         }
       }
       echo tep_draw_form('form', tep_href_link('non-preorder_auth.php')); 
-      echo PREORDER_SUCCESS_ACTIVE_INFO_TEXT; 
-      echo '<br><br>'; 
       echo tep_draw_hidden_field('action', 'send'); 
       ?>
-      <table border="0" width="100%" cellspacing="0" cellpadding="0" style="font-size:14px;">
+      <table border="0" width="95%" cellspacing="0" cellpadding="0" class="box_des">
         <tr>
           <td>
-            <table>
+            <table class="box_des">
               <tr>
-                <td style="font-size:11px; color:#ff0000;"><?php echo INPUT_PREORDER_SEND_MAIL;?></td> 
-                <td>
-                <?php echo tep_draw_input_field('pemail', (isset($_POST['pemail'])?$_POST['pemail']:$pe_email));?> 
+                <td colspan="3" class="mail_img"><img src="images/design/mail_top.gif" alt=""></td> 
+              </tr>
+              <tr>
+                <td colspan="3" class="information_color">
+                <?php echo sprintf(PREORDER_NOTICE_SEND_TO_EMAIL_TEXT, (isset($_POST['pemail'])?$_POST['pemail']:$pe_email));?> 
+                </td>
+              </tr>
+              <tr>
+                <td colspan="3">
+                <br><?php echo PREORDER_ACTIVE_INFO_FRONT_COMMENT;?><br><br> 
+                </td>
+              </tr>
+              <tr>
+                <td width="100"><?php echo INPUT_PREORDER_SEND_MAIL;?></td> 
+                <td width="285">
+                <?php echo tep_draw_input_field('pemail', (isset($_POST['pemail'])?$_POST['pemail']:$pe_email),'size="37"');?> 
                 </td>
                 <td>
                 <?php echo tep_image_submit('button_send_mail.gif', 'mail');?> 
                 </td>
               </tr>
-            </table><br>
+              <tr>
+                <td colspan="3">
+                <br><?php echo PREORDER_ACTIVE_INFO_END_COMMENT;?> 
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td class="mail_img_next">
+          <img src="images/design/mail_bottom.gif" alt=""> 
           </td>
         </tr>
         <tr>
           <td>
-          <?php echo sprintf(PREORDER_SUCCESS_TEXT, '<a href="'.tep_href_link('email_trouble.php').'">'.PREORDER_SUCCESS_TEXT_LINK.'</a>');?> 
+          <?php echo PREORDER_ACTIVE_INFO_EMAIL_READ;?> 
           </td>
-        </tr>
-        <tr>
-          <td><br>
-                <table border="0" width="100%" cellspacing="0" cellpadding="0"> 
-                  <tr> 
-                    <td class="main" align="right"><?php echo '<a href="' .tep_href_link(FILENAME_DEFAULT). '">' .  tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td> 
-                    <td align="right" class="main">
-                    </td> 
-                  </tr> 
-                </table></td> 
         </tr>
       </table>
       </form> 
