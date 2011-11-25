2e36803871b49301336c3eff7090d0b3662cd1e1
kthiz <kthiz2006@gmail.com>
2011-11-25 09:48:25 +0800

    guidance payment add

---
+++ jp/3rmtlib/includes/languages/japanese/modules/payment/guidance.php
@@ -0,0 +1,48 @@
+<?php
+/*
+  $Id$
+*/
+
+// 楽天銀行
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_TITLE', 'ウェブマネー及びゲーム間移動');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_DESCRIPTION', 'ウェブマネー及びゲーム間移動(代金に手数料が連動)');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_PROCESS','ウェブマネー及びゲーム間移動へ振り込みする場合は選択してください。<br>振込手数料はお客様のご負担となります。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_PROCESS_CON', 'LAWSON、スリーエフ、MINI STOP、サークルK、SUNKUSがご利用いただけます。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_FOOTER', '※ SmartPitシートの送信は、弊社営業時間内に行っております。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_FEE', 'ウェブマネー及びゲーム間移動手数料:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_OVERFLOW_ERROR', 'お買い上げ金額がウェブマネー及びゲーム間移動の制限を超えたためお取り扱いできません。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_MAILFOOTER', '');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_TELNUMBER_FOOTER', '');
+  
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_SID', '取引コード:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_ZIP_CODE', '郵便番号:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_ADDRESS', '住所:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_NAME', '氏名:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_L_NAME', '姓:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_F_NAME', '名:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_TEL', '電話番号:');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_ERROR_MESSAGE', 'ウェブマネー及びゲーム間移動の処理中にエラーが発生しました。入力内容を確認し、再度入力してください。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_ERROR_MESSAGE_NOE', '電話番号と電話番号(確認用)が一致しませんでした。');
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_ERROR_MESSAGE_NOM', '入力エラー、電話番号の形式が間違っているか使用できない文字が含まれています。入力内容を確認し、再度入力してください。');
+  define('MODULE_PAYMENT_RAKUTEN_TELNUMBER_TEXT', '電話番号:');
+  define('MODULE_PAYMENT_RAKUTEN_TELNUMBER_CONFIRMATION_TEXT', '電話番号(確認用):');
+  define('MODULE_PAYMENT_RAKUTEN_MUST_INPUT', '<small><font color="#AE0E30">(必須)</font></small>');
+  define('MODULE_PAYMENT_RAKUTEN_INFO_TEXT', '下記入力欄に電話番号をご記入ください。<br>');
+ 
+  define('MODULE_PAYMENT_GUIDANCE_TEXT_CONFIRMATION', '
+電話番号:#TELNUMBER#
+
+下記の口座へお振り込みください。
+------------------------------------------
+銀行名　　：　ウェブマネー及びゲーム間移動（旧イーバンク銀行）
+支店名　　：　ワルツ支店
+口座種別　：　普通
+口座名義　：　カ）アイアイエムワイ
+口座番号　：　7003965
+------------------------------------------
+※ 必ずご注文時に入力したお名前でお振り込みください。
+※ 振込手数料はお客様のご負担となります。
+※ お振り込みはご注文から７日以内にお願いいたします。
+※ ご入金を株式会社iimyが確認した時点でご契約の成立となります。
+
+');
