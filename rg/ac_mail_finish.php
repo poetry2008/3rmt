bfaad2247f5b611b40c06ac97d16bd94a795103a
kthiz <kthiz2006@gmail.com>
2011-11-28 17:38:32 +0800

    rg account and yuyue page edit

--- rg/ac_mail_finish.php
+++ rg/ac_mail_finish.php
@@ -93,10 +93,10 @@
          }
          ?>
          <?php echo tep_draw_form('form', tep_href_link('ac_mail_finish.php', 'action=send', 'SSL'));?> 
-          <table border="0" width="100%" cellspacing="0" cellpadding="0" class="preorder_success_box"> 
+          <table border="0" width="100%" cellspacing="0" cellpadding="0"> 
             <tr>
               <td>
-              <table class="preorder_success_box">
+              <table>
                 <tr>
                   <td colspan="3"><img src="images/design/mail_top.gif" alt=""></td>
                 </tr>
