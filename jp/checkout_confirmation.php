acc70a13cb7fff5774cf3dd6d16771c43e1cf92c
szn <sznforwork@gmail.com>
2012-01-09 10:59:57 +0800

    add jp/checkout_confirmation.php

--- jp/checkout_confirmation.php
+++ jp/checkout_confirmation.php
@@ -45,7 +45,6 @@ require(DIR_WS_ACTIONS.'visites.js');
 </tr> 
 </table></td> 
 <td width="20%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
-<td width="20%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
 <td width="20%"><table border="0" width="100%" cellspacing="0" cellpadding="0"> 
 <tr> 
 <td width="50%"><?php echo tep_draw_separator('pixel_silver.gif', '100%', '1'); ?></td> 
@@ -61,7 +60,6 @@ require(DIR_WS_ACTIONS.'visites.js');
 </table></td> 
 </tr> 
 <tr> 
-<td align="center" width="20%" class="checkoutBarFrom"><?php echo '<a href="' . tep_href_link(FILENAME_CHECKOUT_PRODUCTS, '', 'SSL') . '" class="checkoutBarFrom">' . CHECKOUT_BAR_PRODUCTS . '</a>'; ?></td> 
 <td align="center" width="20%" class="checkoutBarFrom"><?php echo '<a href="' . tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL') . '" class="checkoutBarFrom">' . CHECKOUT_BAR_DELIVERY . '</a>'; ?></td> 
 <td align="center" width="20%" class="checkoutBarFrom"><?php echo '<a href="' . tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '" class="checkoutBarFrom">' . CHECKOUT_BAR_PAYMENT . '</a>'; ?></td> 
 <td align="center" width="20%" class="checkoutBarCurrent"><?php echo CHECKOUT_BAR_CONFIRMATION; ?></td> 
@@ -77,90 +75,15 @@ require(DIR_WS_ACTIONS.'visites.js');
 <td class="main" align="right"><?php echo tep_image_submit('button_confirm_order.gif', IMAGE_BUTTON_CONFIRM_ORDER);?></td> 
 </tr> 
 </table>
-<table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox"> 
-<tr class="infoBoxContents">
+</td>
+</tr>
+<tr>
 <td>
-<?php //这里 回显 配送的 信息?>
-<?php
-$shipping_method_count = count($_SESSION['shipping_method_info_arr']);
-$shipping_method_info_arr = $_SESSION['shipping_method_info_arr'];
-$show_some_shipping = false;
-if($shipping_method_count > 1){
-  $show_some_shipping = true;
-}
-if(!$show_some_shipping){
-  echo "<div>";
-}
-for($i =0,$n=sizeof($order->products);$i<$n;$i++){
-  $product_info = tep_get_product_by_id($order->products[$i]['id'], SITE_ID,
-      $languages_id);
-    if($show_some_shipping){
-      echo "<div>";
-    }
-  echo "<div>";
-    
-    echo "<div>";
-    echo $order->products[$i]['qty'] . '&nbsp;個' .
-    (!empty($product_info['products_attention_1_3']) &&
-     tep_get_full_count_in_order2($order->products[$i]['qty'],
-       $order->products[$i]['id']) ? '<br><span style="font-size:10px">'.
-     tep_get_full_count_in_order2($order->products[$i]['qty'],
-       $order->products[$i]['id']) .'</span>': ''); 
-    echo "</div>";
-    
-    echo "<div>";
-    echo $order->products[$i]['name'];
-    if (STOCK_CHECK == 'true') {
-      echo tep_check_stock($order->products[$i]['id'], $order->products[$i]['qty']);
-    }
-  
-    if ( (isset($order->products[$i]['attributes'])) && (sizeof($order->products[$i]['attributes']) > 0) ) {
-      for ($j=0, $n2=sizeof($order->products[$i]['attributes']); $j<$n2; $j++) {
-        echo '<br><small>&nbsp;<i> - ' . $order->products[$i]['attributes'][$j]['option'] . ': ' . $order->products[$i]['attributes'][$j]['value'] . '</i></small>';
-      }
-    }
-  echo "</div>";
-    
-    if (sizeof($order->info['tax_groups']) > 1) {
-      echo '<div>'.tep_display_tax_value($order->products[$i]['tax']) . '%</div>';
-    }
-  echo '<div>' . $currencies->display_price($order->products[$i]['final_price'], 
-      $order->products[$i]['tax'], $order->products[$i]['qty']) . '</div>'; 
-    
-    echo "</div>";
-    if($show_some_shipping){
-      echo "</div>";
-        //这里输出这个产品的配送
-        echo "<div>";
-        //配送的DIV
-        echo "</div>";
-        echo "<div>";
-        $address_id_arr = explode('|||',
-            $shipping_method_info_arr[$product_info['products_id']]['shipping_address']);
-        $cid = $address_id_arr[0];
-        $aid = $address_id_arr[1];
-        $address_info = tep_get_address_by_cid_aid($cid,$aid,true);
-        foreach($address_info as $address_row){
-          echo "<div>";
-            echo $address_row['text'];
-            echo "</div>";
-        }
-      echo $shipping_method_info_arr[$product_info['products_id']]['torihikihouhou'];
-        echo $shipping_method_info_arr[$product_info['products_id']]['insert_torihiki_date'];
-        echo "</div>";
-    }
-}
-if(!$show_some_shipping){
-  echo "</div>";
-    
-    //单个 配送的输出信息
-    echo "<div>";
-    echo "</div>";
-}
 
-?>
-</td> 
-</tr></table></td></tr>
+<?php //这里 回显 配送的 信息?>
+<?php require_once DIR_WS_ACTIONS.'checkout_confirmation_shipping_list.php';?>
+
+</td>
 </tr> 
 <tr> 
 <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td> 
