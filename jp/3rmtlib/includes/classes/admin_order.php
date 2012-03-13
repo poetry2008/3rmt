6dda58fb28f5d759804bd78d8ac7e38601fd2d58
kthiz <kthiz2006@gmail.com>
2012-03-13 10:19:05 +0800

    option edit

--- jp/3rmtlib/includes/classes/admin_order.php
+++ jp/3rmtlib/includes/classes/admin_order.php
@@ -109,7 +109,7 @@
                                                                      'id' => $attributes['orders_products_attributes_id'],
                                                                      'option_item_id' => $attributes['option_item_id'],
                                                                      'option_group_id' => $attributes['option_group_id'],
-                                                                     'option_info' => @unserialize($attributes['option_info']),
+                                                                     'option_info' => @unserialize(stripslashes($attributes['option_info'])),
                                                                      'price' => $attributes['options_values_price']);
 
             $subindex++;
