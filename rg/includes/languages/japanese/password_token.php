32aa9dba10a1d07880c6cced1d007c03516076c8
kthiz <kthiz2006@gmail.com>
2012-04-12 16:03:27 +0800

    password edit

--- rg/includes/languages/japanese/password_token.php
+++ rg/includes/languages/japanese/password_token.php
@@ -2,7 +2,7 @@
 define('HEADING_TITLE', 'パスワード再発行手続き');
 define('UPDATE_ENTRY_PASSWORD_TEXT', 'パスワード:');
 define('UPDATE_ENTRY_PASSWORD_AGAINTEXT', 'パスワードを再入力:');
-define('UPDATE_ENTRY_PASSWORD_READ', '(例. 01abcdef)&nbsp;<font color="#aabbdd">必須</font>');
+define('UPDATE_ENTRY_PASSWORD_READ', '(例. abcd1234)&nbsp;<font color="#aabbdd">必須</font>');
 define('UPDATE_ENTRY_PASSWORD_WORNG', 'パスワードが間違っています');
 define('UPDATE_ENTRY_NO_USE_OLD_PASSWORD', '安全のため、古いパスワードと違うパスワードを設定してください。');
 define('UPDATE_ENTRY_PASSWORD_LEN_SHORT', 'パスワードは'.ENTRY_PASSWORD_MIN_LENGTH.'文字以上で設定してください。');
