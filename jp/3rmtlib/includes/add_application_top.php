03c3694fb17b5277b09ebfd8c20df81c089bf345
kthiz <kthiz2006@gmail.com>
2012-04-17 10:34:55 +0800

    popup password edit

--- jp/3rmtlib/includes/add_application_top.php
+++ jp/3rmtlib/includes/add_application_top.php
@@ -133,7 +133,7 @@
   define('FILENAME_EMAIL_TROUBLE', 'email_trouble.php');
 
 // define the database table names used in the project
-  define('TABLE_OTHER_CONFIG', 'TABLE_OTHER_CONFIG'); 
+  define('TABLE_OTHER_CONFIG', 'other_config'); 
   define('TABLE_CAMPAIGN', 'campaign'); 
   define('TABLE_CUSTOMER_TO_CAMPAIGN', 'customer_to_campaign'); 
   define('TABLE_ZIPCODE', 'zcode'); 
