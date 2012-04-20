41a5c279820e01d65641742d5036ab8b4255fd9f
kthiz <kthiz2006@gmail.com>
2012-04-20 13:52:26 +0800

    error edit

--- jp/reorder.php
+++ jp/reorder.php
@@ -235,6 +235,7 @@ $breadcrumb->add('再配達フォーム', tep_href_link('reorder.php'));
   $insert_id = $oID;
   
   $o = new order($oID);
+  $payment_code = payment::changeRomaji($o->info['payment_method'], PAYMENT_RETURN_TYPE_CODE); 
 
   # Check
   // ccdd
@@ -264,7 +265,7 @@ $breadcrumb->add('再配達フォーム', tep_href_link('reorder.php'));
   */
 
 // load the before_process function from the payment modules
-  $payment_modules->before_process();
+  $payment_modules->before_process($payment_code);
 
   require(DIR_WS_CLASSES . 'order_total.php');
   $order_total_modules = new order_total;
