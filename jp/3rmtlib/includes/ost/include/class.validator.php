7dea8572a808ca40b860df6e61474728213f4358
szn <sznforwork@gmail.com>
2012-05-15 18:00:08 +0800

    change for email ost

--- jp/3rmtlib/includes/ost/include/class.validator.php
+++ jp/3rmtlib/includes/ost/include/class.validator.php
@@ -133,7 +133,50 @@ class Validator {
    
     /* Functions below can be called directly without class instance. Validator::func(var..); */
     function is_email($email) {
+        /*
         return (preg_match('/^([*+!.&#$|\'\\%\/0-9a-z^_`{}=?~:-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})$/i',trim(stripslashes($email))));
+        */
+
+  $isValid = true;
+  $atIndex = strrpos($email, "@");
+  if (is_bool($atIndex) && !$atIndex) {
+    $isValid = false;
+  } else {
+    $domain = substr($email, $atIndex+1);
+    $local = substr($email, 0, $atIndex);
+    $localLen = strlen($local);
+    $domainLen = strlen($domain);
+    if ($localLen < 1 || $localLen > 64) {
+      // front @ length 
+      $isValid = false;
+    } else if ($domainLen < 1 || $domainLen > 255) {
+      // back @  length 
+      $isValid = false;
+    } else if ($local[0] == '.') {
+      // dot at start or end
+      $isValid = false;
+    } else if (!preg_match('/^[\]\\:[A-Za-z0-9\\-\\.]+$/', $domain)) {
+      // character not valid in domain part
+      $isValid = false;
+    } else if (preg_match('/\\.\\./', $domain)||preg_match('/^\./',$domain)) {
+      // domain part has two consecutive dots
+      $isValid = false;
+    } else if(!preg_match('/^(\\\\."|[\(\)\<\>\[\]\:\;\,A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+          str_replace("\\\\","",$local))) {
+      // character not valid in local part unless 
+      // local part is quoted
+      if (!preg_match('/^"(\\\\"|[^"])+"$/',
+            str_replace("\\\\","",$local))) {
+        $isValid = false;
+      }
+    }
+    if ($isValid && ENTRY_EMAIL_ADDRESS_CHECK == 'true') {
+      if (!checkdnsrr($domain, "MX") && !checkdnsrr($domain, "A")) {
+        $isValid = false;
+      }
+    }
+  }
+  return $isValid;
     }
     function is_phone($phone) {
         /* We're not really validating the phone number but just making sure it doesn't contain illegal chars and of acceptable len */
