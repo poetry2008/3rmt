cca7013ef689cd66b91df52ac59aa09f599f97b5
kthiz <kthiz2006@gmail.com>
2012-05-24 14:05:28 +0800

    option edit

--- jp/ajax_process.php
+++ jp/ajax_process.php
@@ -11,7 +11,11 @@ if ($_GET['action'] == 'calc_price') {
  
   $final_price = $products_price + $attributes_price;
   $price_array = array();
-  $price_array['price'] = $currencies->display_price($final_price, tep_get_tax_rate($products_info['products_tax_class_id']), $_GET['qty']); 
+  if ($final_price < 0) {
+    $price_array['price'] = '<font color="ff0000">'.str_replace(JPMONEY_UNIT_TEXT, '', $currencies->display_price($final_price, tep_get_tax_rate($products_info['products_tax_class_id']), $_GET['qty'])).JPMONEY_UNIT_TEXT.'</font>'; 
+  } else {
+    $price_array['price'] = $currencies->display_price($final_price, tep_get_tax_rate($products_info['products_tax_class_id']), $_GET['qty']); 
+  }
   echo json_encode($price_array);
 } else if ($_GET['action'] == 'calc_radio_price') {
   require('option/HM_Option.php');
