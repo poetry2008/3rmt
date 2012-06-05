fd1e0a43b93d38161d3e435dcc1737b4b6bfc2bd
kthiz <kthiz2006@gmail.com>
2012-06-05 18:33:36 +0800

    zy shipping

--- jp/js/date_time.js
+++ jp/js/date_time.js
@@ -167,9 +167,9 @@ function selectHour(start_time,end_time,hour,min_num,ele){
                   arr_time_m = arr_time_t[k].split(':');
              
                   if(k != arr_time_t.length-1){
-                    string +=  '<div class="time_radio"><input type="radio" id="m'+m+'" name="min" value="'+m+'"'+checked+' onclick="change_time('+m+',\''+array_end[n]+'\');"></div><div class="time_label"><label for="m'+m+'">'+arr_time_m[0]+'時'+arr_time_m[1]+'分～';
+                    string +=  '<div class="time_radio"><input type="radio" id="m'+m+'" name="min" value="'+m+'"'+checked+' onclick="change_time('+m+',\''+array_end[n]+'\');"></div><div class="time_label"><label for="m'+m+'"><a>'+arr_time_m[0]+'時'+arr_time_m[1]+'分～';
                   }else{
-                    string +=  arr_time_m[0]+'時'+arr_time_m[1]+'分</label></div>'; 
+                    string +=  arr_time_m[0]+'時'+arr_time_m[1]+'分</a></label></div>'; 
                   }
                 }
                 if(m % 2 == 1){
