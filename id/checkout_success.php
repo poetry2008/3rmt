82e12c109a985b81d12e81c6c7113a27197cdfd6
kthiz <kthiz2006@gmail.com>
2012-06-29 14:50:06 +0800

    option edit

--- id/checkout_success.php
+++ id/checkout_success.php
@@ -69,9 +69,7 @@
   <!-- body //--> 
   <table width="900" border="0" cellpadding="0" cellspacing="0" class="side_border"> 
     <tr> 
-      <!--<td width="<?php echo BOX_WIDTH; ?>" align="right" valign="top" class="left_colum_border">--> <!-- left_navigation //--> 
-        <?php //require(DIR_WS_INCLUDES . 'column_left.php'); ?> 
-        <!-- left_navigation_eof //--> <!--</td>--> 
+        <!-- left_navigation_eof //--> 
       <!-- body_text //--> 
       <td valign="top" id="contents"><?php echo tep_draw_form('order', tep_href_link(FILENAME_CHECKOUT_SUCCESS, 'action=update', 'SSL')); ?> 
         <h1 class="pageHeading">
