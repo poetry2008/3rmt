16da07d9fc966d65b05094049d07aed1797391f3
kthiz <kthiz2006@gmail.com>
2012-08-13 09:42:29 +0800

    option edit

--- jp/checkout_confirmation.php
+++ jp/checkout_confirmation.php
@@ -157,7 +157,7 @@ for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
   $product_info = tep_get_product_by_id((int)$order->products[$i]['id'], SITE_ID, $languages_id);
     
   echo '          <tr>' . "\n" .
-    '            <td class="main" align="center" valign="top" width="150">' .
+    '            <td align="right" valign="top" class="confirmation_product_num_info">' .
     $order->products[$i]['qty'] . '&nbsp;'. NUM_UNIT_TEXT.  (!empty($product_info['products_attention_1_3']) && tep_get_full_count_in_order2($order->products[$i]['qty'], (int)$order->products[$i]['id']) ? '<br><span style="font-size:10px">'.  tep_get_full_count_in_order2($order->products[$i]['qty'], (int)$order->products[$i]['id']) .'</span>': '') . '</td>' . "\n" .
     '            <td class="main" valign="top">' . $order->products[$i]['name'];
   if ($order->products[$i]['price'] < 0) {
