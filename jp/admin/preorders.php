f95503208e4bb5b3e80149484943e6643732b0ac
szn <sznforwork@gmail.com>
2012-08-17 15:21:12 +0800

    change for preorder item order

--- jp/admin/preorders.php
+++ jp/admin/preorders.php
@@ -1375,13 +1375,35 @@ if(!(isset($_SESSION[$page_name])&&$_SESSION[$page_name])&&$_SESSION['onetime_pw
        '      <td class="dataTableContent" valign="top">' . $order->products[$i]['name'];
 
         if (isset($order->products[$i]['attributes']) && $order->products[$i]['attributes'] && ($k = sizeof($order->products[$i]['attributes'])) > 0) {
+          $all_show_option_id = array();
+          $all_show_option = array();
+          $option_item_order_sql = "select it.id from ".TABLE_PRODUCTS."
+          p,".TABLE_OPTION_ITEM." it 
+          where p.products_id = '".(int)$order->products[$i]['id']."' 
+          and p.belong_to_option = it.group_id 
+          and it.status = 1
+          order by it.sort_num,it.title";
+          $option_item_order_query = tep_db_query($option_item_order_sql);
+          while($show_option_row_item = tep_db_fetch_array($option_item_order_query)){
+            $all_show_option_id[] = $show_option_row_item['id'];
+          }
           for ($j = 0; $j < $k; $j++) {
             if (is_array($order->products[$i]['attributes'][$j]['option_info'])) {
+            $all_show_option[$order->products[$i]['attributes'][$j]['option_item_id'] =
+              $order->products[$i]['attributes'][$j];
+            /*
               echo '<br><nobr>&nbsp; - ' .  $order->products[$i]['attributes'][$j]['option_info']['title'] . ': ' . str_replace(array("<br>", "<BR>"), "",$order->products[$i]['attributes'][$j]['option_info']['value']);
+             */
             }
             //if ($order->products[$i]['attributes'][$j]['price'] != '0') echo ' (' . $order->products[$i]['attributes'][$j]['prefix'] . $currencies->format($order->products[$i]['attributes'][$j]['price'] * $order->products[$i]['qty'], true, $order->info['currency'], $order->info['currency_value']) . ')';
-              echo '</nobr>';
           }
+          // new option list 
+          foreach($all_show_option_id as $t_item_id){
+            if (is_array($all_show_option[$t_item_id]['option_info'])) {
+            echo '<br><nobr>&nbsp; - ' .  $all_show_option[$t_item_id]['option_info']['title'] . ': ' .  str_replace(array("<br>", "<BR>"), "",$all_show_option[$t_item_id]['option_info']['value']);
+            echo '</nobr>';
+          }
+
         }
 
       if ( DISPLAY_PRICE_WITH_TAX == 'true' ) {
