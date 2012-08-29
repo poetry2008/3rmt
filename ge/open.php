805fc0371d6eb68d0f6cf9ad92d079cbe5f9f2bf
szn <sznforwork@gmail.com>
2012-08-29 10:17:37 +0800

    commit for ge patch

--- ge/open.php
+++ ge/open.php
@@ -1,6 +1,7 @@
 <?php
 $_noemailclass = true;
   require_once('includes/application_top.php');
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_OPEN);
 //require('includes/configure.php');
 if(isset($_POST)&&$_POST){
   foreach($_POST as $pos_key => $pos_val){
@@ -21,13 +22,13 @@ if($_POST):
     $_POST['emailId']=0; //Just Making sure we don't accept crap...only topicId is expected.
     if(!$thisuser && $cfg->enableCaptcha()){
         if(!$_POST['captcha'])
-            $errors['captcha']='認証コードを入力してください';
+            $errors['captcha']=TEXT_OPEN_INPUT_CAPTCHA;
         elseif(strcmp($_SESSION['captcha'],md5(strtolower($_POST['captcha']))))
-            $errors['captcha']='認証コードを入力しなおしてください!';
+            $errors['captcha']=TEXT_OPEN_INPUT_CAPTCHA_AGAIN;
     }
     //Ticket::create...checks for errors..
     if(($ticket=Ticket::create($_POST,$errors,SOURCE))){
-              $msg='お客様のご質問は '.STORE_NAME.' へ送信されました。';
+              $msg=sprintf(TEXT_OPEN_MAIL_SEND,STORE_NAME);
 
       /*
         if($thisclient && $thisclient->isValid()) //Logged in...simply view the newly created ticket.
@@ -36,12 +37,11 @@ if($_POST):
         //Thank the user and promise speedy resolution!
         $inc='thankyou.inc.php';
     }else{
-        $errors['err']=$errors['err']?$errors['err']:'必要な情報を正しくご入力ください';
+        $errors['err']=$errors['err']?$errors['err']:TEXT_OPEN_INPUT_ERROR;
     }
 endif;
 
 //page
-define('NAVBAR_TITLE','お問い合わせ');
 
 $breadcrumb->add(NAVBAR_TITLE, tep_href_link(FILENAME_BROWSER_IE6X));
 
