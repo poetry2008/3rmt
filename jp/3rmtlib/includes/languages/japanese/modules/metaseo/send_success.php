e699828aeee6662022f1e73cdfebc29327fa8683
szn <sznforwork@gmail.com>
2012-09-14 16:37:25 +0800

    change for seo

--- jp/3rmtlib/includes/languages/japanese/modules/metaseo/send_success.php
+++ jp/3rmtlib/includes/languages/japanese/modules/metaseo/send_success.php
@@ -5,5 +5,5 @@
 
   define('MODULE_METASEO_SEND_SUCCESS_TEXT_TITLE', 'パスワード再発行手続き');
   define('MODULE_METASEO_SEND_SUCCESS_TEXT_EXPLAIN', '説明');
-  define('MODULE_METASEO_SEND_SUCCESS_TEXT_DESCRIPTION', 'パスワード再発行手続きージのSEO');
+  define('MODULE_METASEO_SEND_SUCCESS_TEXT_DESCRIPTION', 'パスワード再発行手続きページのSEO');
 ?>
