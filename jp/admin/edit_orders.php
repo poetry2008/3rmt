b2cb3c08518af31947e0b0a17550bae0e3ef8fc4
kthiz <kthiz2006@gmail.com>
2012-09-24 09:25:08 +0800

    shiru edit

--- jp/admin/edit_orders.php
+++ jp/admin/edit_orders.php
@@ -1196,29 +1196,27 @@ if($address_error == false){
 
         // 2.2.1 Update inventory Quantity
         $p = tep_db_fetch_array(tep_db_query("select * from products where products_id='".$add_product_products_id."'"));
-        if ($customer_guest['is_calc_quantity'] != '1') {
-          if ((int)$add_product_quantity > $p['products_real_quantity']) {
-            // 买取商品大于实数
-            tep_db_perform('products',array(
-                  'products_real_quantity' => 0,
-                  //'products_virtual_quantity' => 0,
-                  //'products_virtual_quantity' => $p['products_virtual_quantity'] - ((int)$add_product_quantity + $p['products_real_quantity'])
-                  'products_virtual_quantity' => $p['products_virtual_quantity'] - (int)$add_product_quantity + $p['products_real_quantity']
-                  ),
-                'update',
-                "products_id = '" . $add_product_products_id . "'");
-          } else {
-            tep_db_perform('products',array(
-                  'products_real_quantity' =>$p['products_real_quantity']  - (int)$add_product_quantity
-                  // 'products_real_quantity' =>$p['products_real_quantity']+ $p['products_virtual_quantity'] - (int)$add_product_quantity
-                  ),
-                'update',
-                "products_id = '" . $add_product_products_id . "'");
-          }
-          // 增加销售量
-          tep_db_query("update " . TABLE_PRODUCTS . " set products_ordered = products_ordered + " . (int)$add_product_quantity . " where products_id = '" . $add_product_products_id . "'");
-          // 处理负数问题
+        if ((int)$add_product_quantity > $p['products_real_quantity']) {
+          // 买取商品大于实数
+          tep_db_perform('products',array(
+                'products_real_quantity' => 0,
+                //'products_virtual_quantity' => 0,
+                //'products_virtual_quantity' => $p['products_virtual_quantity'] - ((int)$add_product_quantity + $p['products_real_quantity'])
+                'products_virtual_quantity' => $p['products_virtual_quantity'] - (int)$add_product_quantity + $p['products_real_quantity']
+                ),
+              'update',
+              "products_id = '" . $add_product_products_id . "'");
+        } else {
+          tep_db_perform('products',array(
+                'products_real_quantity' =>$p['products_real_quantity']  - (int)$add_product_quantity
+                // 'products_real_quantity' =>$p['products_real_quantity']+ $p['products_virtual_quantity'] - (int)$add_product_quantity
+                ),
+              'update',
+              "products_id = '" . $add_product_products_id . "'");
         }
+        // 增加销售量
+        tep_db_query("update " . TABLE_PRODUCTS . " set products_ordered = products_ordered + " . (int)$add_product_quantity . " where products_id = '" . $add_product_products_id . "'");
+        // 处理负数问题
         tep_db_query("update " . TABLE_PRODUCTS . " set products_real_quantity = 0 where products_real_quantity < 0 and products_id = '" . $add_product_products_id . "'");
         tep_db_query("update " . TABLE_PRODUCTS . " set products_virtual_quantity = 0 where products_virtual_quantity < 0 and products_id = '" . $add_product_products_id . "'");
         /*
