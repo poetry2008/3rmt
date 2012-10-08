0611db162c358e940fc823a54a23f87cec355d40
homehost <sznforwork+homehost@gmail.com>
2012-10-08 10:02:30 +0800

    change for help customer language

--- jp/admin/includes/languages/chinese/customers.php
+++ jp/admin/includes/languages/chinese/customers.php
@@ -3,7 +3,6 @@
   $Id$
 */
 
-
 define('HEADING_TITLE', '顾客管理');
 define('HEADING_TITLE_SEARCH', '搜索:');
 
@@ -37,11 +36,13 @@ define('BUTTON_CUSTOMERS_PRODUCTS_TEXT', '商品历史记录');
 define('CUSTOMER_COMMUNITY_TEXT', '该公司使用栏');
 define('CUSTOMER_COMMUNITY_SEARCH_TEXT', '信用调查:');
 define('CUSTOMER_COMMUNITY_SEARCH_OLD_CUSTOMER_TEXT', '老顾客【HQ】&nbsp;&nbsp;注意【WA】&nbsp;&nbsp;禁止发送【BK】');
-define('CUSTOMER_COMMUNITY_SEARCH_CREADIT_TEXT', '信用卡初次结算日：C2007/01/01&nbsp;&nbsp;&nbsp;&nbsp;范围一致：Aok&nbsp;&nbsp;&nbsp;&nbsp;本人确认完了：Hok&nbsp;&nbsp;&nbsp;&nbsp;YahooID更新日：Y2007/01 /01&nbsp;&nbsp;&nbsp;&nbsp;referer：R');
+define('CUSTOMER_COMMUNITY_SEARCH_CREDIT_TEXT', '信用卡初次结算日：C2007/01/01&nbsp;&nbsp;&nbsp;&nbsp;范围一致：Aok&nbsp;&nbsp;&nbsp;&nbsp;本人确认完了：Hok&nbsp;&nbsp;&nbsp;&nbsp;YahooID更新日：Y2007/01 /01&nbsp;&nbsp;&nbsp;&nbsp;referer：R');
 define('CUSTOMER_COMMUNITY_SEARCH_EXAMPLE_TEXT', '填写例：WA-Aok-C2007/01/01-Hok-RグーグルFF11 RMT');
 define('CUSTOMER_SEARCH_READ_TITLE', '※搜索对象：「顾客名（姓/名/）」「注音假名（姓/名）」「邮箱地址」');
 define('CUSTOMER_RESET', '更改密码委托');
 define('TEXT_USER_ADDED','账号注册者:');
 define('TEXT_USER_UPDATE','账号更新者:');
 define('CUSTOMER_IS_SEAL','禁止下订单');
-define('TEXT_REORDER_DUMMY_WIDTH','日语假名あいうえお宽度');
+define('CUSTOMER_PIC_TEXT', '标记');
+define('CUSTOMER_NO_SEND_MAIL_TEXT', '不发送邮件');
+define('CUSTOMER_CALC_QUANTITY_TEXT', '不自动计算库存数量的增减');
