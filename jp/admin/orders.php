41fb1f3924449d932869eb109660c6378247fc95
hm1002 <liufangzhaoly@gmail.com>
2012-10-22 17:10:08 +0800

    750

--- jp/admin/orders.php
+++ jp/admin/orders.php
@@ -4287,11 +4287,11 @@ if($c_parent_array['parent_id'] == 0){
             <td align="right" class="smallText">
             <table width=""  border="0" cellspacing="1" cellpadding="0">
             <tr>
-            <td class="smallText" valign='top'>
+            <td class="smallText" valign='top' align="right">
             <?php echo tep_draw_form('orders1', FILENAME_ORDERS, '',
                 'get','id="orders1" onsubmit="return false"'); ?><?php echo
             TEXT_ORDER_FIND;?> 
-            <input name="keywords" style="width:320px;" type="text" id="keywords" size="40" value="<?php if(isset($_GET['keywords'])) echo stripslashes($_GET['keywords']); ?>">
+            <input name="keywords" style="width:310px;" type="text" id="keywords" size="40" value="<?php if(isset($_GET['keywords'])) echo stripslashes($_GET['keywords']); ?>">
             <select name="search_type" onChange='search_type_changed(this)' style="text-align:left;">
             <option value="none"><?php echo TEXT_ORDER_FIND_SELECT;?></option>
             <option value="orders_id"><?php echo TEXT_ORDER_FIND_OID;?></option>
