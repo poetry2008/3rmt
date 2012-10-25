45ae466742f7c33973840480f7ad0d4f1b114aa6
zhaoyun <zhaoyun668@gmail.com>
2012-10-25 18:14:44 +0800

    zy admin

--- jp/admin/customers_products.php
+++ jp/admin/customers_products.php
@@ -103,10 +103,14 @@ function textarea_check(ele){
         i++;
       }
     } 
-    $(ele).attr("rows",comment_line+comment_str_length-i);
+    var comment_str_temp = 0;
+    if(navigator.userAgent.indexOf('Chrome') > 0){
+      comment_str_temp = 1; 
+    }
+    $(ele).attr("rows",comment_line+comment_str_length-i+comment_str_temp);
 }
 $(document).ready(function(){
-  $("textarea").change(function(){
+  $("textarea").keyup(function(){
    textarea_check(this); 
   });
 });
@@ -458,7 +462,7 @@ $(document).ready(function(){
   }
   
   // 上部文本发生改动时要重新分表格
-  function textarea_change(num){
+  function textarea_change(){
     data_empty(num);
     create_table(table_data);
   }
@@ -530,7 +534,7 @@ $(document).ready(function(){
       <table border="0" width="50%" align="right" class="print_innput" style=" margin-top:10px;">
       <tr><td height="4"></td></tr>
         <tr><td height="30" valign="bottom" align="right"><input name="textfield" type="text" id="textfield" value="<?php echo str_replace(array(' 月曜日', ' 火曜日', ' 水曜日', ' 木曜日', ' 金曜日', ' 土曜日', ' 日曜日'),'',tep_date_long(date('Y-m-d H:i:s')));?>" style=" height:20px; width:150px; text-align:right; font-size:16px;  margin:5px 0 20px 20px;"></td></tr>
-        <tr><td align="right"><textarea id="data10" type="text" rows="2" style="font-size:14px; overflow-y:visible; width:280px;  text-align:right;" ></textarea></td></tr>
+        <tr><td align="right"><textarea id="data10" type="text" rows="2" style="font-size:14px; overflow-y:visible; width:280px; resize:none;text-align:right;" ></textarea></td></tr>
         <tr><td align="right" class="input_print02">
   <font size="2">
   <input name="textfield" type="text" id="email" value="" onChange="$('#email_display').html(this.value)" onpropertychange="$('#email_display').html(this.value)" onBlur="$('#email_display').html(this.value)" style="text-align:right; font-size:12px; width:300px;">
