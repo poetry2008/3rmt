db7ce03e45211876ed2f49ccf6b2b1b949dc733b
szn000 <sznforwork+git@gmail.com>
2012-10-29 18:03:24 +0800

    change for products manual

--- jp/admin/products_manual.php
+++ jp/admin/products_manual.php
@@ -620,13 +620,13 @@ require("includes/note_js.php");
 <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
 <!-- header_eof -->
 
-<!-- body //-->
+<!-- body -->
 <table border="0" width="100%" cellspacing="2" cellpadding="2" class="content">
   <tr>
     <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft"><tr><td>
 <!-- left_navigation --> <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?> <!-- left_navigation_eof -->
     </td></tr></table>
-<!-- body_text //-->
+<!-- body_text -->
 <td width="100%" valign = "top" id='categories_right_td'><?php echo $notes;?><div class="compatible"><table border="0" width="100%" cellspacing="0" cellpadding="2">
 <tr>
 
@@ -655,7 +655,7 @@ require("includes/note_js.php");
 </tr>
 </form>
 </table>
-<!-- body_eof //-->
+<!-- body_eof -->
 </td></div>
    </table> 
 
