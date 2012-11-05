aff0a5177c56ad1d7a742017e7425f1ee3a0491b
kthiz <kthiz2006@gmail.com>
2012-11-05 17:14:09 +0800

    back point

--- id/change_preorder_confirm.php
+++ id/change_preorder_confirm.php
@@ -649,7 +649,6 @@ if(MODULE_ORDER_TOTAL_POINT_CUSTOMER_LEVEL == 'true') {
   }
   //----------------------------------------------
   
-  //丛傅唯を纷换----------------------------------
   if(mb_ereg("||", MODULE_ORDER_TOTAL_POINT_CUSTOMER_LEVER_BACK)) {
     $back_rate_array = explode("||", MODULE_ORDER_TOTAL_POINT_CUSTOMER_LEVER_BACK);
   $back_rate = MODULE_ORDER_TOTAL_POINT_FEE;
@@ -672,7 +671,6 @@ if(MODULE_ORDER_TOTAL_POINT_CUSTOMER_LEVEL == 'true') {
 } else {
   $point_rate = MODULE_ORDER_TOTAL_POINT_FEE;
 }
-// ここまでカスタマ〖レベルに炳じたポイント丛傅唯换叫============================================================
   $point_rate = $payment_modules->get_point_rate($con_payment_code);
   if ($preorder_subtotal > 0) {
     if (isset($_SESSION['preorder_campaign_fee'])) {
