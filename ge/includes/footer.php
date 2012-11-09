22ad626ab677cc8a92a7a41a8fa7e778358e217c
hm1002 <liufangzhaoly@gmail.com>
2012-11-09 09:16:50 +0800

    footer

--- ge/includes/footer.php
+++ ge/includes/footer.php
@@ -4,14 +4,17 @@
 */
 ?>
 <div id="f_menu">
-<?php if ($banner = tep_banner_exists('dynamic', 'footer1')) { echo  '<div align="center">' . "\n" . tep_display_banner('static', $banner) . '</div>'; }?>
+<?php if ($banner = tep_banner_exists('dynamic', 'footer1')) { echo  tep_display_banner('static', $banner); }?>
 </div>
 <div id="footer">
   <address class="copyright">
   <?php echo FOOTER_TEXT_BODY ; ?>
   </address>
 </div>
-<?php if ($banner = tep_banner_exists('dynamic', 'footer')) {  echo  '<div class="footer_links" align="center">' .tep_display_banner('static',  $banner) . '<br><a href="'.HTTP_SERVER.'">'.STORE_NAME.'</a></div>'; }?>
+<?php if ($banner = tep_banner_exists('dynamic', 'footer')) { 
+  echo  '<div class="footer_links" align="center">' .tep_display_banner('static',
+      $banner) . '<br><a href="'.HTTP_SERVER.'">'.STORE_NAME.'</a></div>'; 
+}?>
 <?php 
 if (STORE_DB_TRANSACTIONS && false) {?>
 <div id="debug_info" style="text-align:left;">
