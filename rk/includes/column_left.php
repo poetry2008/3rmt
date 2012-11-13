543d9a0bcb987cc446bf98dbb5aedf34720f4c10
kthiz <kthiz2006@gmail.com>
2012-11-13 19:56:21 +0800

    left category5

--- rk/includes/column_left.php
+++ rk/includes/column_left.php
@@ -5,7 +5,31 @@
 
   //require(DIR_WS_BOXES . 'quick_link.php');
 
-  include(DIR_WS_BOXES . 'categories.php');
+  if ( (USE_CACHE == 'true') && !SID ) {
+    $tmp_id_array = array();
+    if (basename($_SERVER['PHP_SELF']) == FILENAME_PREORDER) {
+      $tmp_left_products_id = tep_preorder_get_products_id_by_param();
+      $tmp_left_ca_path = tep_get_product_path($tmp_left_products_id);
+      if (tep_not_null($tmp_left_ca_path)) {
+        $tmp_id_array = tep_parse_category_path($tmp_left_ca_path); 
+      }
+    } else if (basename($_SERVER['PHP_SELF']) == FILENAME_PREORDER_PAYMENT) {
+      $tmp_left_ca_path = tep_get_product_path($_POST['products_id']);
+      if (tep_not_null($tmp_left_ca_path)) {
+        $tmp_id_array = tep_parse_category_path($tmp_left_ca_path); 
+      }
+    } else if (basename($_SERVER['PHP_SELF']) == FILENAME_PREORDER_SUCCESS) {
+      $tmp_left_preorder_product_raw = tep_db_query("select products_id from ".TABLE_PREORDERS_PRODUCTS." where orders_id = '".$_SESSION['send_preorder_id']."'"); 
+      $tmp_left_preorder_product = tep_db_fetch_array($tmp_left_preorder_product_raw);
+      $tmp_left_ca_path = tep_get_product_path($tmp_left_preorder_product['products_id']);
+      if (tep_not_null($tmp_left_ca_path)) {
+        $tmp_id_array = tep_parse_category_path($tmp_left_ca_path); 
+      }
+    }
+    echo tep_cache_categories_box(false, false, $tmp_id_array);
+  } else {
+    include(DIR_WS_BOXES . 'categories.php');
+  }
 /*
   //Color
   if(COLOR_SEARCH_BOX_TF == 'true') {
