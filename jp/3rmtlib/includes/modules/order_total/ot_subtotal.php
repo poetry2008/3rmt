7afafb951a09d62625d785533337a8a9e56bd234
hm1002 <liufangzhaoly@gmail.com>
2012-11-15 10:03:58 +0800

    made

--- jp/3rmtlib/includes/modules/order_total/ot_subtotal.php
+++ jp/3rmtlib/includes/modules/order_total/ot_subtotal.php
@@ -56,12 +56,7 @@
 
     function install() {
       // ccdd
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title,
-        configuration_key, configuration_value, configuration_description,
-        configuration_group_id, sort_order, set_function, date_added,user_added,
-        site_id) values ('小計の表示', 'MODULE_ORDER_TOTAL_SUBTOTAL_STATUS', 'true',
-          '小計の表示をしますか?', '6', '1','tep_cfg_select_option(array(\'true\',
-              \'false\'), ', now(),'".$_SESSION['user_name']."', ".$this->site_id.")");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added,user_added, site_id) values ('小計の表示', 'MODULE_ORDER_TOTAL_SUBTOTAL_STATUS', 'true', '小計の表示をしますか?', '6', '1','tep_cfg_select_option(array(\'true\', \'false\'), ', now(),'".$_SESSION['user_name']."', ".$this->site_id.")");
       // ccdd
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added, site_id) values ('表示の整列順', 'MODULE_ORDER_TOTAL_SUBTOTAL_SORT_ORDER', '1', '表示の整列順を設定できます. 数字が小さいほど上位に表示されます.', '6', '2', now(), ".$this->site_id.")");
     }
