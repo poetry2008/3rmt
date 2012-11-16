e45e6e50155e0484d5c40909f629fb83c10a3fa0
szn000 <sznforwork+git@gmail.com>
2012-11-16 08:40:31 +0800

    change for function nage

--- jp/admin/categories.php
+++ jp/admin/categories.php
@@ -1420,9 +1420,9 @@ require("includes/note_js.php");
 // 数据传输错误 提示DIV 
 if(isset($_GET['eof'])&&$_GET['eof']=='error'){ 
 ?>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF" onload="show_eof_error()" >
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF" onload="show_error_message()" >
 <div id="popup_info">
-<div class="popup_img"><img onclick="close_eof_error()" src="images/close.gif" alt="close" /></div>
+<div class="popup_img"><img onclick="close_error_message()" src="images/close_error_message.gif" alt="close" /></div>
 <span><?php echo TEXT_EOF_ERROR_MSG;?></span>
 </div>
 <div id="popup_box"></div>
--- jp/admin/includes/javascript/all_page.js
+++ jp/admin/includes/javascript/all_page.js
@@ -12,14 +12,14 @@ function resizepage(){
   var _left = (document.body.clientWidth - msg_div_width)/2;
   $('#popup_info').css({'left':_left});
 }
-function show_eof_error(){
+function show_error_message(){
   var msg_div_height = $('#popup_info').height();
   var msg_div_width = $('#popup_info').width();
   var _left = (document.body.clientWidth - msg_div_width)/2;
   $('#popup_info').css({'left':_left,'top':_top});
   $('#popup_box').css('height',document.body.scrollHeight);
 }
-function close_eof_error(){
+function close_error_message(){
   $('#popup_info').css('display','none');
   $('#popup_box').css('display','none');
 }
