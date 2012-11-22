60bab915a63f3d7395e1a0d593810fa4e8a458a6
zhaoyun <zhaoyun668@gmail.com>
2012-11-22 14:58:30 +0800

    zy admin

--- jp/admin/includes/column_left.php
+++ jp/admin/includes/column_left.php
@@ -91,7 +91,10 @@ function toggle_leftColumn()
         box_warp_left = box_warp.left;
       }
       ele_obj = $("#popup_window_value_"+temp_id).offset();
-      $("#popup_window").css('top',ele_obj.top+$("#popup_window_value_"+temp_id).height()-box_warp_top);
+      var font_size = $("#popup_window_value_"+temp_id).css("font-size");
+      font_size = font_size.replace("px","");
+      font_size = parseInt(font_size);
+      $("#popup_window").css('top',ele_obj.top+$("#popup_window_value_"+temp_id).height()-box_warp_top-$("#popup_window_value_"+temp_id).height()+font_size);
       $("#popup_window").css('left',ele_obj.left-box_warp_left);
     }
   }
