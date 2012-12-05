438f49689428da02e360b32dd37644bcda7e0a37
hm1002 <liufangzhaoly@gmail.com>
2012-12-05 16:35:19 +0800

    notes

--- ff/includes/languages/japanese/present_success.php
+++ ff/includes/languages/japanese/present_success.php
@@ -10,7 +10,7 @@ define('HEADING_TITLE', 'プレゼント応募の手続きが完了しました!
 
 define('TEXT_SUCCESS', '応募されましたプレゼント商品は応募者の中から厳正な抽選を行います。<br>
 当選された方には、電子メールにてお知らせいたしいます。');
-define('TEXT_CONTACT_STORE_OWNER', 'プレゼント応募についてご質問がございましたら、<a href="' . tep_href_link(FILENAME_CONTACT_US) . '">株式会社iimy</a>までお問い合わせください。');
+define('TEXT_CONTACT_STORE_OWNER', 'プレゼント応募についてご質問がございましたら、<a href="' . tep_href_link(FILENAME_CONTACT_US) . '">'.COMPANY_NAME.'</a>までお問い合わせください。');
 define('TEXT_THANKS_FOR_SHOPPING', 'プレゼント応募ありがとうございました。');
 
 
