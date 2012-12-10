cb5884e86dc2447d614037596207648fb3db26d9
zhaoyun <zhaoyun668@gmail.com>
2012-12-10 13:13:24 +0800

    zy option

--- rr/checkout_confirmation.php
+++ rr/checkout_confirmation.php
@@ -215,12 +215,14 @@ if(array_key_exists($fixed_option_list_array[3],$ad_array)){
   foreach($all_show_option_id as $t_item_id){
       $op_price = tep_get_show_attributes_price( $all_show_option[$t_item_id]['item_id'],
         $all_show_option[$t_item_id]['group_id'], $all_show_option[$t_item_id]['value']); 
-      echo '<br><small>&nbsp;<i> - ' . $all_show_option[$t_item_id]['front_title'] .
-      ': ' .  str_replace(array("<br>", "<BR>"), '', $all_show_option[$t_item_id]['value']);
-      if ($op_price != '0') {
-        echo ' ('.$currencies->format($op_price).')'; 
+      if(trim($all_show_option[$t_item_id]['value']) != ''){
+        echo '<br><small>&nbsp;<i> - ' . $all_show_option[$t_item_id]['front_title'] .
+             ': ' .  str_replace(array("<br>", "<BR>"), '', $all_show_option[$t_item_id]['value']);
+        if ($op_price != '0') {
+          echo ' ('.$currencies->format($op_price).')'; 
+        }
+        echo '</i></small>';
       }
-      echo '</i></small>';
 
   }
   echo '</td>' . "\n";
