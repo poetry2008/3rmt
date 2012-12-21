53dc61948603b0ab3cdcd25898b60dc55fe2744a
zhaoyun <zhaoyun668@gmail.com>
2012-12-21 08:48:22 +0800

    zy admin one time pwd

--- jp/admin/ajax_preorders.php
+++ jp/admin/ajax_preorders.php
@@ -284,7 +284,7 @@ if ($_POST['orders_id'] &&
 }  else if(isset($_GET['action'])&&$_GET['action'] == 'getallpwd'){
   $sql = "select u.userid,u.rule,l.letter from ".
     TABLE_USERS." u , ".TABLE_LETTERS." l 
-		where u.userid = l.userid and (l.letter != '' or l.letter != null)"; 
+		where u.userid = l.userid and (l.letter != '' or l.letter != null)";
   $result = tep_db_query($sql);
   $arr =array();
   while($row = tep_db_fetch_array($result)){
@@ -479,7 +479,7 @@ if ($_POST['orders_id'] &&
       TABLE_USERS." u , ".TABLE_LETTERS." l,".TABLE_PERMISSIONS." p 
 			where u.userid = l.userid 
 			and (l.letter != '' or l.letter != null)
-			and u.userid=p.userid "; 
+			and u.userid=p.userid ";
     $result = tep_db_query($sql);
     $arr =array();
     while($row = tep_db_fetch_array($result)){
