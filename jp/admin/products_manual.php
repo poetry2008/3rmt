cb589ed43ab7a88e58515ffabb4a27c25b436ff9
kthiz <kthiz2006@gmail.com>
2012-12-29 11:23:52 +0800

    tinymce

--- jp/admin/products_manual.php
+++ jp/admin/products_manual.php
@@ -585,9 +585,6 @@ tinyMCE.init({
         theme_advanced_toolbar_align : "left",
         theme_advanced_statusbar_location : "bottom",
         theme_advanced_resizing : false,
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : "bottom",
         
         skin : "o2k7",
         skin_variant : "silver",
