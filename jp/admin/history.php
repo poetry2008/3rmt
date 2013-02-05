267d6fff1eaa3d968a538a588b43a61c3e3b326e
kthiz <kthiz2006@gmail.com>
2013-02-05 13:05:56 +0800

    tongye button

--- jp/admin/history.php
+++ jp/admin/history.php
@@ -165,7 +165,7 @@ require("includes/note_js.php");
 	  <input type="button" onClick = "goto()" value='<?php echo IMAGE_BACK;?>'>
           <?php
           if ($ocertify->npermission > 7) {
-            if ($_GET['action'] == 'dougyousya_categories') { 
+            if ($_GET['action'] == 'dougyousya_categories' || $_GET['action'] == 'dougyousya') { 
           ?>
           <input type="button" onClick="location.href='cleate_dougyousya.php'" value="<?php echo HISTORY_SET_NAME_TEXT;?>"> 
           <?php
