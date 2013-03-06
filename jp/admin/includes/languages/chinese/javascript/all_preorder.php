cb546f28e88c4bbdd1ce2f4f1fc9b38e48fe25dd
zhaoyun <zhaoyun668@gmail.com>
2013-03-06 09:21:36 +0800

    zy admin

--- jp/admin/includes/languages/chinese/javascript/all_preorder.php
+++ jp/admin/includes/languages/chinese/javascript/all_preorder.php
@@ -1,6 +1,6 @@
 <?php
 define('JS_TEXT_ALL_PREORDER_SAVED','已保存');
-define('JS_TEXT_ALL_PREORDER_COMPLETION_TRANSACTION','预约结束');
+define('JS_TEXT_ALL_PREORDER_COMPLETION_TRANSACTION','预约完成');
 define('JS_TEXT_ALL_PREORDER_SAVE','保存');
 define('JS_TEXT_ALL_PREORDER_BROWER_REJECTED','被浏览器拒绝！\n请在浏览器地址栏输入\'about:config\'后按Enter键\n另外\'signed.applets.codebase_principal_support\'把数值设为\'true\'');
 define('JS_TEXT_ALL_PREORDER_NO_OPTION_ORDER','尚未选择订单。');
