b26a2a2c79b777114db1f5c5957a5504ab80a4d2
kthiz <kthiz2006@gmail.com>
2013-04-18 15:27:44 +0800

    reviews edit

--- jp/3rmtlib/includes/actions/product_reviews_write.php
+++ jp/3rmtlib/includes/actions/product_reviews_write.php
@@ -63,15 +63,15 @@
         and site_id = '" . SITE_ID . "'
       ")) > REVIEWS_DAY_LIMIT) {
       $form_error = true;
-      $error_message .= "※ 今日あまりにも多いコメントを送りました、明日送ってください。";
+      $error_message .= '※ 今日あまりにも多いコメントを送りました、明日送ってください。\n';
     }
     $last_reviews_query = tep_db_query("
       select * from ".TABLE_REVIEWS." where reviews_ip = '".$_SERVER['REMOTE_ADDR']."' order by date_added DESC
     ");
     $last_reviews = tep_db_fetch_array($last_reviews_query);
     if ($last_reviews && time() - strtotime($last_reviews['date_added']) < REVIEWS_TIME_LIMIT) {
-      $form_error = true;
-      $error_message .= "※ 投稿が制限されています。時間をおいてお試しください。";
+      $form_error = true; 
+      $error_message .= '※ 投稿が制限されています。時間をおいてお試しください。\n';
     }
     if ($form_error === false) {
       tep_db_query("
