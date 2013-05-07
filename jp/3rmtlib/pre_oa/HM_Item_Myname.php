a81b6bd302a47b65ad72aa8eb77b549b4baecda1
zhaoyun <zhaoyun668@gmail.com>
2013-05-07 21:13:29 +0800

    zy oa

--- jp/3rmtlib/pre_oa/HM_Item_Myname.php
+++ jp/3rmtlib/pre_oa/HM_Item_Myname.php
@@ -75,7 +75,7 @@ class HM_Item_Myname extends HM_Item_Basic
       $classrequire = '';
     }
     
-    echo "<input id='hidden".$this->formname."' type='hidden' name='".$this->formname."'>";
+    echo "<input id='hidden".$this->formname."' type='hidden' name='".$this->formname."' value='".$this->getDefaultValue()."'>";
     echo $this->beforeInput."<span id='".$this->formname."'type='text' class='".$classrequire." outform'size='".$this->size."' name='".$this->formname."' >".$this->getDefaultValue()."</span >";
     echo "<button type='button' id = '".$this->formname.'submit'."' >$this->submitName</button>".$this->afterInput;
     echo "</td>";
