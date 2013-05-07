21c3e266244c313dc7b66693a08f6de2b347f3a9
zhaoyun <zhaoyun668@gmail.com>
2013-05-07 21:07:06 +0800

    zy oa

--- jp/3rmtlib/oa/HM_Item_Myname.php
+++ jp/3rmtlib/oa/HM_Item_Myname.php
@@ -74,9 +74,8 @@ class HM_Item_Myname extends HM_Item_Basic
     }else {
       $classrequire = '';
     }
-    if (!$m){
-      echo "<input id='hidden".$this->formname."' type='hidden' name='".$this->formname."'>";
-    }
+
+    echo "<input id='hidden".$this->formname."' type='hidden' name='".$this->formname."'>";
     echo $this->beforeInput."<span id='".$this->formname."'type='text' class='".$classrequire." outform'size='".$this->size."' name='".$this->formname."' >".$this->getDefaultValue()."</span >";
     echo "<button type='button' id = '".$this->formname.'submit'."' >$this->submitName</button>".$this->afterInput;
     echo "</td>";
@@ -108,14 +107,8 @@ class HM_Item_Myname extends HM_Item_Basic
                                  $("#popup_info").show();
                                  $("#popup_box").show();
                                }else{
-                                 $("#<?php echo $this->formname;?>").text(data);		     
-                                 <?php 
-                                 if($m){
-                                 ?>
-                                   $("#hidden<?php echo $this->formname;?>").attr("value",data);		     
-                                 <?php 
-                                  }
-                                 ?>
+                                 $("#<?php echo $this->formname;?>").text(data);		      
+                                 $("#hidden<?php echo $this->formname;?>").attr("value",data);		      
                                  $("#wait").hide();
                                  $('body').css('cursor','');
                                  checkLockOrder();
