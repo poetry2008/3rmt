5167bc7e43b96df2a439a7ffda11780bae5b8a8c
zhaoyun <zhaoyun668@gmail.com>
2013-06-06 11:27:05 +0800

    zy admin total

--- jp/admin/edit_new_preorders.php
+++ jp/admin/edit_new_preorders.php
@@ -1982,14 +1982,11 @@ if (($action == 'edit') && ($order_exists == true)) {
              '  </tr>' . "\n";
       } else { //游客
         echo '  <tr>' . "\n" .
-             '    <td align="left" class="' . $TotalStyle .  '">'.EDIT_ORDERS_TOTALDETAIL_READ.'</td>' . 
-             '    <td align="right" class="' . $TotalStyle . '">' . trim($TotalDetails["Name"]) . '</td>' . "\n" .
-             '    <td align="right" class="' . $TotalStyle . '">' . $TotalDetails["Price"] . 
+             '    <td colspan="4">'. 
                 "<input type='hidden' name='update_totals[$TotalIndex][title]' size='" . $max_length . "' value='" . trim($TotalDetails["Name"]) . "'>" . 
                 "<input type='hidden' name='update_totals[$TotalIndex][value]' size='6' value='" . $TotalDetails["Price"] . "'>" . 
                 "<input type='hidden' name='update_totals[$TotalIndex][class]' value='" . $TotalDetails["Class"] . "'>" . 
                 "<input type='hidden' name='update_totals[$TotalIndex][total_id]' value='" . $TotalDetails["TotalID"] . "'>" . 
-             '    <td align="right" class="' . $TotalStyle . '"><b>' . tep_draw_separator('pixel_trans.gif', '1', '17') . '</b>' . 
              '   </td>' . "\n" .
              '  </tr>' . "\n";
       }
