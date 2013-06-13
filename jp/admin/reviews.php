70992547ae2cebc3bdb4c98a0160516e70a2d59c
kthiz <kthiz2006@gmail.com>
2013-06-13 08:57:35 +0800

    admin permission

--- jp/admin/reviews.php
+++ jp/admin/reviews.php
@@ -659,7 +659,7 @@ require("includes/note_js.php");
 <!-- header -->
 <?php if(!(isset($_SESSION[$page_name])&&$_SESSION[$page_name])&&$_SESSION['onetime_pwd']){?>
   <script language='javascript'>
-    one_time_pwd('<?php echo $page_name;?>');
+    one_time_pwd('<?php echo $page_name;?>', '<?php echo (!empty($_SERVER['HTTP_REFERER']))?urlencode($_SERVER['HTTP_REFERER']):urlencode(tep_href_link(FILENAME_DEFAULT));?>');
   </script>
 <?php }?>
 <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
