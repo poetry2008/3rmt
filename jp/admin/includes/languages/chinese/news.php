3a0e5f01f63d5e26932b0450e502ea148b99a666
kthiz <kthiz2006@gmail.com>
2013-06-21 07:35:52 +0800

    admin modify

--- jp/admin/includes/languages/chinese/news.php
+++ jp/admin/includes/languages/chinese/news.php
@@ -37,4 +37,6 @@ define('SHOP_NAME','店铺名:');
 define('SELECT_ALL_TEXT', '全选');
 define('TEXT_ERROR_SITE', '请选择一个网站。');
 define('NEWS_FIX_SITE_TEXT', '指定网站');
+define('TEXT_SORT_ASC','▲');
+define('TEXT_SORT_DESC','▼');
 ?>
