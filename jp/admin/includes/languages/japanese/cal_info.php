d3b95486df53b5dbdc2fe74e53a38960e42f3318
hm1002 <liufangzhaoly@gmail.com>
2013-08-08 08:49:27 +0800

    hm1002

--- jp/admin/includes/languages/japanese/cal_info.php
+++ jp/admin/includes/languages/japanese/cal_info.php
@@ -8,4 +8,8 @@ define('HEADING_TITLE', '統計');
 define('HEADER_ENTRY_CAL_CUSTOMERS', '顧客数');
 define('HEADER_ENTRY_CAL_PRODUCTS', '商品数');
 define('HEADER_ENTRY_CAL_REVIEWS', 'レビュー数');
+define('TEXT_CAL_PROJECT','項目');
+define('TEXT_CAL_NUMBER','件数');
+define('TEXT_CAL_ORDER','(注文)');
+define('TEXT_CAL_PREORDER','(予約)');
 ?>
