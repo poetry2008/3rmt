ca2aade0afa861d428e5f5bf6527d0cecec9764a
hm1002 <liufangzhaoly@gmail.com>
2013-08-08 14:38:56 +0800

    hm1002

--- jp/admin/cal_info.php
+++ jp/admin/cal_info.php
@@ -236,7 +236,7 @@ require("includes/note_js.php");
               }
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
-                  'text'   => $orders.$list_status_arr[$i][0]
+                  'text'   => $list_status_arr[$i][0].$orders
                   );
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
@@ -268,7 +268,7 @@ require("includes/note_js.php");
               }
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
-                  'text'   => $orders.$list_status_arr[$i][0]
+                  'text'   => $list_status_arr[$i][0].$orders
                   );
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
@@ -300,7 +300,7 @@ require("includes/note_js.php");
               }
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
-                  'text'   => $orders.$list_status_arr[$i][0]
+                  'text'   => $list_status_arr[$i][0].$orders
                   );
               $cal_info[] = array(
                   'params' => 'class="dataTableContent"'.$onclik,
