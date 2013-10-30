792f65d0626e0bac61e28bc1c436a5687194f54f
zhaoyun <zhaoyun668@gmail.com>
2013-10-30 12:51:18 +0800

    zy present

--- jp/admin/ajax/ajax_popup.php
+++ jp/admin/ajax/ajax_popup.php
@@ -7489,7 +7489,8 @@ resize:vertical;">'.stripslashes($sql1['text']).'</textarea><br><span id="text_e
         array('text' => $present_input)
     ); 
           $present_y = '<select name="start_y" '.($disabled?$disabled:'').'>';
-          for($y=2007;$y<=$yyyy+5;$y++){
+          $start_year = $sele_sty < $yyyy ? 2007 : $yyyy;
+          for($y=$start_year;$y<=$yyyy+5;$y++){
             $nen = $y;
             if($sele_sty == $nen){
               $present_y .= "<option value=\"$nen\" selected>$nen</option>"."\n";
@@ -7531,7 +7532,8 @@ resize:vertical;">'.stripslashes($sql1['text']).'</textarea><br><span id="text_e
               $present_d .= DAY_TEXT.'<br>';
               $present_l_y = KEYWORDS_SEARCH_END_TEXT;
               $present_l_y .= '<select name="limit_y" '.($disabled?$disabled:'').'>';
-          for($y=2007;$y<=$yyyy+5;$y++){
+          $start_year = $sele_sty < $yyyy ? 2007 : $yyyy;
+          for($y=$start_year;$y<=$yyyy+5;$y++){
             $nen = $y;
             if($sele_liy == $nen){
               $present_l_y .= "<option value=\"$nen\" selected>$nen</option>"."\n";
