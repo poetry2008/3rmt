856dc40a839a3c8dc6523e7d0cc4f4913b276822
kthiz <kthiz2006@gmail.com>
2013-11-04 13:53:43 +0800

    gp yingxie edit

--- gp/index.php
+++ gp/index.php
@@ -30,9 +30,6 @@ if(isset($_GET['cmd'])&&$_GET['cmd']){
       forward404(); 
     } 
   }
-  //if (isset($_GET['tags_id'])) {
-    //forward404(); 
-  //}
 ?>
 <?php page_head();?>
 <script type="text/javascript" src="./js/jquery-1.3.2.min.js"></script>
@@ -90,7 +87,6 @@ if ($category_depth == 'nested') {
 ?> 
   <td valign="top" id="contents_long">
 <?php
-  #$current_category    = tep_db_fetch_array(tep_db_query("select * from ".TABLE_CATEGORIES." where categories_id='".$current_category_id."'"));
   if (tep_show_warning($current_category_id)) {
     echo '<div class="waring_category">'.WARN_PRODUCT_STATUS_TEXT.'</div>'; 
   }
@@ -196,7 +192,6 @@ if ($category_depth == 'nested') {
   ?>
   </div>
   </div>
-      <!--<div class="pageBottom_long"></div>-->
   <?php
       if (isset($cPath_array)) {
         if ($seo_category['seo_description']) {
@@ -259,14 +254,12 @@ $oc_title_raw = tep_db_query("select value from ".TABLE_OTHER_CONFIG." where key
 $oc_title = tep_db_fetch_array($oc_title_raw);
 if ($oc_title) {
 $oc_title_text = $oc_title['value'];
-//  echo $oc_title['value'].'<br>';
 }
 $oc_content_text = '';
 $oc_content_raw = tep_db_query("select value from ".TABLE_OTHER_CONFIG." where keyword = 'reset_pwd_content'");
 $oc_content = tep_db_fetch_array($oc_content_raw);
 if ($oc_content) {
 $oc_content_text = $oc_content;
-//  echo tep_get_replaced_reset_msg($oc_content['value']).'<br>';
 }
 
 ?>
@@ -292,26 +285,6 @@ $update_url = tep_get_popup_url();
 }
 ?> 
     </tr> 
-  <?php
-  //if (isset($default_bottom)) {
-  if (false) {
-  ?>
-  <tr>
-    <td colspan="3">
-    <?php include(DIR_WS_BOXES.'information.php');?> 
-    <div class="buttom_warp02">
-    <?php if ($banner = tep_banner_exists('dynamic', 'footer')) { echo tep_display_banner('static', $banner); }?>
-    </div>
-    
-    <div class="buttom_warp03">
-    <?php echo DEFAULT_PAGE_BOTTOM_CONTENTS;?> 
-    </div>
-    
-    </td>
-  </tr>
-    <?php
-  }
-  ?>
   </table> 
   <!-- body_eof //--> 
   <!-- footer //--> 
