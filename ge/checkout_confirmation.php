09f1e4b558f10634a6bfd97f6139de8f5ca07d89
kthiz <kthiz2006@gmail.com>
2013-11-05 15:15:43 +0800

    ge yingxie edit

--- ge/checkout_confirmation.php
+++ ge/checkout_confirmation.php
@@ -200,15 +200,6 @@ for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
     for ($j=0, $n2=sizeof($order->products[$i]['op_attributes']); $j<$n2; $j++) {  
       $all_show_option[$order->products[$i]['op_attributes'][$j]['item_id']] 
       = $order->products[$i]['op_attributes'][$j];
-      /*
-      $op_price = tep_get_show_attributes_price($order->products[$i]['op_attributes'][$j]['item_id'], $order->products[$i]['op_attributes'][$j]['group_id'], $order->products[$i]['op_attributes'][$j]['value']); 
-       
-      echo '<br>&nbsp;<i> - ' .  $order->products[$i]['op_attributes'][$j]['front_title'] . ': ' .  str_replace(array("<br>", "<BR>"), '', $order->products[$i]['op_attributes'][$j]['value']);
-      if ($op_price != '0') {
-        echo ' ('.$currencies->format($op_price).')'; 
-      }
-      echo '</i>';
-      */
     }
   }
   
@@ -216,14 +207,6 @@ for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
    for ($jk=0, $n3=sizeof($order->products[$i]['ck_attributes']); $jk<$n3; $jk++) {
       $all_show_option[$order->products[$i]['ck_attributes'][$jk]['item_id']] 
       = $order->products[$i]['ck_attributes'][$jk];
-      /*
-      $cop_price = tep_get_show_attributes_price($order->products[$i]['ck_attributes'][$jk]['item_id'], $order->products[$i]['ck_attributes'][$jk]['group_id'], $order->products[$i]['ck_attributes'][$jk]['value']); 
-      echo '<br>&nbsp;<i> - ' .  $order->products[$i]['ck_attributes'][$jk]['front_title'] . ': ' .  str_replace(array("<br>", "<BR>"), '', $order->products[$i]['ck_attributes'][$jk]['value']);
-      if ($cop_price != '0') {
-        echo ' ('.$currencies->format($cop_price).')'; 
-      }
-      echo '</i>';
-      */
     }
   }
   // new option list 
@@ -292,8 +275,6 @@ if(!empty($_SESSION['options'])){
  */
 
 
-//$address = tep_db_prepare_input($_POST['address']);
-//$country = tep_db_prepare_input($_POST['country']);
   $country_fee_array = array();
   $country_fee_id_query = tep_db_query("select name_flag,fixed_option from ". TABLE_ADDRESS ." where fixed_option!='0' and status='0'");
   while($country_fee_id_array = tep_db_fetch_array($country_fee_id_query)){
@@ -722,23 +703,6 @@ echo '<a href="' .  tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '"><sp
                   <?php
       }
 ?>
-                <?php
-                  if ($bflag_cnt == 'View' && false) {
-                    $con_show_fee = calc_buy_handle($order->info['total']); 
-                    if ($con_show_fee != 0) { 
-                ?>
-                    <tr> 
-                      <td class="main" colspan="4"><?php echo CONFIRMATION_BUYING_TEXT_TITLE; ?></td> </tr> 
-                    <tr> 
-                      <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td> 
-                      <td class="main"><?php echo CONFIRMATION_BUYING_TEXT_FEE.$currencies->format($con_show_fee); ?></td> 
-                      <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td> 
-                      <td class="main">&nbsp;</td> 
-                    </tr> 
-                <?php
-                    }
-                  }
-                ?>
                 </table>
               </td>
             </tr>
