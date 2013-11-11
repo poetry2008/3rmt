159e4ee5081addb9041b8ac6b3cd82a7f75a5907
kthiz <kthiz2006@gmail.com>
2013-11-11 14:42:39 +0800

    yingxie edit

--- rr/shopping_cart.php
+++ rr/shopping_cart.php
@@ -526,9 +526,6 @@ function change_num(ob,targ, quan,a_quan, origin_qty, origin_small)
               </td>
             </tr>
             <tr>
-              <td><?php //echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
-            </tr>
-            <tr>
               <td align="right" class="main"><b><?php echo SUB_TITLE_SUB_TOTAL; ?>
                 <span id="sub_total"><?php echo $currencies->format_total($cart->show_total()); ?></span></b></td>
             </tr>
