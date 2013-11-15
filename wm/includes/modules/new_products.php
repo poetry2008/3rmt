9a08272775d205bf8e5342dbd83c051f6fcc6f3d
szn000 <sznforwork+git@gmail.com>
2013-11-15 16:57:10 +0800

    change for new_products

--- wm/includes/modules/new_products.php
+++ wm/includes/modules/new_products.php
@@ -24,7 +24,7 @@
     
     $new_products_query = tep_db_query("
         select * from (select p.products_id, 
-               p.products_image, 
+               pd.products_image, 
                p.products_tax_class_id, 
                p.products_price, 
                p.products_price_offset, 
@@ -43,7 +43,7 @@
     
     $new_products_query = tep_db_query("
         select * from (select distinct p.products_id, 
-                        p.products_image, 
+                        pd.products_image, 
                         p.products_tax_class_id, 
                         p.products_price, 
                         p.products_price_offset, 
