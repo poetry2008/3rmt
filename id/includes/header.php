4dadf36f85c5a092b842ddf4cf265e55f21e6207
szn000 <sznforwork+git@gmail.com>
2013-12-18 10:30:09 +0800

    change for cmd top

--- id/includes/header.php
+++ id/includes/header.php
@@ -78,7 +78,7 @@
           &nbsp;&nbsp;
           <?php 
           if ($_SERVER['PHP_SELF'] == '/change_preorder_confirm.php') {
-            echo '<a href="'.HTTP_SERVER.'" class="headerNavigation">'.HEADER_TITLE_TOP.'</a>'; 
+            echo '<a href="'.tep_href_link(FILENAME_DEFAULT,'','NONSSL').'" class="headerNavigation">'.HEADER_TITLE_TOP.'</a>'; 
             echo ' &raquo; ';
             echo '<a href="javascript:void(0);" class="headerNavigation" onclick="document.forms.order1.submit();">'.CHANGE_PREORDER_BREADCRUMB_FETCH.'</a>';
             echo ' &raquo; ';
@@ -159,7 +159,6 @@ echo tep_draw_form('quick_find', tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, 
                 <?php 
             echo '<input type="hidden" name="search_in_description" value="1">';
             echo '<input type="hidden" name="inc_subcat" value="1">';
-            echo tep_hide_session_id(); 
         ?>
                 </form>
               </div>
