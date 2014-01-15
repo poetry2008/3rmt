cc3ba6a34f8405a44634c2cb8a6a6aea5c585274
kthiz <kthiz2006@gmail.com>
2014-01-15 13:57:42 +0800

    create order speed

--- jp/admin/includes/step-by-step/create_order_new_first.php
+++ jp/admin/includes/step-by-step/create_order_new_first.php
@@ -7,44 +7,7 @@
 <link rel="stylesheet" type="text/css" href="includes/jquery.autocomplete.css">
 <?php require('includes/step-by-step/form_check.js.php'); ?>
 <script language="javascript" src="includes/javascript/jquery_include.js"></script>
-<script type="text/javascript">
-function one_time_pwd(page_name, redirect_back_url){
-  $.ajax({
-url: 'ajax_orders.php?action=getpwdcheckbox',
-type: 'POST',
-data: 'page_name='+page_name,
-dataType: 'text',
-async : false,
-success: function(data) {
-if(data !='false'){
-var pwd_arr = data.split(",");
-if(data.indexOf('[SQL-ERROR]')==-1){
-pwd =  window.prompt("<?php echo JS_TEXT_INPUT_ONETIME_PWD;?>","");
-if(in_array(pwd,pwd_arr)){
-$.ajax({
-url: 'ajax_orders.php?action=save_pwd_log',
-type: 'POST',
-data: 'one_time_pwd='+pwd+'&page_name='+page_name,
-dataType: 'text',
-async : false,
-success: function(_data) {
-}
-});
-}else{
-  if (window.confirm("<?php echo JS_TEXT_ONETIME_PWD_ERROR;?>")) {
-    one_time_pwd(page_name, redirect_back_url); 
-  } else {
-    window.location.href = decodeURIComponent(redirect_back_url);
-  }
-}
-}else{
-  location.href='/admin/sql_error.php';
-}
-}
-}
-});
-}
-</script>
+<script language="javascript" src="js2php.php?path=includes|javascript&name=one_time_pwd&type=js"></script>
 <script language="javascript" src="js2php.php?path=includes&name=general&type=js"></script>
 <script language="javascript" src="includes/jquery.form.js"></script>
 <script type="text/javascript">
