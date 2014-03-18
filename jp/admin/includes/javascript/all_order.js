077dbb643c6eec925d41db0f4ce1091e60aef559
szn000 <sznforwork+git@gmail.com>
2014-03-18 10:32:36 +0800

    change for sound

--- jp/admin/includes/javascript/all_order.js
+++ jp/admin/includes/javascript/all_order.js
@@ -323,7 +323,11 @@ function playSound()
       url: 'ajax_orders.php?action=check_play_sound',
       success: function(sound_msg) {
         if (sound_msg == '1') {
-          splay('images/warn.mp3');
+          if (node.controls) {
+            node.controls.play();
+          } else {
+            node.play();
+          }
         }
       }
     });
