6c76a5f0a15c2682d6a3459dc6d39cee3f049c16
szn000 <sznforwork+git@gmail.com>
2014-04-14 14:35:04 +0800

    change for tags sql

--- jp/admin/tags.php
+++ jp/admin/tags.php
@@ -1777,7 +1777,7 @@ require("includes/note_js.php");
   from " . TABLE_TAGS . " t order by t.tags_order,t.tags_name";
   if(isset($_GET['sort'])&&$_GET['sort']){
     $tags_query_raw = "
-      select t.tags_id, t.tags_name, t.tags_images, t.tags_checked
+      select t.tags_id, t.tags_name, t.tags_images, t.tags_checked, t.user_added,t.date_added,t.user_update,t.date_update
       from " . TABLE_TAGS ." t ";
     switch($_GET['sort']){
 /*----------------------------
