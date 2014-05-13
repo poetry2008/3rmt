fb47fb3b0c81dffbb3a1eb2fe5b9e4cd9ab03f7b
hm1002 <liufangzhaoly@gmail.com>
2014-05-13 09:23:29 +0800

    hm1002

--- jp/admin/includes/general.js.php
+++ jp/admin/includes/general.js.php
@@ -460,31 +460,15 @@ function check_toggle_status(url_str, c_permission,status)
     async : false,
     success: function(data) {
       if (c_permission == 31) {
-       if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
-        if(status == 0){
         if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
           window.location.href = url_str+'&up_rs=true'; 
         }
-        }else{
-          window.location.href = url_str; 
-        }
-       }else{
-          window.location.href = url_str; 
-       }
       } else {
         var tmp_msg_arr = data.split('|||'); 
         var pwd_list_array = tmp_msg_arr[1].split(',');
         if (tmp_msg_arr[0] == '0') {
-        if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
-        if(status == 0){
         if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
           window.location.href = url_str+'&up_rs=true'; 
-        }
-        }else{
-          window.location.href = url_str; 
-        }
-        }else{
-          window.location.href = url_str; 
         }
         } else {
           var input_pwd_str = window.prompt('<?php echo JS_TEXT_INPUT_ONETIME_PWD;?>', ''); 
@@ -496,13 +480,9 @@ function check_toggle_status(url_str, c_permission,status)
              data: 'current_pwd='+input_pwd_str+'&url_redirect_str='+encodeURIComponent(url_str),
              async: false,
              success: function(msg_info) {
-             if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
               if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
                 window.location.href = url_str+'&once_pwd='+input_pwd_str+'&up_rs=true'; 
               }
-             }else{
-                window.location.href = url_str+'&once_pwd='+input_pwd_str; 
-             }
              }
            }); 
           } else {
@@ -1264,30 +1244,14 @@ function check_toggle_black_status(url_str, c_permission,status)
       var tmp_msg_arr = data.split('|||');
       var pwd_arr = tmp_msg_arr[1].split(",");
       if (c_permission == 31) {
-       if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
-        if(status == 0){
         if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
           window.location.href = url_str+'&up_rs=true'; 
         }
-        }else{
-          window.location.href = url_str; 
-        }
-        }else{
-          window.location.href = url_str; 
-        }
       } else {
         if (tmp_msg_arr[0] == '0') {
-        if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
-        if(status == 0){
           if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
             window.location.href = url_str+'&up_rs=true'; 
           }
-        }else{
-          window.location.href = url_str; 
-        }
-        }else{
-          window.location.href = url_str; 
-        }
         } else {
           var input_pwd_str = window.prompt('<?php echo JS_TEXT_INPUT_ONETIME_PWD;?>', ''); 
           if (in_array(input_pwd_str, pwd_arr)) {
@@ -1298,17 +1262,9 @@ function check_toggle_black_status(url_str, c_permission,status)
              data: 'current_pwd='+input_pwd_str+'&url_redirect_str='+encodeURIComponent(url_str),
              async: false,
              success: function(msg_info) {
-             if(url_str.match('status=0')==null && url_str.match('flag=1')==null){
-              if(status == 0){
                 if (window.confirm('<?php echo JS_TEXT_GENERAL_SHOW_REVIEW;?>')) {
                   window.location.href = url_str+'&once_pwd='+input_pwd_str+'&up_rs=true'; 
                 }
-              }else{
-                  window.location.href = url_str+'&once_pwd='+input_pwd_str; 
-              }
-             }else{
-                  window.location.href = url_str+'&once_pwd='+input_pwd_str; 
-             }
              }
            }); 
           } else {
