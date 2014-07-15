a7daa8fffe313a2bcadedaf65f3416a8085848fa
szn000 <sznforwork+git@gmail.com>
2014-07-15 13:06:40 +0800

    change for ros

--- jp/admin/roster_records.php
+++ jp/admin/roster_records.php
@@ -359,7 +359,6 @@ while($j<=$day_num)
       echo "<div>";
       foreach($show_select_group_user as $u_list){
         echo tep_valadate_attendance($u_list,$date,$att_info,$att_info['src_text']);
-        echo '<br>';
       }
       echo "</div>";
     }
