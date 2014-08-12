0e6e0e5572ddaaafad6f9aa1dba84378cdc020ea
glz <gaolinzhi@163.com>
2014-08-12 16:25:09 +0800

    language

---
+++ jp/admin/includes/languages/chinese/payrolls.php
@@ -0,0 +1,31 @@
+<?php
+define('TEXT_HEAD_TITLE','计算工资');
+define('TEXT_SHOW_CONTENTS_TITLE','显示内容');
+define('TEXT_PAYROLLS_CURRENCY_TYPE_JPY','日元');
+define('TEXT_PAYROLLS_CURRENCY_TYPE_RMB','人民元');
+define('TEXT_PAYROLLS_CURRENCY_TYPE_USD','美元');
+define('TEXT_PAYROLLS_CURRENCY_TYPE_VND','盾');
+define('TEXT_PAYROLLS_DATE_SELECT','从日期里选');
+define('TEXT_PAYROLLS_CONTENTS_SELECT','从保存的内容里选');
+define('TEXT_GROUP_SELECT', '组');
+define('TEXT_GROUP_USER_LIST', '员工');
+define('TEXT_ALL_GROUP', '全部');
+define('TEXT_UPDATE', '更新');
+define('TEXT_PAYROLLS_NAME', '姓名');
+define('TEXT_PAYROLLS_OPTION', '操作');
+define('TEXT_PAYROLLS_RESET', '重置');
+define('TEXT_PAYROLLS_PRINT', '打印');
+define('TEXT_PAYROLLS_EXPORT', '导出');
+define('TEXT_PAYROLLS_SEND_MAIL', '工资明细送信');
+define('TEXT_PAYROLLS_TOTAL', '总计');
+define('TEXT_USER_EDIT_MUST_SELECT','至少选择一个');
+define('TEXT_USER_SEND_MAIL_CONFIRM','确定要送信吗？');
+define('TEXT_PAYROLLS_TITLE','的工资 信息统计');
+define('TEXT_PAYROLLS_EFFECTIVE_PERIOD','有效期');
+define('TEXT_PAYROLLS_CONTENTS','备注');
+define('TEXT_PAYROLLS_DATE_TOTAL','总计期间');
+define('TEXT_PAYROLLS_NORMAL_ATTENDANCE','正常上班');
+define('TEXT_PAYROLLS_NORMAL_OVERTIME','加班');
+define('TEXT_PAYROLLS_PAID_LEAVE','带薪休假');
+define('TEXT_PAYROLLS_UNPAID_VACATION','请假');
+?>
