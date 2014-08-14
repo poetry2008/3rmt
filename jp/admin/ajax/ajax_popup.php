e10de28bf82869eee159a4631ea75c84e27833ca
glz <gaolinzhi@163.com>
2014-08-14 08:43:13 +0800

    add_user update_user

--- jp/admin/ajax/ajax_popup.php
+++ jp/admin/ajax/ajax_popup.php
@@ -10938,12 +10938,13 @@ if($row_array['set_time']==0){
  $user_wage_list = $_POST['user_wage_list'];
  $user_wage_list_array = array();
  $user_wage_date_array = array();
+ $show_tep_info = array();
  $user_wage_contents = '';
  if($user_wage_list != ''){
 
-   $user_wage_query = tep_db_query("select wage_id,wage_value,start_date,end_date,contents from ".TABLE_USER_WAGE_INFO." where id in (".$user_wage_list.")"); 
+   $user_wage_query = tep_db_query("select wage_id,wage_value,start_date,end_date,contents,create_date,update_date,create_user,update_user from ".TABLE_USER_WAGE_INFO." where id in (".$user_wage_list.")"); 
    while($user_wage_array = tep_db_fetch_array($user_wage_query)){
-
+   $show_tep_info[]=$user_wage_array;
      $user_wage_list_array[$user_wage_array['wage_id']] = $user_wage_array['wage_value'];
      $user_wage_date_array[$user_wage_array['wage_id']] = array('start'=>$user_wage_array['start_date'],'end'=>$user_wage_array['end_date']);
      if($user_wage_contents == ''){
@@ -11208,17 +11209,16 @@ if($row_array['set_time']==0){
        );
    $group_content_table[] = array('text'=>$group_content_row_wage);
  }
- 
- if($_POST['group_id'] > 0&&false){
+ if($_POST['group_id'] > 0){
    //作成者、作成时间、更新者、更新时间
    $group_content_table[]['text'] = array(
-        array('align' => 'left','params' => 'width="15%"', 'text' => TEXT_USER_ADDED.'&nbsp;'.((tep_not_null($groups_array['create_user'])?$groups_array['create_user']:TEXT_UNSET_DATA))), 
-        array('align' => 'left','params' => 'width="85%"','text' => TEXT_DATE_ADDED.'&nbsp;'.((tep_not_null(tep_datetime_short($groups_array['create_time'])))?tep_datetime_short($groups_array['create_time']):TEXT_UNSET_DATA)), 
+        array('align' => 'left','params' => 'width="15%"', 'text' => TEXT_USER_ADDED.'&nbsp;'.((tep_not_null($show_tep_info[0]['create_user'])?$show_tep_info[0]['create_user']:TEXT_UNSET_DATA))), 
+        array('align' => 'left','params' => 'width="85%"','text' => TEXT_DATE_ADDED.'&nbsp;'.((tep_not_null(tep_datetime_short($show_tep_info[0]['create_date'])))?tep_datetime_short($show_tep_info[0]['create_date']):TEXT_UNSET_DATA)), 
       );
   
    $group_content_table[]['text'] = array(
-        array('align' => 'left', 'text' => TEXT_USER_UPDATE.'&nbsp;'.((tep_not_null($groups_array['update_user'])?$groups_array['update_user']:TEXT_UNSET_DATA))), 
-        array('align' => 'left', 'text' => TEXT_DATE_UPDATE.'&nbsp;'.((tep_not_null(tep_datetime_short($groups_array['update_time'])))?tep_datetime_short($groups_array['update_time']):TEXT_UNSET_DATA)), 
+        array('align' => 'left', 'text' => TEXT_USER_UPDATE.'&nbsp;'.((tep_not_null($show_tep_info[0]['update_user'])?$show_tep_info[0]['update_user']:TEXT_UNSET_DATA))), 
+        array('align' => 'left', 'text' => TEXT_DATE_UPDATE.'&nbsp;'.((tep_not_null(tep_datetime_short($show_tep_info[0]['update_date'])))?tep_datetime_short($show_tep_info[0]['update_date']):TEXT_UNSET_DATA)), 
       ); 
  }
  $group_content_row_opt = array();
