2b8c1394a73984fc2f817f89f9ce2ddbcdd3e396
glz <gaolinzhi@163.com>
2014-08-21 16:59:12 +0800

    vietnamese language add

--- jp/admin/includes/languages/japanese/roster_records.php
+++ jp/admin/includes/languages/japanese/roster_records.php
@@ -73,8 +73,7 @@ define('TEXT_LEAVE_ONE_DAY','--');
 define('TEXT_SELECT_USER','ユーザ');
 define('TEXT_DELETE_REMIND','削除しますか？');
 define('TEXT_ONLY_USER_ATTENDANCE','スタッフ設定');
-define('TEXT_WARN_ATTENDANCE_TYPE_DIFF','同じく時間帯／時間数で設定してください。');
-define('TEXT_WARN_CHANGE_ATTENDANCE_OVERLAP','休暇時間は一つのシフトの出勤時間内にコントロールしてください。');
+define('TEXT_WARN_CHANGE_ATTENDANCE_OVERLAP','「開始時刻」と「終了時刻」の範囲内に設定してください。');
 
 define('SHOW_ALL_ATT_STATUS','全時間');
 define('SHOW_ERROR_ATT_STATUS','遅刻・早退');
