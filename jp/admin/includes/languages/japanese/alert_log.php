c00197b907f23ddd9d26d99186d6a61fc215eefe
hm1003 <hm1003@200.com>
2014-08-21 14:31:30 +0800

    update bulletin_board and upload icon

--- jp/admin/includes/languages/japanese/alert_log.php
+++ jp/admin/includes/languages/japanese/alert_log.php
@@ -4,7 +4,7 @@ define('HEADING_TITLE', 'アラートログ');
 
 // 访问表错误信息
 define('TEXT_ERRINFO_DB_NO_LOGINFO', 'アクセス情報が取得できませんでした');
-define('NOTICE_BULLETIN_TITLE', 'ネトゲ速報');
+define('TEXT_BULLETIN_BOARD','掲示板');
 
 // 信息
 define('TEXT_INFO_DELETE_DAY', 'アクセス情報の削除');
