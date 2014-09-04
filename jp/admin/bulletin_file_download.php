0885986bce434adc9a510174ac118faf55567aed
hm1003 <hm1003@200.com>
2014-09-04 13:00:40 +0800

    add file and download url

---
+++ jp/admin/bulletin_file_download.php
@@ -0,0 +1,27 @@
+<?php
+	
+	if(empty($_GET['file_id'])){
+		$file_id_status = false;
+	}else{
+		$file_id_status = true;
+	}
+	if($file_id_status){
+		//die(var_dump($file_id_status));	
+		if (!file_exists('upload/bulletin_board/'.$file_id)){
+			header("Content-type: text/html; charset=utf-8");
+			echo "File not found!";
+			exit; 
+                } else {
+			$file_name = explode('|||',$file_id);
+			$file_name = $file_name[0];
+			//die(var_dump($file_name));	
+			$file = fopen('upload/bulletin_board/'.$file_id,"r"); 
+			Header("Content-type: application/octet-stream");
+			Header("Accept-Ranges: bytes");
+                        Header("Accept-Length: ".filesize('upload/bulletin_board/'.$file_name));
+			Header("Content-Disposition: attachment; filename=\"".$file_name."\"");
+			echo fread($file, filesize('upload/bulletin_board/'.$file_id));
+			fclose($file);
+		}
+	}
+?>
