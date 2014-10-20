9b2b4e7ad803fd6471a5c0194286fddb2b985757
zhaoyun <zhaoyun668@gmail.com>
2014-10-20 10:23:07 +0800

    zy admin

--- jp/admin/ajax/ajax_popup.php
+++ jp/admin/ajax/ajax_popup.php
@@ -10880,8 +10880,12 @@ if($row_array['set_time']==0){
     $date_str .= '&nbsp;&nbsp;'.$self_user['name'];
   }else{
     $attendance_dd_arr = array();
-    foreach($all_user as $user_info){
-      $attendance_dd_arr = array_merge($attendance_dd_arr,tep_get_attendance_user($_GET['date'],$user_info['userid']));
+    if($_GET['index'] == ''){
+      foreach($all_user as $user_info){
+        $attendance_dd_arr = array_merge($attendance_dd_arr,tep_get_attendance_user($_GET['date'],$user_info['userid']));
+      }
+    }else{
+      $attendance_dd_arr = array_merge($attendance_dd_arr,tep_get_attendance_user($_GET['date'],$_GET['uid']));
     }
   }
 
