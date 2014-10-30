fbcaca6910e83e7af1a86ec3b8046641c7c5d772
szn000 <sznforwork+git@gmail.com>
2014-10-30 15:29:49 +0800

    change for index speed

--- jp/admin/index.php
+++ jp/admin/index.php
@@ -4,7 +4,6 @@
 */
 
   require('includes/application_top.php');
-  require(DIR_WS_CLASSES . 'currencies.php');
 
 //notes
 $notes = '';  
@@ -20,26 +19,6 @@ while($row=tep_db_fetch_array($query)){
   list($left,$top,$zindex,$xlen,$ylen) = explode('|',$row['xyz']); 
   $note_arr[] = $row['id'];
   $height_arr[] = $ylen+$top+10;
-  $time = strtotime($row['addtime']);
-  $attribute = $row['attribute'];
-  $attribute_image = $attribute == 1 ? '<image id="image_id_'.$row['id'].'" alt="'.TEXT_ATTRIBUTE_PUBLIC.'" src="images/icons/public.gif">' : '<image id="image_id_'.$row['id'].'" alt="'.TEXT_ATTRIBUTE_PRIVATE.'" src="images/icons/private.gif">';
-  $notes.= '
-    <div id="note_'.$row['id'].'" ondblclick="changeLayer(this);" class="note '.$row['color'].'" 
-    style="left:'.$left.'px;top:'.$top.'px;z-index:'.$zindex.';height:'.$ylen.'px;width:'.$xlen.'px;'.(($row['is_show'] == '1')?'display:block;':'display:none;').'">
-    <div class="note_head">
-    <div id="note_title_'.$row['id'].'" class="note_title">
-    <input type="button" onclick="note_save_text(\''.$row['id'].'\')"
-     value=" '.IMAGE_SAVE.'" >&nbsp;'.$attribute_image.'&nbsp;'.$row['title'].'&nbsp;&nbsp;
-    '.substr($row['addtime'],0,strlen($row['addtime'])-3).'
-    </div><div class="note_close">
-    <input type="hidden" value="'.$row['id'].'" class="hidden">
-    <a href="javascript:void(0);" onclick="note_min_window(\''.$row['id'].'\')"><img alt="min" title="min" src="images/icons/note_min_window.gif"></a>&nbsp; 
-    <a href="javascript:void(0);" onclick="note_desplay_none(\''.$row['id'].'\')"><img alt="close" title="close" src="images/icons/note_close.gif"></a></div>
-    </div><div id="note_text_'.$row['id'].'" class="note_textarea"
-    style="height:'.($ylen-37).'px">'
-    .'<textarea style="overflow:auto;resize: none;font-size:11px;" id="note_textarea_'.$row['id'].'">'
-    .trim(htmlspecialchars($row['content'])).'</textarea></div>
-    </div>';
 }
 //end nodes
 ?>
@@ -60,8 +39,21 @@ while($row=tep_db_fetch_array($query)){
 <script type="text/javascript" src='includes/javascript/admin_index.js?v=<?php echo $back_rand_info?>'></script>
 <?php if(!empty($height_arr)){?>
 <script language="javascript">
+function add_notes(page_self){
+  $.ajax({
+    url: 'ajax_orders.php?action=get_index_notes',
+    data:'page_name='+page_self ,
+    type: 'POST',
+    dataType: 'text',
+    async : false,
+    success: function(data){
+      $('.box_warp').html(data);
+    }
+  });
+}
 $(document).ready(function() { 
 $('.box_warp').height(<?php echo max($height_arr);?>);
+add_notes('<?php echo $belong;?>');
 <?php
 //监听memo的缩放
 foreach($note_arr as $note_row){
@@ -115,7 +107,6 @@ foreach($note_arr as $note_row){
 <td width="100%" valign="top">
 <table width="100%"><tr><td>
 <div class="box_warp">
-<?php echo $notes;?>
 </div>
 </td></tr>
 </table>
