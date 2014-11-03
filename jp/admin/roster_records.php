c6c212b3a69ecfb87ad684f070e55e8756ed8af2
gaoting <gaolinzhi@163.com>
2014-11-03 16:29:36 +0800

    permission ros

--- jp/admin/roster_records.php
+++ jp/admin/roster_records.php
@@ -1049,7 +1049,7 @@ if($param_tep[0]!=''){
         $group_str .= '<td>';
         $group_str .= TEXT_ATTENDANCE_SETTING;
         $group_sr .= '</td>';
-        $group_str .= '<td>';
+        $group_str .= '<td '.(($ocertify->npermission>15)?'':'style="display:none;"').'>';
         $group_str .= '<a id="set_attendance_info"" style="text-decoration: underline;" href="javascript:void(0);"'.($ocertify->npermission>15 ? ' onclick="set_attendance_info(this, 0,0'.$param.')"' : '').'>'.TEXT_ATTENDANCE_SETTING_MOVE.'</a>';
         $group_str .= '</td>';
         $group_str .= '<td '.(($ocertify->npermission>15 || $is_manager)?'':'style="display:none;"').'>';
@@ -1061,8 +1061,8 @@ if($param_tep[0]!=''){
         $group_str .= '<td>';
         $group_str .= '<a href="javascript:void(0);" onclick="show_replace_attendance_info(this,\'\',\'\',\'\',\'\',\''.$show_group_id.'\');"><u>'.TEXT_ATTENDANCE_SETTING_CHANGE.'</u></a>';
         $group_str .= '</td>';
-        $group_str .= '<td>';
-        $group_str .= '<a id="set_payrols_info" style="text-decoration: underline;" href="javascript:void(0);"'.($ocertify->npermission>15 ? ' onclick="set_attendance_info(this, 0,1'.$param.')"' : '').'>'.TEXT_ATTENDANCE_SETTING_PAYROLLS.'</a>';
+        $group_str .= '<td '.(($ocertify->npermission>15 || $is_manager)?'':'style="display:none;"').'>';
+        $group_str .= '<a id="set_payrols_info" style="text-decoration: underline;" href="javascript:void(0);" onclick="set_attendance_info(this, 0,1'.$param.')" >'.TEXT_ATTENDANCE_SETTING_PAYROLLS.'</a>';
         $group_str .= '</td>';
         $group_str .= '</tr>';
 
