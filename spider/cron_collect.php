9bf14c1ab79fee5f907a9398af27eaeea98664d9
hm1002 <liufangzhaoly@gmail.com>
2014-11-07 08:37:56 +0800

    auto update

---
+++ spider/cron_collect.php
@@ -0,0 +1,145 @@
+
+<?php
+define('PRO_ROOT_DIR','/home/hm1002/project/spider/');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+//file patch
+
+//require_once(PRO_ROOT_DIR."includes/configure.php");
+require_once(PRO_ROOT_DIR."class/spider.php");
+require_once(PRO_ROOT_DIR."collect.php");
+
+define('LOG_DIR',PRO_ROOT_DIR.'log/');
+
+define('LOG_FILE_NAME',LOG_DIR.date('Y-m-d',time()).'.log');
+define('LOG_FILE_NAME_LAST',LOG_DIR.'last.log');
+
+define('DB_SERVER', 'localhost'); //服务器名
+define('DB_SERVER_USERNAME', 'root'); //用户名
+define('DB_SERVER_PASSWORD', '123456'); //密码
+define('DB_DATABASE', 'osc_collect'); //数据库名
+
+$link = mysql_connect(DB_SERVER,DB_SERVER_USERNAME,DB_SERVER_PASSWORD);
+mysql_select_db(DB_DATABASE);
+
+$category_name_array=array();
+$category_type=array();
+$site_array=array();
+
+//游戏
+$category_query=  mysql_query("select distinct(category_name) from category order by category_id asc");  
+while($row = mysql_fetch_array($category_query)){
+    $category_name_array[] = $row['category_name'];
+}
+//买卖
+$category_type=array(1=>'buy',0=>'sell');
+
+//site(网站)
+$site_query = mysql_query("select site_id,site_name from site order by site_id asc");
+while($site_row = mysql_fetch_array($site_query)){
+   $site_array[] =$site_row; 
+}
+$act = $_POST['act'];
+$flag = 1;
+if($act == 'auto'){
+  $status__query = mysql_query("select is_stop,is_run from product_auto");
+  $status_array = mysql_fetch_row($status__query);
+  $is_stop = $status_array[0];
+  $is_run  = $status_array[1];
+ // $is_stop = 0;
+  /*查is_stop is_stop==1 开始更新 is_stop == 0 停止更新 update product_auto is_stop
+  =1 game_name =A game_type=B,site_name=C
+   
+   */
+
+  $auto_query = mysql_query("select game_name,game_type,site_name from product_auto");
+  $auto_attay = mysql_fetch_assoc($auto_query);
+   /*  if(empty($auto_attay)){
+       $auto_attay['game_name'] = 'FF11';
+       $auto_attay['game_type'] = 1;
+       $auto_attay['site_name'] = 1;
+    */ }
+    // print_r($auto_attay);
+if($is_run){
+ // while(true){
+    foreach($category_name_array as $game){
+        if($game == $auto_attay['game_name'] && $flag == 1){
+          continue;
+        }
+        //echo 123;die;
+        foreach($category_type as $key=>$category){
+
+          if($key != $auto_attay['game_type'] && $flag == 1){
+            continue;
+          }
+          foreach($site_array as $site){
+
+             if($site['site_id'] == $auto_attay['site_name'] && $flag == 1){
+                 $flag = 0;
+                 continue;
+             }
+             if($is_stop){
+                $is_run = 0;
+               // echo $game.'<br>';
+               // echo $key.'<br>';
+               // echo $site['site_id'];
+               // mysql_query("update product_auto set is_run='".$is_run."',game_name='".$game."',game_type='".$key."',site_name='".$site['site_id']."'");
+              echo "update product_auto set is_run='".$is_run."',game_name='".$game."',game_type='".$key."',site_name='".$site['site_id']."'";
+              exit;
+            }
+              $is_run = 1;
+              echo 123;
+            //  $tep = get_contents_main($game,$key,$site['site_id']); 
+          //    sleep(10);
+         }     
+      }
+    }
+ // }
+ }
+
+die;
+//发送游戏相关信息
+/*foreach($category_name_array as $game){
+   foreach($category_type as $key=>$category){
+	   foreach($site_array as $site){
+		   
+          $tep = get_contents_main($game,$key,$site['site_id']); 
+		  explode('|||',$tep);
+		  if($tep[0]!='error'){
+			  $write_str =$game.'--'.$category.'--'.$site['site_name'];
+			  cron_log($write_str);
+		  }else{
+			  $write_str = 'collect fail'.$game.'-'.$site['site_name'].'<br/>';
+			  cron_log($write_str);
+		  }
+		  sleep(10);
+	   } 
+   }
+}
+
+*/
+function cron_log($collect_info){
+  //文件不存在则建立
+
+  if(!file_exists(LOG_FILE_NAME)){
+     echo 'file not exist ,creating';
+     $handle=fopen(LOG_FILE_NAME,"w"); //创建文件
+     fclose($handle);
+  }
+  if (!file_exists(LOG_FILE_NAME)){
+    echo 'create file failed'.LOG_FILE_NAME;
+  }else {
+    $str_write = '';
+    $str_write .=date('H:i:s',time()).str_repeat(' ',5);
+    $str_write .= $collect_info."\n";
+
+    $handle=fopen(LOG_FILE_NAME,"a+");
+        //写日志
+    echo $str_write;
+    if(!fwrite($handle,$str_write)){//写日志失败
+      echo "failed to write log";
+    fclose($handle);
+    }
+  }
+  //将message写入文件 
+}
