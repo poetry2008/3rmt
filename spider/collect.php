924109307b1f2b7ef09930303d410a0b3b307b13
zhaoyun <zhaoyun668@gmail.com>
2014-11-14 14:51:10 +0800

    zy admin

--- spider/collect.php
+++ spider/collect.php
@@ -30,10 +30,12 @@ if($flag_check!= ''){
    if(!empty($collect_error_array)){
      //获取所有的网站
      $site_list_array = array();
-     $site_query = mysql_query("select site_id,site_name from site");
+     $site_url_array = array();
+     $site_query = mysql_query("select site_id,site_name,site_url from site");
      while($site_array = mysql_fetch_array($site_query)){
 
        $site_list_array[$site_array['site_id']] = $site_array['site_name'];
+       $site_url_array[$site_array['site_id']] = $site_array['site_url'];
      }
      //发送错误邮件
      $mail_str = '手動更新失敗詳細'."\n";
@@ -46,7 +48,13 @@ if($flag_check!= ''){
          $category_type = 0;
        }
        $category_query = mysql_query("select category_id,site_id from category where category_name='".$collect_error_value['game']."' and category_url='".$collect_error_value['url']."' and category_type='".$category_type."'");
-       $category_array = mysql_fetch_array($category_query);
+       if(mysql_num_rows($category_query) > 0){
+         $category_array = mysql_fetch_array($category_query);
+       }else{
+         $url_array = parse_url($collect_error_value['url']);
+         $url_str = $url_array['scheme'].'://'.$url_array['host'].'/';
+         $category_array['site_id'] = array_search($url_str,$site_url_array); 
+       }
        mysql_query("update product set is_error=1 where category_id='".$category_array['category_id']."'");
        $mail_str .= date('H:i:s',$collect_error_value['time']).'　　';
        $mail_str .= $collect_error_value['game'].'--';
--- spider/cron_collect.php
+++ spider/cron_collect.php
@@ -163,10 +163,12 @@ while(true){
   if(!empty($collect_error_array)){
    //获取所有的网站
    $site_list_array = array();
+   $site_url_array = array();
    $site_query = mysql_query("select site_id,site_name from site");
    while($site_array = mysql_fetch_array($site_query)){
 
      $site_list_array[$site_array['site_id']] = $site_array['site_name'];
+     $site_url_array[$site_array['site_id']] = $site_array['site_url'];
    }
    //发送错误邮件
    $mail_str = '自動更新失敗詳細'."\n";
@@ -179,7 +181,14 @@ while(true){
        $category_type = 0;
      }
      $category_query = mysql_query("select category_id,site_id from category where category_name='".$collect_error_value['game']."' and category_url='".$collect_error_value['url']."' and category_type='".$category_type."'");
-     $category_array = mysql_fetch_array($category_query);
+     if(mysql_num_rows($category_query) > 0){
+       $category_array = mysql_fetch_array($category_query);
+     }else{
+
+       $url_array = parse_url($collect_error_value['url']);
+       $url_str = $url_array['scheme'].'://'.$url_array['host'].'/';
+       $category_array['site_id'] = array_search($url_str,$site_url_array);
+     }
      mysql_query("update product set is_error=1 where category_id='".$category_array['category_id']."'");
      $mail_str .= date('H:i:s',$collect_error_value['time']).'　　';
      $mail_str .= $collect_error_value['game'].'--';
