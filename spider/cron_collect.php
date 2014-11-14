54ab824ba75235d43c7bda01ed90f4e0c464d4cf
zhaoyun <zhaoyun668@gmail.com>
2014-11-14 19:08:50 +0800

    zy admin

--- spider/cron_collect.php
+++ spider/cron_collect.php
@@ -160,10 +160,10 @@ while(true){
         $site_list_array = array();
         $site_url_array = array();
         $site_query = mysql_query("select site_id,site_name from site");
-        while($site_array = mysql_fetch_array($site_query)){
+        while($site_id_array = mysql_fetch_array($site_query)){
 
-          $site_list_array[$site_array['site_id']] = $site_array['site_name'];
-          $site_url_array[$site_array['site_id']] = $site_array['site_url'];
+          $site_list_array[$site_id_array['site_id']] = $site_id_array['site_name'];
+          $site_url_array[$site_id_array['site_id']] = $site_id_array['site_url'];
         }
         foreach($collect_error_array as $collect_error_value){
 
@@ -200,10 +200,10 @@ while(true){
    $site_list_array = array();
    $site_url_array = array();
    $site_query = mysql_query("select site_id,site_name from site");
-   while($site_array = mysql_fetch_array($site_query)){
+   while($site_id_array = mysql_fetch_array($site_query)){
 
-     $site_list_array[$site_array['site_id']] = $site_array['site_name'];
-     $site_url_array[$site_array['site_id']] = $site_array['site_url'];
+     $site_list_array[$site_id_array['site_id']] = $site_id_array['site_name'];
+     $site_url_array[$site_id_array['site_id']] = $site_id_array['site_url'];
    }
    //发送错误邮件
    $mail_str = '自動更新失敗詳細'."\n";
