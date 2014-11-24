ba2b6cf72fc95303c3430b7f37aa55e0a92bdb9d
zhaoyun <zhaoyun668@gmail.com>
2014-11-24 19:29:40 +0800

    zy spider

--- spider/show.php
+++ spider/show.php
@@ -24,7 +24,7 @@ mysql_select_db(DB_DATABASE);
 /**
 打开页面自动通过api自动获取主站数据
   */
-//get_iimy_data();
+get_iimy_data();
 function get_iimy_data(){
     $game_name = !isset($_GET['game']) ? 'FF11' : $_GET['game'];
     $category_type = $_GET['flag'] == 'sell' ? '0' : '1';
